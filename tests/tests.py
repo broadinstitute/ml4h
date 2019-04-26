80fc877aeccafa2d31947fdc4e984c0a7660ab9f
Samuel Friedman <sam@broadinstitute.org>
2019-04-26 16:54:32 -0400

    tester

--- tests/tests.py
+++ tests/tests.py
@@ -5,11 +5,12 @@ from keras.losses import logcosh
 
 # ML4CVD Imports
 sys.path.append("..")
+sys.path.append("../ml4cvd")
 # #from ..ml4cvd.DatabaseClient import BigQueryDatabaseClient, SqLiteDatabaseClient
-from ..ml4cvd.TensorMap import TensorMap
-from ..ml4cvd.arguments import parse_args
-from ..ml4cvd.tensor_maps_by_script import TMAPS
-from ..ml4cvd.recipes import test_multimodal_multitask, train_multimodal_multitask
+from ml4cvd.TensorMap import TensorMap
+from ml4cvd.arguments import parse_args
+from ml4cvd.tensor_maps_by_script import TMAPS
+from ml4cvd.recipes import test_multimodal_multitask, train_multimodal_multitask
 
 ALL_TENSORS = '/mnt/disks/data/generated/tensors/test/2019-03-21/'
 MODELS = '/mnt/ml4cvd/projects/jamesp/data/models/'
