52945ad484eea9d7bd5270ce224bde91d615cf41
Samuel Friedman <sam@broadinstitute.org>
2019-05-28 14:35:06 -0400

    sub plot better

--- ml4cvd/tensor_generators.py
+++ ml4cvd/tensor_generators.py
@@ -104,6 +104,9 @@ def multimodal_multitask_generator(batch_size, input_maps, output_maps, train_pa
                 stats[f"ValueError while attempting to generate tensor:\n{traceback.format_exc()}\n"] += 1
             except OSError:
                 stats[f"OSError while attempting to generate tensor:\n{traceback.format_exc()}\n"] += 1
+            except RuntimeError:
+                stats[f"RuntimeError while attempting to generate tensor:\n{traceback.format_exc()}\n"] += 1
+                logging.info(f"\n\nRuntime error at:\n\n{tp}\n\n")
             _log_first_error(stats)
 
         stats['epochs'] += 1
