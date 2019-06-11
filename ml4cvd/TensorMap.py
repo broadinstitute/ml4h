0b168828f52326cd93ed27dfc35fafc34af70dc6
Samuel Friedman <sam@broadinstitute.org>
2019-06-11 11:16:42 -0400

    custom loss ignore 0s

--- ml4cvd/TensorMap.py
+++ ml4cvd/TensorMap.py
@@ -256,7 +256,7 @@ class TensorMap(object):
                     # and the data itself is 0, then overwrite the value with a draw from a N(0,1)
                     if not_missing_in_channel_map and np_tensor[self.channel_map[NOT_MISSING]] == 0 and np_tensor[i] == 0:
                         np_tensor[i] = np.random.normal(1)
-                    else:
+                    elif np_tensor[i] != 0:
                         np_tensor[i] -= self.normalization['mean']
                         np_tensor[i] /= (self.normalization['std'] + EPS)
             else:
