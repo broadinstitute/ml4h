ce8e6927d0020756f66481496263f2d67149b953
Samuel Friedman <sam@broadinstitute.org>
2019-06-17 16:37:57 -0400

    append csvs

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -190,6 +190,20 @@ TMAPS['ejection_fraction'] = TensorMap('ejection_fraction', group='continuous', 
                                    normalization={'mean': 0.50, 'std': 0.046},
                                    loss='logcosh', loss_weight=1.0, channel_map={'ejection_fraction': 0})
 
+
+TMAPS['corrected_extracted_lvesv'] = TensorMap('corrected_extracted_lvesv', group='continuous', activation='linear',
+                                    loss='logcosh', channel_map={'corrected_extracted_lvesv': 0},
+                                    normalization={'mean': 47.0, 'std': 10.0})
+TMAPS['corrected_extracted_lvedv'] = TensorMap('corrected_extracted_lvedv', group='continuous', activation='linear',
+                                     loss='logcosh', channel_map={'corrected_extracted_lvedv': 0},
+                                     normalization={'mean': 142.0, 'std': 21.0})
+TMAPS['corrected_extracted_lvef'] = TensorMap('corrected_extracted_lvef', group='continuous', activation='linear',
+                                   normalization={'mean': 0.50, 'std': 0.046},
+                                   loss='logcosh', loss_weight=1.0, channel_map={'corrected_extracted_lvef': 0})
+
+
+
+
 TMAPS['end_systole_volume_corrected'] = TensorMap('end_systole_volume_corrected', group='continuous', activation='linear',
                                     loss='logcosh', channel_map={'end_systole_volume_corrected': 0},
                                     normalization={'mean': 47.0, 'std': 10.0})
