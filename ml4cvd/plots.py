2cbe08aa8b8b9f298704756838aaad8d67a4fee5
Samuel Friedman <sam@broadinstitute.org>
2019-06-21 11:22:43 -0400

    add args

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -80,7 +80,8 @@ def evaluate_predictions(tm: TensorMap, y_predictions: np.ndarray, y_truth: np.n
     elif len(tm.shape) > 1:
         prediction_flat = tm.rescale(y_predictions).flatten()
         truth_flat = tm.rescale(y_truth).flatten()
-        performance_metrics.update(plot_scatter(prediction_flat, truth_flat, title, prefix=folder))
+        if prediction_flat.shape[0] == truth_flat.shape[0]:
+            performance_metrics.update(plot_scatter(prediction_flat, truth_flat, title, prefix=folder))
     elif tm.is_continuous():
         performance_metrics.update(plot_scatter(tm.rescale(y_predictions), tm.rescale(y_truth), title, prefix=folder, paths=test_paths))
         scatters.append((tm.rescale(y_predictions), tm.rescale(y_truth), title, test_paths))
