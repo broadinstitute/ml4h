6f93065f7f6c4137ec80f3a517f309b0ae99714b
Samuel Friedman <sam@broadinstitute.org>
2019-06-25 20:40:58 -0400

    add test

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -22,7 +22,7 @@ TMAPS = dict()
 
 TMAPS['rs3829740'] = TensorMap('rs3829740', group='categorical_index', channel_map=diploid_cm, loss=weighted_crossentropy([1, .5, 2], 'rs3829740'))
 TMAPS['rs2234962'] = TensorMap('rs2234962', group='categorical_index', channel_map=diploid_cm, loss=weighted_crossentropy([.5, 1, 10], 'rs2234962'))
-TMAPS['rs2042995'] = TensorMap('rs2042995', group='categorical_index', channel_map=diploid_cm, loss=weighted_crossentropy([.5, 1, 6], 'rs2042995'))
+TMAPS['rs2042995'] = TensorMap('rs2042995', group='categorical_index', channel_map=diploid_cm, loss=weighted_crossentropy([.2, 2, 10], 'rs2042995'))
 
 TMAPS['akap9_lof'] = TensorMap('AKAP9', group='categorical_flag', channel_map={'no_akap9_lof': 0, 'akap9_lof': 1})
 TMAPS['dsc2_lof'] = TensorMap('DSC2', group='categorical_flag', channel_map={'no_dsc2_lof': 0, 'dsc2_lof': 1})
