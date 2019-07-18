4fd23a324d82a58b989e33937a61cc60af698658
Samwell Freeman <lucidtronix@gmail.com>
2019-07-17 20:37:03 -0400

    respond to review

--- ml4cvd/TensorMap.py
+++ ml4cvd/TensorMap.py
@@ -273,15 +273,15 @@ class TensorMap(object):
 
         if 'mean' in self.normalization and 'std' in self.normalization:
             not_missing_in_channel_map = NOT_MISSING in self.channel_map
-            if self.is_continuous():
+            if self.is_continuous() and not_missing_in_channel_map:
                 for i in range(0, len(np_tensor)):
-                    if not_missing_in_channel_map and self.channel_map[NOT_MISSING] == i:
+                    if self.channel_map[NOT_MISSING] == i:
                         continue
                     # If the not-missing channel exists in the channel_map and it is marked as "missing" (value of 0)
                     # and the data itself is 0, then overwrite the value with a draw from a N(0,1)
-                    if not_missing_in_channel_map and np_tensor[self.channel_map[NOT_MISSING]] == 0 and np_tensor[i] == 0:
+                    if np_tensor[self.channel_map[NOT_MISSING]] == 0 and np_tensor[i] == 0:
                         np_tensor[i] = np.random.normal(1)
-                    elif self.sentinel is None or np_tensor[i] == self.sentinel:
+                    elif np_tensor[i] == 0:
                         np_tensor[i] -= self.normalization['mean']
                         np_tensor[i] /= (self.normalization['std'] + EPS)
             else:
