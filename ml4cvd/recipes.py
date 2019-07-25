65ebe4bb1fdc07cd083b7bbd5070b026cdbf4c06
Samwell Freeman <lucidtronix@gmail.com>
2019-07-24 21:13:09 -0400

    liver fat

--- ml4cvd/recipes.py
+++ ml4cvd/recipes.py
@@ -13,9 +13,9 @@ from ml4cvd.defines import TENSOR_EXT
 from ml4cvd.arguments import parse_args
 from ml4cvd.tensor_map_maker import write_tensor_maps
 from ml4cvd.tensor_writer_ukbb import write_tensors, append_float_csv, append_gene_csv
-from ml4cvd.tensor_generators import TensorGenerator, test_train_valid_tensor_generators, big_batch_from_minibatch_generator
 from ml4cvd.metrics import get_roc_aucs, get_precision_recall_aucs, get_pearson_coefficients, log_aucs, log_pearson_coefficients
 from ml4cvd.explorations import sample_from_char_model, mri_dates, ecg_dates, predictions_to_pngs, sort_csv, plot_heatmap_from_tensor_files
+from ml4cvd.tensor_generators import TensorGenerator, test_train_valid_tensor_generators, big_batch_from_minibatch_generator, get_test_train_valid_paths
 from ml4cvd.explorations import plot_histograms_from_tensor_files_in_pdf, plot_while_learning, find_tensors, tabulate_correlations_from_tensor_files, test_labels_to_label_dictionary
 from ml4cvd.models import make_multimodal_to_multilabel_model, train_model_from_generators, get_model_inputs_outputs, make_shallow_model, make_character_model_plus, embed_model_predict
 from ml4cvd.plots import evaluate_predictions, plot_scatters, plot_rocs, plot_precision_recalls, subplot_rocs, subplot_comparison_rocs, subplot_scatters, subplot_comparison_scatters, plot_tsne
@@ -130,25 +130,21 @@ def test_multimodal_scalar_tasks(args):
 
 
 def compare_multimodal_multitask_models(args):
-    input_prefix = "input"
-    output_prefix = "output"
-
-    tensor_paths = _get_tensor_files(args.tensors)
+    _, _, generate_test = test_train_valid_tensor_generators(args.tensor_maps_in, args.tensor_maps_out, args.tensors, args.batch_size,
+                                                             args.valid_ratio, args.test_ratio, args.test_modulo, args.balance_csvs)
     models_inputs_outputs = get_model_inputs_outputs(args.model_files, args.tensor_maps_in, args.tensor_maps_out)
-    generator = TensorGenerator(args.batch_size, args.tensor_maps_in, args.tensor_maps_out, tensor_paths, keep_paths=True)
-    input_data, output_data, paths = big_batch_from_minibatch_generator(args.tensor_maps_in, args.tensor_maps_out, generator, args.test_steps)
-
-    common_outputs = _get_common_outputs(models_inputs_outputs, output_prefix)
-    predictions = _get_predictions(args, models_inputs_outputs, input_data, common_outputs, input_prefix, output_prefix)
+    input_data, output_data, paths = big_batch_from_minibatch_generator(args.tensor_maps_in, args.tensor_maps_out, generate_test, args.test_steps)
+    common_outputs = _get_common_outputs(models_inputs_outputs, 'output')
+    predictions = _get_predictions(args, models_inputs_outputs, input_data, common_outputs, 'input', 'output')
     _calculate_and_plot_prediction_stats(args, predictions, output_data, paths)
 
 
 def compare_multimodal_scalar_task_models(args):
-    tensor_paths = _get_tensor_files(args.tensors)
+    _, _, generate_test = test_train_valid_tensor_generators(args.tensor_maps_in, args.tensor_maps_out, args.tensors, args.batch_size,
+                                                             args.valid_ratio, args.test_ratio, args.test_modulo, args.balance_csvs)
     models_io = get_model_inputs_outputs(args.model_files, args.tensor_maps_in, args.tensor_maps_out)
-    generator = TensorGenerator(args.batch_size, args.tensor_maps_in, args.tensor_maps_out, tensor_paths, keep_paths=True)
     outs = _get_common_outputs(models_io, "output")
-    predictions, labels, paths = _scalar_predictions_from_generator(args, models_io, generator, args.test_steps, outs, "input", "output")
+    predictions, labels, paths = _scalar_predictions_from_generator(args, models_io, generate_test, args.test_steps, outs, "input", "output")
     _calculate_and_plot_prediction_stats(args, predictions, labels, paths)
 
 
