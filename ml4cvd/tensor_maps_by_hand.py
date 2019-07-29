439cfa6796f85aea8308bcd7e6ab068f2de4f11d
Samwell Freeman <lucidtronix@gmail.com>
2019-07-29 16:19:38 -0400

    liver fat

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -248,6 +248,8 @@ TMAPS['corrected_extracted_lvef_sentinel'] = TensorMap('corrected_extracted_lvef
 
 TMAPS['liver_fat'] = TensorMap('22402_Liver-fat-percentage_2_0', group='continuous', channel_map={'22402_Liver-fat-percentage_2_0': 0},
                                normalization={'mean': 3.91012, 'std': 4.64437}, activation='linear', loss='logcosh', loss_weight=1.0)
+TMAPS['liver_fat_sentinel'] = TensorMap('22402_Liver-fat-percentage_2_0', group='continuous', channel_map={'22402_Liver-fat-percentage_2_0': 0},
+                               normalization={'mean': 3.91012, 'std': 4.64437}, activation='linear', sentinel=0.0)
 TMAPS['gre_mullti_echo_10_te_liver'] = TensorMap('gre_mullti_echo_10_te_liver', (160, 160, 10), group='root_array')
 TMAPS['gre_mullti_echo_10_te_liver_12bit'] = TensorMap('gre_mullti_echo_10_te_liver_12bit', (160, 160, 10), group='root_array')
 TMAPS['lms_ideal_optimised_low_flip_6dyn'] = TensorMap('lms_ideal_optimised_low_flip_6dyn', (232, 256, 36), group='root_array')
