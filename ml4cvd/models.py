081e597a7db875c90314179d8375716c19054645
Samwell Freeman <lucidtronix@gmail.com>
2019-08-02 17:37:09 -0400

    long axis tensor maps

--- ml4cvd/models.py
+++ ml4cvd/models.py
@@ -880,7 +880,7 @@ def _get_layer_index_offset_str(named_layer, offset):
 
 
 def _get_layer_kind_sorted(named_layers, kind):
-    return [k for k, _ in sorted(named_layers.items(), key=lambda x: int(x.split('_')[-1])) if kind in k]
+    return [k for k in sorted(list(named_layers.keys()), key=lambda x: int(x.split('_')[-1])) if kind in k]
 
 
 def _dense_block2d(x: K.placeholder,
