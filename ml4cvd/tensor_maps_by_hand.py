4fb8f53febbd7ce175019ab94bcd795af3959473
Samwell Freeman <lucidtronix@gmail.com>
2019-08-14 18:03:17 -0400

    lvh

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -232,7 +232,7 @@ TMAPS['t-axisp'] = TensorMap('TAxis', group='continuous', channel_map={'TAxis': 
                         normalization={'mean': 40.8, 'std': 32.6}, parents=['output_median_ecg_rest'])
 
 TMAPS['charge'] = TensorMap('charge', group='continuous', channel_map={'charge': 0}, normalization={'mean': 12.0, 'std': 2.0})
-TMAPS['af_prs'] = TensorMap('AF_PRS_LDscore', group='continuous', channel_map={'AF_PRS_LDscore': 0}, normalization={'mean': 0.0, 'std': 20.0})
+TMAPS['af_prs'] = TensorMap('AF_PRS_LDscore', group='continuous', channel_map={'AF_PRS_LDscore': 0}, normalization={'mean': 0.0, 'std': 1.0})
 
 
 TMAPS['p-axis-no0'] = TensorMap('PAxis', group='continuous', channel_map={'PAxis': 0}, loss=ignore_zeros_logcosh, metrics=['logcosh'],
