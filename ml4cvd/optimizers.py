837e70626dc94bbf199170bd93d3ebe69f365115
ndiamant <ndiamant@broadinstitute.org>
2019-08-29 13:13:24 -0400

    another fix attempt

--- ml4cvd/optimizers.py
+++ ml4cvd/optimizers.py
@@ -6,7 +6,7 @@ def get_optimizer(name: str, lr: float):
     name = str.lower(name)
     try:
         opt = optimizers.get(name)
-        opt.lr = lr
+        opt.__init__(learning_rate=lr)
         return opt
     except ValueError:
         print('Using optimizer not in keras')
