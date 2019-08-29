797c70e191e9993d12a3448f5f0af763933b612d
ndiamant <ndiamant@broadinstitute.org>
2019-08-29 12:41:09 -0400

    added radam to requirements

--- ml4cvd/optimizers.py
+++ ml4cvd/optimizers.py
@@ -5,7 +5,8 @@ from keras_radam import RAdam
 def get_optimizer(name: str):
     name = str.lower(name)
     try:
-        optimizers.get(name)
+        opt = optimizers.get(name)
+        return opt
     except ValueError:
         print('Using optimizer not in keras')
     return NON_KERAS_OPTIMIZERS[name]
