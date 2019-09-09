2fd8771cbda4ec78be6d9caba42656cec0aee9f5
Samwell Freeman <lucidtronix@gmail.com>
2019-09-09 14:29:46 -0400

    brain volumes

--- ml4cvd/tensor_map_maker.py
+++ ml4cvd/tensor_map_maker.py
@@ -262,7 +262,7 @@ def _write_continuous_tensor_maps(f: TextIO, db_client: DatabaseClient, include_
         if include_missing:
             channel_map += "'not-missing': " + str(row.max_array + 1)
         channel_map += "}"
-        f.write(f"TMAPS['{row.FieldID}_{row.instance}'] = TensorMap('{name}', group='{group}', normalization={{'mean': {row.mean}, 'std': {row.std}}}, annotation_units={row.max_array+1}, {channel_map})\n")
+        f.write(f"TMAPS['{row.FieldID}_{row.instance}'] = TensorMap('{name}', group='{group}', loss='logcosh', normalization={{'mean': {row.mean}, 'std': {row.std}}}, annotation_units={row.max_array+1}, {channel_map})\n")
 
 
 def _segmented_map(name):
