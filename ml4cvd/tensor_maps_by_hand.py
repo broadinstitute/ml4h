6ee43320cd0ddd391f421d0fcc1a59e8a3bd67e3
Samwell Freeman <lucidtronix@gmail.com>
2019-09-16 21:24:41 -0400

    indexed prediction tensor maps

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -79,7 +79,7 @@ def ecg_rest_from_file(tm, hd5, dependents={}):
     return tensor / 2000.0
 
 
-TMAPS['ecg_rest_raw'] = TensorMap('strip', shape=(5000, 12), group='ecg_rest', tensor_from_file=ecg_rest_from_file,
+TMAPS['ecg_rest_raw'] = TensorMap('ecg_rest_raw', shape=(5000, 12), group='ecg_rest', tensor_from_file=ecg_rest_from_file,
                                   channel_map={'strip_I': 0, 'strip_II': 1, 'strip_III': 2, 'strip_V1': 3, 'strip_V2': 4, 'strip_V3': 5,
                      'strip_V4': 6, 'strip_V5': 7, 'strip_V6': 8, 'strip_aVF': 9, 'strip_aVL': 10, 'strip_aVR': 11})
 
