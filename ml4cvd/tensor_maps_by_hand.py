3f81f4e69adeaf0bf23a91b4cb884504c58f8e8e
Samwell Freeman <lucidtronix@gmail.com>
2019-09-24 10:34:48 -0400

    infer latent

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -557,7 +557,7 @@ TMAPS['mri_slice_blackout'] = TensorMap('mri_slice_blackout', (256, 256, 1), ten
                                         dependent_map=TMAPS['mri_slice_blackout_segmented_weighted'])
 
 TMAPS['genetic_pca_5'] = TensorMap('genetic_pca_5', group='continuous', normalization={'mean': -0.014422761536727896, 'std': 10.57799283718005},
-                                   loss='logcosh', annotation_units=5,
+                                   loss='logcosh', annotation_units=5, shape=(5,),
                                    channel_map={'22009_Genetic-principal-components_0_0': 0, '22009_Genetic-principal-components_0_1': 1,
                                                 '22009_Genetic-principal-components_0_2': 2, '22009_Genetic-principal-components_0_3': 3,
                                                 '22009_Genetic-principal-components_0_4': 4})
