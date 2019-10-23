8643c0d59b41d3ef6f122802970c994828a538dd
Samwell Freeman <lucidtronix@gmail.com>
2019-10-22 20:26:03 -0400

    b

--- ml4cvd/recipes.py
+++ ml4cvd/recipes.py
@@ -256,7 +256,8 @@ def train_siamese_model(args):
 
     data, labels, paths = big_batch_from_minibatch_generator(args.tensor_maps_in, args.tensor_maps_out, generate_test, args.test_steps, siamese=True)
     prediction = siamese_model.predict(data)
-    return plot_roc_per_class(prediction, labels, {'siamese_out': 0}, args.id, os.path.join(args.output_folder, args.id + '/'))
+    print(f'siam pred shape {prediction.shape} siam labe shape {labels.shape}')
+    return plot_roc_per_class(np.expand_dims(prediction, -1), np.expand_dims(labels, -1), {'siamese_out': 0}, args.id, os.path.join(args.output_folder, args.id + '/'))
 
 
 def plot_predictions(args):
