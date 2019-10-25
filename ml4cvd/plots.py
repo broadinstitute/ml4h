f66362ec5a80feb564a37c671a0c7c834f7f6eca
Samwell Freeman <lucidtronix@gmail.com>
2019-10-24 21:15:22 -0400

    proportional hazards

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -281,8 +281,8 @@ def plot_survival(prediction, truth, title, days_window=1825, prefix='./figures/
     intervals = truth.shape[-1] // 2
     plt.figure(figsize=(SUBPLOT_SIZE, SUBPLOT_SIZE))
     logging.info(f"Prediction shape is: {prediction.shape} truth shape is: {truth.shape}")
-    logging.info(f"Prediction  is: {prediction[:5,:5]} truth  is: {truth.shape[:5,:5]}")
-    logging.info(f"Prediction  is: {prediction[:5, intervals:intervals+5]} truth  is: {truth.shape[:5, intervals:intervals+5]}")
+    logging.info(f"Prediction  is: {prediction[:5,:5]} truth  is: {truth[:5,:5]}")
+    logging.info(f"Prediction  is: {prediction[:5, intervals:intervals+5]} truth  is: {truth[:5, intervals:intervals+5]}")
     predicted_proportion = np.sum(prediction, axis=0) / prediction.shape[0]
     true_proportion = np.sum(truth, axis=0) / truth.shape[0]
     logging.info(f"proportion shape is: {predicted_proportion.shape} truth shape is: {true_proportion.shape} begin")
