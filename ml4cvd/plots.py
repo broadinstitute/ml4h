53dbf6ef8c966f863b0dca5acc4ea80fa9b6b4df
Samwell Freeman <lucidtronix@gmail.com>
2019-10-24 21:08:42 -0400

    proportional hazards

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -288,8 +288,8 @@ def plot_survival(prediction, truth, title, days_window=1825, prefix='./figures/
     logging.info(f"proportion pred: {predicted_proportion}")
     if paths is not None:
         pass
-    plt.plot(range(0, days_window, 1+days_window//intervals), 1-predicted_proportion[:intervals], marker='o', label='predicted_proportion')
-    plt.plot(range(0, days_window, 1+days_window//intervals), 1-true_proportion[:intervals], marker='o', label='true_proportion')
+    plt.plot(range(0, days_window, 1+days_window//intervals), 1-predicted_proportion[intervals:], marker='o', label='predicted_proportion')
+    plt.plot(range(0, days_window, 1+days_window//intervals), 1-true_proportion[intervals:], marker='o', label='true_proportion')
     #plt.plot(range(intervals), predicted_proportion[:intervals], marker='o', label='predicted_proportion')
     #plt.plot(range(intervals), true_proportion[:intervals], marker='o', label='true_proportion')
     plt.xlabel('Follow up time (days)')
