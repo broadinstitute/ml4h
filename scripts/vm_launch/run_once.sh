c97455dd31ae87b81154a896a4df7c8a7ceb8846
ndiamant <ndiamant@broadinstitute.org>
2019-11-19 22:27:33 +0000

    added pre-commit to run once

--- scripts/vm_launch/run_once.sh
+++ scripts/vm_launch/run_once.sh
@@ -8,3 +8,7 @@ sudo usermod -aG docker $(whoami)
 
 # Use the docker-credential-gcr that we installed on bootup
 docker-credential-gcr configure-docker
+
+# Install pre-commit
+curl https://pre-commit.com/install-local.py | python -
+pre-commit install
