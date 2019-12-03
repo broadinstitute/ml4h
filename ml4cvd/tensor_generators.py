06d155f8f639fdd5ffff5d52da5127a0641e9dc5
Samuel Friedman <lucidtronix@gmail.com>
2019-12-03 13:55:39 -0500

    break

--- ml4cvd/tensor_generators.py
+++ ml4cvd/tensor_generators.py
@@ -253,7 +253,7 @@ class _MultiModalMultiTaskWorker:
         if self.hd5 is None:  # Don't open hd5 if everything is in the self.cache
             self.hd5 = h5py.File(path, 'r')
         tensor = tm.tensor_from_file(tm, self.hd5, self.dependents)
-        batch[name][idx] = tensor
+        batch[name][idx] = tensor.copy()
         if tm.cacheable:
             self.cache[path, name] = tensor
         return self.hd5
