678f1fe2700545920c09b0ca117b19c5e55a88df
Samwell Freeman <lucidtronix@gmail.com>
2019-12-18 18:30:02 -0500

    saliency recipe

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -1152,9 +1152,9 @@ def _transparent_saliency_map_3d(image, gradients, blur_radius=7):
     g_mean = np.mean(gradients)
     g_std = np.std(gradients)
     blurred = gaussian_filter(gradients, sigma=blur_radius)
-    rgba_map = np.zeros((image.shape[0], image.shape[1], 4))
-    rgba_map[:, :, :3] = image
-    rgba_map[:, :, :4] = np.abs(blurred - g_mean) > g_std
+    rgba_map = np.zeros((image.shape[0], image.shape[1], image.shape[2], 4))
+    rgba_map[..., :3] = image
+    rgba_map[..., :4] = np.abs(blurred - g_mean) > g_std
     return rgba_map
 
 
