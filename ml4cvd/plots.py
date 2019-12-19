6c18f0cc595590faeae22b73b97689e0bd6a5b51
Samwell Freeman <lucidtronix@gmail.com>
2019-12-18 19:25:59 -0500

    saliency recipe

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -1148,15 +1148,20 @@ def plot_tsne(x_embed, categorical_labels, continuous_labels, gene_labels, label
     logging.info(f"Saved T-SNE plot at: {figure_path}")
 
 
-def _transparent_saliency_map_3d(image, gradients, blur_radius=2):
+def _transparent_saliency_map_3d(image, gradients, blur_radius=5):
+    logging.info(f'gradients max {gradients.max()} gradients max {gradients.min()}')
     gradients -= gradients.min()
-    gradients *= (0.3 / gradients.max())
+    gradients *= (0.5 / gradients.max())
+    logging.info(f'gradients max {gradients.max()} gradients max {gradients.min()}')
     blurred = gaussian_filter(gradients, sigma=blur_radius)
+    logging.info(f'blurred max {blurred.max()} blurred max {blurred.min()}')
+    blurred -= blurred.mean()
+    logging.info(f'blurred max {blurred.max()} blurred max {blurred.min()}')
     rgba_map = np.zeros((image.shape[0], image.shape[1], image.shape[2], 3))
     image -= image.min()
-    image *= (0.7 / image.max())
+    image *= (0.5 / image.max())
     rgba_map[..., 0] = image
-    rgba_map[..., 1] = image + (blurred - gradients.mean())
+    rgba_map[..., 1] = image + blurred
     rgba_map[..., 2] = image
     return rgba_map
 
