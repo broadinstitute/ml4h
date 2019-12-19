76cfad5927cb501481bad39de9afc80beaa0c40b
Samwell Freeman <lucidtronix@gmail.com>
2019-12-18 19:16:30 -0500

    saliency recipe

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -1148,14 +1148,14 @@ def plot_tsne(x_embed, categorical_labels, continuous_labels, gene_labels, label
     logging.info(f"Saved T-SNE plot at: {figure_path}")
 
 
-def _transparent_saliency_map_3d(image, gradients, blur_radius=7):
+def _transparent_saliency_map_3d(image, gradients, blur_radius=2):
     blurred = gaussian_filter(gradients, sigma=blur_radius)
     rgba_map = np.zeros((image.shape[0], image.shape[1], image.shape[2], 3))
     image -= image.min()
     image *= (1.0 / image.max())
     np.ma.masked_where((blurred - gradients.mean()) > (0.5*gradients.std()), image)
     rgba_map[..., 0] = image
-    rgba_map[..., 1] = blurred - gradients.mean()
+    rgba_map[..., 1] = image + (blurred - gradients.mean())
     rgba_map[..., 2] = image
     return rgba_map
 
