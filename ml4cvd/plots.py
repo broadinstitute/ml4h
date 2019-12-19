a899d7eeba9ff1f3d2efd1a4a0b114e68b0629b5
Samwell Freeman <lucidtronix@gmail.com>
2019-12-18 19:36:25 -0500

    saliency recipe

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -1148,17 +1148,17 @@ def plot_tsne(x_embed, categorical_labels, continuous_labels, gene_labels, label
     logging.info(f"Saved T-SNE plot at: {figure_path}")
 
 
-def _transparent_saliency_map_3d(image, gradients, blur_radius=4):
+def _transparent_saliency_map_3d(image, gradients, blur_radius=2):
     blurred = gaussian_filter(gradients, sigma=blur_radius)
     logging.info(f'gradients max {gradients.max()} gradients max {gradients.min()}')
     logging.info(f'blurred max {blurred.max()} blurred max {blurred.min()}')
     blurred -= blurred.min()
-    blurred *= (0.5 / blurred.max())
+    blurred *= (0.8 / blurred.max())
     blurred -= blurred.mean()
     logging.info(f'blurred max {blurred.max()} blurred max {blurred.min()}')
     rgba_map = np.zeros((image.shape[0], image.shape[1], image.shape[2], 3))
     image -= image.min()
-    image *= (0.75 / image.max())
+    image *= (0.8 / image.max())
     rgba_map[..., 0] = image - blurred
     rgba_map[..., 1] = image + blurred
     rgba_map[..., 2] = image
