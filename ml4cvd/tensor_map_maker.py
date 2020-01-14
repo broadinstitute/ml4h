340f59239a06f9934d76ed4b0870b4a5390647e5
Christopher Reeder <creeder@broadinstitute.org>
2020-01-14 11:17:01 -0500

    set channel_map and size

--- ml4cvd/tensor_map_maker.py
+++ ml4cvd/tensor_map_maker.py
@@ -321,7 +321,12 @@ def generate_multi_field_continuous_tensor_map(continuous_tensors: [str], includ
 
 def generate_continuous_tensor_map_from_file(file_name: str, column_name, tensor_map_name: str, normalization: bool,
                                              discretization_boundaries: [float]) -> TensorMap:
-    return TensorMap(f'{tensor_map_name}', channel_map={tensor_map_name: 0},
-                     dtype=DataSetType.CATEGORICAL if discretization_boundaries else DataSetType.CONTINUOUS,
-                     tensor_from_file=_build_tensor_from_file(file_name, column_name, normalization),
-                     discretization=discretization_from_boundaries(discretization_boundaries))
+    if discretization_boundaries:
+        channel_map = {str(i): i for i in range(len(discretization_boundaries)+1)}
+        return TensorMap(f'{tensor_map_name}', channel_map=channel_map, shape=(len(discretization_boundaries)+1,),
+                         dtype=DataSetType.CATEGORICAL,
+                         tensor_from_file=_build_tensor_from_file(file_name, column_name, normalization),
+                         discretization=discretization_from_boundaries(discretization_boundaries))
+
+    return TensorMap(f'{tensor_map_name}', channel_map={tensor_map_name: 0}, dtype=DataSetType.CONTINUOUS,
+                     tensor_from_file=_build_tensor_from_file(file_name, column_name, normalization))
