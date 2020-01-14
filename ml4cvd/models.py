5dd0ae37330675c784ea3f1b7cef54f1d129f105
Samuel Friedman <lucidtronix@gmail.com>
2020-01-14 14:10:23 -0500

    fix

--- ml4cvd/models.py
+++ ml4cvd/models.py
@@ -587,17 +587,18 @@ def _dense_block(x: K.placeholder,
                  normalization: str,
                  regularization: str,
                  regularization_rate: float):
+    start_x = x
     for i, conv_layer in enumerate(conv_layers):
-        x = layers[f"Conv_{str(len(layers))}"] = conv_layer(x)
-        x = layers[f"Activation_{str(len(layers))}"] = _activation_layer(activation)(layers[f"Conv_{str(len(layers)-1)}"])
+        x = layers[f"Conv_{str(len(layers))}"] = conv_layer(start_x)
+        x = layers[f"Activation_{str(len(layers))}"] = _activation_layer(activation)(x)
         x = layers[f"Normalization_{str(len(layers))}"] = _normalization_layer(normalization)(x)
         x = layers[f"Regularization_{str(len(layers))}"] = _regularization_layer(dimension, regularization, regularization_rate)(x)
         if i % block_size == 0:  # TODO: pools should come AFTER the dense conv block not before.
             x = layers[f"Pooling{JOIN_CHAR}{str(len(layers))}"] = pool_layers[i // block_size](x)
-            dense_connections = [layers[f"Pooling{JOIN_CHAR}{str(len(layers)-1)}"]]
+            dense_connections = [x]
         else:
-            dense_connections += [layers[f"Regularization_{str(len(layers)-1)}"]]
-            x = layers[f"concatenate{JOIN_CHAR}{str(len(layers))}"] = concatenate(dense_connections, axis=CHANNEL_AXIS)
+            dense_connections += [x]
+            start_x = layers[f"concatenate{JOIN_CHAR}{str(len(layers))}"] = concatenate(dense_connections, axis=CHANNEL_AXIS)
     return _get_last_layer(layers)
 
 
