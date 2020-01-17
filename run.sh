3916d8842a070b00c4eb345aa66cd652048de67d
Erik Reinertsen <erikr@mit.edu>
2020-01-17 16:50:52 -0500

    changes input_tensors name to partners_ecg in wrapper that calls tf.sh recipes.py for consistency

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 
 #./scripts/tf.sh /home/${USER}/repos/ml/ml4cvd/recipes.py --mode train --tensors /home/erik/partners_ecg_hd5_paolo/ --input_tensors ecg_partners --output_tensors ecg_partners_intervals --inspect_model --training_steps 96 --validation_steps 32 --test_steps 16 --batch_size 32 --epochs 20 --patience 12 --test_modulo 0 --output_folder /home/${USER}/output/ --id ecg_partners__ecg_partners_intervals --learning_rate 0.00002
 
-./scripts/tf.sh /home/${USER}/repos/ml/ml4cvd/recipes.py --mode train --tensors /home/${USER}/partners_ecg_betatest/ --input_tensors ecg_partners --output_tensors supranodal_rhythms --inspect_model --training_steps 384 --validation_steps 32 --test_steps 16 --batch_size 32 --epochs 40 --patience 12 --test_modulo 0 --output_folder "/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/" --id supranodal_rhythms --learning_rate 0.00002
+./scripts/tf.sh /home/${USER}/repos/ml/ml4cvd/recipes.py --mode train --tensors /home/${USER}/partners_ecg_betatest/ --input_tensors partners_ecg --output_tensors supranodal_rhythms --inspect_model --training_steps 384 --validation_steps 32 --test_steps 16 --batch_size 32 --epochs 40 --patience 12 --test_modulo 0 --output_folder "/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/" --id supranodal_rhythms --learning_rate 0.00002
 
