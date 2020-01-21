7efd4a5f64fdb104da574f9fe765cd9324ea3e18
Samuel Friedman <lucidtronix@gmail.com>
2020-01-21 11:35:02 -0500

    more hyperparamz

--- ml4cvd/hyperparameters.py
+++ ml4cvd/hyperparameters.py
@@ -150,32 +150,32 @@ def optimize_architecture(args):
 
 
 def optimize_ecg_rest_architecture(args):
-    dense_blocks_sets = [[32], [48], [32, 16], [32, 32], [32, 24, 16], [48, 32, 24], [48, 48, 48], [32, 32, 32, 32], [48, 48, 48, 48]]
-    conv_layers_sets = [[16], [32], [48], [32, 32], [48, 32], [48, 32, 24], [48, 48, 48], [32, 32, 32, 32], [48, 48, 48, 48]]
-    dense_layers_sets = [[32, 32, 32], [32, 32], [16, 64], [8, 128], [16, 16], [8, 8], [48], [32], [24], [16], [8]]
+    dense_blocks_sets = [[], [32], [32, 24], [32, 24, 16], [48, 48, 48], [48, 48, 48, 48]]
+    conv_layers_sets = [[], [32], [48], [32, 32], [48, 48], [48, 32, 24], [48, 48, 48], [32, 32, 32, 32], [48, 48, 48, 48]]
+    dense_layers_sets = [[8], [16], [16, 64], [32, 128]]
     conv_dilate = [True, False]
     activation = ['leaky', 'prelu', 'relu']
     conv_normalize = ['', 'batch_norm']
     pool_type = ['max', 'average']
     space = {
-        'pool_x': hp.quniform('pool_x', 1, 16, 1),
-        #'conv_layers': hp.choice('conv_layers', conv_layers_sets),
-        #'dense_blocks': hp.choice('dense_blocks', dense_blocks_sets),
-        #'dense_layers': hp.choice('dense_layers', dense_layers_sets),
-        'conv_dilate': hp.choice('conv_dilate', conv_dilate),
-        'activation': hp.choice('activation', activation),
-        'conv_normalize': hp.choice('conv_normalize', conv_normalize),
+        'pool_x': hp.quniform('pool_x', 1, 8, 1),
+        'conv_layers': hp.choice('conv_layers', conv_layers_sets),
+        'dense_blocks': hp.choice('dense_blocks', dense_blocks_sets),
+        'dense_layers': hp.choice('dense_layers', dense_layers_sets),
+        #'conv_dilate': hp.choice('conv_dilate', conv_dilate),
+        #'activation': hp.choice('activation', activation),
+        #'conv_normalize': hp.choice('conv_normalize', conv_normalize),
         'pool_type': hp.choice('pool_type', pool_type),
-        #'conv_width': hp.loguniform('conv_width', 1, 5),
+        'conv_width': hp.loguniform('conv_width', 1, 5),
         'block_size': hp.quniform('block_size', 1, 6, 1),
     }
     param_lists = {
-        #'conv_layers': conv_layers_sets,
-        #'dense_blocks': dense_blocks_sets,
-        #'dense_layers': dense_layers_sets,
-        'conv_dilate': conv_dilate,
-        'activation': activation,
-        'conv_normalize': conv_normalize,
+        'conv_layers': conv_layers_sets,
+        'dense_blocks': dense_blocks_sets,
+        'dense_layers': dense_layers_sets,
+        #'conv_dilate': conv_dilate,
+        #'activation': activation,
+        #'conv_normalize': conv_normalize,
         'pool_type': pool_type,
     }
     hyperparameter_optimizer(args, space, param_lists)
