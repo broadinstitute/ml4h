de32b7e211d12c3ada68221f4f0a5d977a542786
Erik Reinertsen <erikr@mit.edu>
2020-01-29 16:19:04 -0500

    adds shell script to run train CNN to classify rhythms

--- run_rhythms.sh
+++ run_rhythms.sh
@@ -1,19 +1,38 @@
+MODELFILE="/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/supranodal_rhythms/supranodal_rhythms.hd5"
 TFSCRIPT="tf_gpu2.sh"
 ./scripts/${TFSCRIPT} -t \
     /home/${USER}/repos/ml/ml4cvd/recipes.py \
-    --mode train \
+    --mode test_scalar \
     --tensors /home/${USER}/partners_ecg/hd5 \
     --input_tensors partners_ecg_voltage \
     --output_tensors \
                         supranodal_rhythms \
     --inspect_model \
-    --epochs 50 \
-    --batch_size 128 \
-    --training_steps 400 \
-    --validation_steps 16 \
-    --test_steps 16 \
+    --batch_size 100 \
+    --test_steps 25000 \
     --patience 10 \
     --test_modulo 0 \
-    --learning_rate 0.0002 \
+    --learning_rate 0.001 \
+    --model_file $MODELFILE \
     --output_folder "/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/" \
-    --id supranodal_rhythms_epochs50_batch128_trainingsteps400_learning0002
+    --id supranodal_rhythms_eval_test
+    
+#TFSCRIPT="tf_gpu2.sh"
+#./scripts/${TFSCRIPT} -t \
+#    /home/${USER}/repos/ml/ml4cvd/recipes.py \
+#    --mode train \
+#    --tensors /home/${USER}/partners_ecg/hd5 \
+#    --input_tensors partners_ecg_voltage \
+#    --output_tensors \
+#                        supranodal_rhythms \
+#    --inspect_model \
+#    --epochs 50 \
+#    --batch_size 128 \
+#    --training_steps 100 \
+#    --validation_steps 40 \
+#    --test_steps 20 \
+#    --patience 10 \
+#    --test_modulo 0 \
+#    --learning_rate 0.001 \
+#    --output_folder "/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/" \
+#    --id supranodal_rhythms
