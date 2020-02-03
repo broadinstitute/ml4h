0666fcd72b6a312b1d7c3018390c48a0e7035f5d
mklarqvist <marcus.klarqvist@gmail.com>
2020-02-03 16:20:32 -0500

    more examples

--- example.py
+++ example.py
@@ -120,6 +120,92 @@ def example7():
     print(imp.retrieve_processed_data())
 
 
+def example8():
+    cur_path = pathlib.Path(__file__).parent.absolute()
+    filename = str(cur_path) + '/tensormap/data/ingest/ecg/muse_ecg_deidentified_1.xml'
+    imp = tensormap.ingest.importer.EcgImporter(filename)
+    imp.output_file = '/'.join(["./", imp.automated_name()])
+    imp.input_callbacks = [tensormap.utils.ingest.open_read_load,
+                           tensormap.utils.ingest.xml_parse]
+
+    # JSON mapper file
+    with open(str(cur_path) + '/tensormap/data/ingest/maps/muse_ecg_data.json') as r:
+        mapper = json.loads(r.read())
+
+    imp.mapper = mapper
+    imp.mapper_only = False # Import no other fields
+
+    imp.build()
+    imp.voltage_postprocessing()
+    print(imp._input_file)
+    print(imp.retrieve_processed_data())
+
+
+def example9():
+    cur_path = pathlib.Path(__file__).parent.absolute()
+    filename = str(cur_path) + '/tensormap/data/ingest/ecg/muse_ecg_deidentified_1.xml'
+    imp = tensormap.ingest.importer.EcgImporter(filename)
+    imp.output_file = '/'.join(["./", imp.automated_name()])
+    imp.input_callbacks = [tensormap.utils.ingest.open_read_load,
+                           tensormap.utils.ingest.xml_parse]
+
+    # JSON mapper file
+    with open(str(cur_path) + '/tensormap/data/ingest/maps/muse_ecg_data.json') as r:
+        mapper = json.loads(r.read())
+
+    imp.mapper = mapper
+    imp.mapper_only = True # Import all other fields
+
+    imp.build()
+    imp.voltage_postprocessing()
+    print(imp._input_file)
+    print(imp.retrieve_processed_data())
+
+
+def example10():
+    cur_path = pathlib.Path(__file__).parent.absolute()
+    filename = str(cur_path) + '/tensormap/data/ingest/ecg/muse_ecg_deidentified_1.xml.zst'
+    imp = tensormap.ingest.importer.EcgImporter(filename)
+    imp.output_file = '/'.join(["./", imp.automated_name()])
+    imp.input_callbacks = [tensormap.utils.ingest.open_read_load,
+                           tensormap.utils.ingest.zstd_decompress,
+                           tensormap.utils.ingest.xml_parse]
+
+    # JSON mapper file
+    with open(str(cur_path) + '/tensormap/data/ingest/maps/muse_ecg_data.json') as r:
+        mapper = json.loads(r.read())
+
+    imp.mapper = mapper
+    imp.mapper_only = False # Import no other fields
+
+    imp.build()
+    imp.voltage_postprocessing()
+    print(imp._input_file)
+    print(imp.retrieve_processed_data())
+
+
+def example11():
+    cur_path = pathlib.Path(__file__).parent.absolute()
+    filename = str(cur_path) + '/tensormap/data/ingest/ecg/muse_ecg_deidentified_1.xml.zst'
+    imp = tensormap.ingest.importer.EcgImporter(filename)
+    imp.output_file = '/'.join(["./", imp.automated_name()])
+    imp.input_callbacks = [tensormap.utils.ingest.open_read_load,
+                           tensormap.utils.ingest.zstd_decompress,
+                           tensormap.utils.ingest.xml_parse]
+
+    # JSON mapper file
+    with open(str(cur_path) + '/tensormap/data/ingest/maps/muse_ecg_data.json') as r:
+        mapper = json.loads(r.read())
+
+    imp.mapper = mapper
+    imp.mapper_only = True # Import all other fields
+
+    imp.build()
+    imp.voltage_postprocessing()
+    print(imp._input_file)
+    print(imp.retrieve_processed_data())
+
+
 if __name__ == "__main__":    
     example1()
     example2()
@@ -128,4 +214,8 @@ if __name__ == "__main__":
     example5()
     # example6() # Broken
     example7()
+    example8()
+    example9()
+    example10()
+    example11()
 
