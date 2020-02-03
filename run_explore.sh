0fd9d769ae33631866ac7f828da041c3b4ce8eae
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-02-03 11:03:40 -0500

    renames run_explore to describe tmap being explored

--- run_explore.sh
+++
@@ -1,9 +0,0 @@
-TFSCRIPT="tf.sh"
-./scripts/${TFSCRIPT} -t \
-    /home/${USER}/repos/ml/ml4cvd/recipes.py \
-    --mode explore \
-    --tensors /home/${USER}/partners_ecg/hd5/1995-01 \
-    --input_tensors supranodal_rhythms \
-    --test_modulo 0 \
-    --output_folder "/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/" \
-    --id explore_supranodal_rhythms
