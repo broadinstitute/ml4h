35f204d4520ac86a3787e4c624668b81548e120a
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-02-07 16:52:46 -0500

    new run_.sh script to generate metadata CSV for Puneet to explore in PostGres

--- run_generate_metadata_csv.sh
+++ run_generate_metadata_csv.sh
@@ -6,10 +6,12 @@ TFSCRIPT="tf.sh"
     --tensors \
                 /home/${USER}/partners_ecg/hd5 \
     --input_tensors \
-                    partners_ecg_read_md_clean_supranodal_rhythms \
-                    partners_ecg_read_pc_clean_supranodal_rhythms \
+                    partners_ecg_mrn \
+                    partners_ecg_date \
                     partners_ecg_read_md_raw \
                     partners_ecg_read_pc_raw \
+                    partners_ecg_read_md_clean_supranodal_rhythms \
+                    partners_ecg_read_pc_clean_supranodal_rhythms \
     --test_modulo 0 \
     --output_folder "/home/${USER}/Dropbox\ \(Partners\ HealthCare\)/partners_ecg/ml4cvd_results/" \
-    --id explore_supranodal_rhythms_all_afib_first
+    --id generate_metadata_csv
