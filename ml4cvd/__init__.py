e13bb3c930d77cc5d1c31950a53060400025a151
mklarqvist <marcus.klarqvist@gmail.com>
2020-02-10 16:42:34 -0500

    added try/catch for gcloud authentication and versioning in setup.py

--- ml4cvd/__init__.py
+++ ml4cvd/__init__.py
@@ -1,2 +1,5 @@
 from . import defines
-# from .tensorize.database.tensorize import tensorize_sql_fields, write_tensor_from_sql
+try:
+    from .tensorize.database.tensorize import tensorize_sql_fields, write_tensor_from_sql
+except Exception:
+    pass
--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     description='Machine Learning for Disease',
     url='https://github.com/broadinstitute/ml',
     python_requires='>=3.6',
-    install_requires=["numpy", "pandas", "h5py", "keras", "scipy", "numcodecs", "xmltodict", "pytest", "sklearn", "vtk", "pydicom", "nibabel", "imageio", "seaborn", "biosppy", "google-cloud-bigquery", "keras_radam"],
+    install_requires=["numpy", "pandas", "h5py", "keras", "scipy", "numcodecs", "xmltodict", "pytest", "sklearn", 
+        "pydot==1.2.4", "keras-rectified-adam==0.10.0", "nibabel==2.5.0", "pydicom==1.2.2", 
+        "hyperopt==0.1.2", "protobuf==3.7.1", "seaborn==0.9.0", "biosppy", "vtk==8.1.2",
+        "imageio==2.6.1", "apache-beam[gcp]==2.12.0", "google-cloud-storage==1.13.0", "ipywidgets==7.5.1", "bokeh",
+        "keras_radam"],
     packages=find_packages(),
 )
