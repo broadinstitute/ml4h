55d84cd13a112ab124a44496c309d9198fe4740b
mklarqvist <marcus.klarqvist@gmail.com>
2020-02-10 15:57:25 -0500

    dicom dependencies for imaging

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='Machine Learning for Disease',
     url='https://github.com/broadinstitute/ml',
     python_requires='>=3.6',
-    install_requires=["numpy", "pandas", "h5py", "keras", "scipy", "numcodecs", "xmltodict", "pytest"],
+    install_requires=["numpy", "pandas", "h5py", "keras", "scipy", "numcodecs", "xmltodict", "pytest", "sklearn", "vtk", "pydicom", "nibabel", "imageio", "seaborn", "biosppy", "google-cloud-bigquery", "keras_radam"],
     packages=find_packages(),
 )
