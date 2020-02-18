710deed9ecec47368f62324d2aea7d6e598140b8
Samuel Friedman <lucidtronix@gmail.com>
2020-02-18 19:47:57 +0000

    workers killed again

--- ml4cvd/models.py
+++ ml4cvd/models.py
@@ -875,6 +875,8 @@ def train_model_from_generators(model: Model,
 
     history = model.fit(generate_train, steps_per_epoch=training_steps, epochs=epochs, verbose=1, validation_steps=validation_steps,
                         validation_data=generate_valid, callbacks=_get_callbacks(patience, model_file))
+    generate_train.kill_workers()
+    generate_valid.kill_workers()
 
     logging.info('Model weights saved at: %s' % model_file)
     if plot:
