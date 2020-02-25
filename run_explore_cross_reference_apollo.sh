39fa8313db294037f1f73cf635e7e010339f4c89
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-02-25 17:49:35 -0500

    adds more tmaps to run_explore for cross-ref against Apollo cath lab data

--- run_explore_cross_reference_apollo.sh
+++ run_explore_cross_reference_apollo.sh
@@ -2,15 +2,19 @@ TFSCRIPT=tf.sh
 ./scripts/${TFSCRIPT} -t \
     /home/${USER}/repos/ml/ml4cvd/recipes.py \
     --mode explore \
-    --tensors /data/partners_ecg/hd5_subset \
+    --tensors /data/partners_ecg/hd5 \
     --input_tensors \
         partners_ecg_patientid_cross_reference_apollo \
         partners_ecg_date \
         partners_ecg_dob \
-    --validator_csv /data/apollo/demographics.csv \
-    --validator_key_tensor patientid \
-    --validator_key_csv Patient_ID \
+        partners_ecg_read_md_raw \
+        partners_ecg_read_pc_raw \
+        partners_ecg_rate \
+        partners_ecg_qrs \
+        partners_ecg_pr \
+        partners_ecg_qt \
+        partners_ecg_qtc \
+        voltage_len \
     --test_modulo 0 \
     --output_folder /home/${USER}/ml4cvd_results/ \
     --id explore_cross_reference_apollo
-
