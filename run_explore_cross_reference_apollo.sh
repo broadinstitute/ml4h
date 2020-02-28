a42c838e91d27c5070f947de5427a27b40e41f87
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-02-27 23:35:06 -0500

    modifies run scripts

--- run_explore_cross_reference_apollo.sh
+++ run_explore_cross_reference_apollo.sh
@@ -7,14 +7,15 @@ TFSCRIPT=tf.sh
         partners_ecg_patientid_cross_reference_apollo \
         partners_ecg_date \
         partners_ecg_dob \
-        partners_ecg_read_md_raw \
-        partners_ecg_read_pc_raw \
-        partners_ecg_rate \
-        partners_ecg_qrs \
-        partners_ecg_pr \
-        partners_ecg_qt \
-        partners_ecg_qtc \
-        voltage_len \
     --test_modulo 0 \
     --output_folder /home/${USER}/ml4cvd_results/ \
-    --id explore_cross_reference_apollo
+    --id explore_cross_reference_apollo_mrn_date_dob
+
+#        partners_ecg_read_md_raw \
+#        partners_ecg_read_pc_raw \
+#        partners_ecg_rate \
+#        partners_ecg_qrs \
+#        partners_ecg_pr \
+#        partners_ecg_qt \
+#        partners_ecg_qtc \
+#        voltage_len \
