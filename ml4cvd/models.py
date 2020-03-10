886543fce61af9e7d858e6a32d1a4feb30b4fc3c
Samwell Freeman <lucidtronix@gmail.com>
2020-03-10 08:34:25 -0400

    no compile

--- ml4cvd/models.py
+++ ml4cvd/models.py
@@ -806,7 +806,7 @@ def make_multimodal_multitask_model(tensor_maps_in: List[TensorMap] = None,
         freeze = kwargs.get('freeze_model_layers', False)
         m.load_weights(model_layers, by_name=True)
         try:
-            m_other = load_model(model_layers, custom_objects=custom_dict)
+            m_other = load_model(model_layers, custom_objects=custom_dict, compile=False)
             for other_layer in m_other.layers:
                 try:
                     target_layer = m.get_layer(other_layer.name)
