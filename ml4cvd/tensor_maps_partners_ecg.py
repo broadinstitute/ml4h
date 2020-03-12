4c8adbb223c52b303e2419d8285b54a97d4ec792
Samwell Freeman <lucidtronix@gmail.com>
2020-03-12 12:36:13 -0400

    fix

--- ml4cvd/tensor_maps_partners_ecg.py
+++ ml4cvd/tensor_maps_partners_ecg.py
@@ -92,7 +92,7 @@ def make_voltage(population_normalize: float = None):
 def uniform_voltage(population_normalize: float = None, desired_sample_rate=500):
     def get_voltage_from_file(tm, hd5, dependents={}):
         tensor = np.zeros(tm.shape, dtype=np.float32)
-        sample_rate = _decompress_data(data_compressed=hd5['ecgsamplebase'][()], dtype=hd5['ecgsamplebase'].attrs['dtype'])
+        sample_rate = int(_decompress_data(data_compressed=hd5['ecgsamplebase'][()], dtype=hd5['ecgsamplebase'].attrs['dtype']))
         for cm in tm.channel_map:
             voltage = _decompress_data(data_compressed=hd5[cm][()], dtype=hd5[cm].attrs['dtype'])
             voltage = _resample_voltage_with_rate(voltage, tm.shape[0], sample_rate, desired_sample_rate)
