a420563e66bb98c5eb66eb842db593fa7617a559
Samuel Friedman <lucidtronix@gmail.com>
2020-03-16 12:20:34 -0400

    quantify 0s

--- ml4cvd/explorations.py
+++ ml4cvd/explorations.py
@@ -51,7 +51,8 @@ def sort_csv(tensors, tensor_maps_in):
                             stats[f'{folder}_{tm.name}_{tensor[0]}'] += 1
                             stats[f'Total_{tm.name}_{tensor[0]}'] += 1
             except (IndexError, KeyError, ValueError, OSError, RuntimeError) as e:
-                logging.info(f'Got error at {name} error:\n {e} {traceback.format_exc()}')
+                pass
+                #logging.info(f'Got error at {name} error:\n {e} {traceback.format_exc()}')
 
         logging.info(f'In folder {folder} with {len(os.listdir(os.path.join(tensors, folder)))} ECGs')
         if len(os.listdir(os.path.join(tensors, folder))) > 0:
