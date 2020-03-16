66ff1fa5c0401b5f9f5ec445dcb478e5c3aaf471
Samuel Friedman <lucidtronix@gmail.com>
2020-03-16 13:47:46 -0400

    afib incident

--- scripts/tf.sh
+++ scripts/tf.sh
@@ -111,12 +111,11 @@ Attempting to run Docker with
         ${DOCKER_IMAGE} ${PYTHON_COMMAND} ${PYTHON_ARGS}
 LAUNCH_MESSAGE
 
-
-#-v /media/erisone_snf13/:/media/erisone_snf13/ \
-#--gpus all \
 ${DOCKER_COMMAND} run ${INTERACTIVE} \
+--gpus all \
 --rm \
 --ipc=host \
 -v ${WORKDIR}/:${WORKDIR}/ \
 -v /data/:/data/ \
+-v /media/erisone_snf13/:/media/erisone_snf13/ \
 ${DOCKER_IMAGE} /bin/bash -c "pip install ${WORKDIR}; ${PYTHON_COMMAND} ${PYTHON_ARGS}"
