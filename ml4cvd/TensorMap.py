85582a6778be61e53618d2f126fcdf1d691bc3d9
Samuel Friedman <lucidtronix@gmail.com>
2020-03-16 15:36:34 -0400

    afib incident

--- ml4cvd/TensorMap.py
+++ ml4cvd/TensorMap.py
@@ -317,7 +317,7 @@ def _translate(val, cur_min, cur_max, new_min, new_max):
 def str2date(d):
     parts = d.split('-')
     if len(parts) < 2:
-        return datetime.datetime.now().date()
+        raise ValueError(f'cant make date from {d}')
     return datetime.date(int(parts[0]), int(parts[1]), int(parts[2]))
 
 
