9e90acfd704ee8cf0fb684ad58cf0ad3157d2163
Samwell Freeman <lucidtronix@gmail.com>
2020-04-02 07:38:41 -0400

    language

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -338,12 +338,11 @@ def plot_survival(prediction, truth, title, days_window=1825, prefix='./figures/
     logging.info(f"Censored at each step is: {(truth.shape[0]-np.sum(truth[:, :intervals], axis=0))-cumulative_sick} out of {truth.shape[0]}")
     logging.info(f"Survivorship: {survivorship}")
     predicted_proportion = np.sum(np.cumprod(prediction[:, :intervals], axis=1), axis=0) / truth.shape[0]
-    true_proportion = cumulative_sick / truth.shape[0]  # (cumulative_sick + np.sum(truth[:, :intervals], axis=0))
     logging.info(f"proportion shape is: {predicted_proportion.shape} truth shape is: {true_proportion.shape} begin")
     if paths is not None:
         pass
     plt.plot(range(0, days_window, 1 + days_window // intervals), predicted_proportion, marker='o', label=f'Predicted Proportion C-Index:{c_index:0.3f}')
-    plt.plot(range(0, days_window, 1 + days_window // intervals), 1 - true_proportion, marker='o', label='True Proportion')
+    plt.plot(range(0, days_window, 1 + days_window // intervals), survivorship, marker='o', label='True Proportion')
     plt.xlabel('Follow up time (days)')
     plt.ylabel('Proportion Surviving')
     plt.title(title + '\n')
