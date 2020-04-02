49f9352a0e4adfe742d81e600a0861aead0eb241
Samwell Freeman <lucidtronix@gmail.com>
2020-04-02 09:54:16 -0400

    language

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -339,7 +339,7 @@ def plot_survival(prediction, truth, title, days_window=1825, prefix='./figures/
     logging.info(f"Censored at each step is: {(truth.shape[0]-np.sum(truth[:, :intervals], axis=0))-cumulative_sick} out of {truth.shape[0]}")
     logging.info(f"Cumulative Censored: {cumulative_censored}")
     logging.info(f"Survivorship: {survivorship}")
-    predicted_proportion = np.sum(np.cumprod((truth[:, :intervals]+truth[:, intervals:])*prediction[:, :intervals], axis=1), axis=0) / (truth.shape[0]-cumulative_censored)
+    predicted_proportion = np.cumsum(np.cumprod((truth[:, :intervals]+truth[:, intervals:])*prediction[:, :intervals], axis=1), axis=0) / (truth.shape[0]-cumulative_censored)
     logging.info(f"proportion shape is: {predicted_proportion.shape} survivorship shape is: {survivorship.shape} begin")
     if paths is not None:
         pass
