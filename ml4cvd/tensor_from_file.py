4a85ef6560bccb8aeecbdb7671f7bad164153783
Samwell Freeman <lucidtronix@gmail.com>
2020-04-24 07:04:02 -0400

    fix

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -1977,8 +1977,8 @@ def _preprocess_sentence(sentence):
     sentence = sentence.strip()
     # creating a space between a word and the punctuation following it
     # eg: "he is a boy." => "he is a boy ."
-    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-    sentence = re.sub(r'[" "]+', " ", sentence)
+    # sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    # sentence = re.sub(r'[" "]+', " ", sentence)
     # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
     sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
     sentence = sentence.strip()
