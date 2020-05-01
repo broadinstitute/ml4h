7c99cca5262f9016ef4e6550e85fd951fe5200ed
Samuel Friedman <lucidtronix@gmail.com>
2020-05-01 11:16:45 -0400

    ecg date

--- ml4cvd/tensor_maps_partners_ecg.py
+++ ml4cvd/tensor_maps_partners_ecg.py
@@ -1562,6 +1562,15 @@ def build_partners_tensor_maps(needed_tensor_maps: List[str]) -> Dict[str, Tenso
     }
 
     logging.info(f'needed name {needed_tensor_maps}')
+    for needed_name in needed_tensor_maps:
+        if 'survival' not in needed_name:
+            continue
+        potential_day_string = needed_name.split('_')[-1]
+        try:
+            days_window = int(potential_day_string)
+        except ValueError:
+            days_window = 1825  # Default to 5 years of follow up
+
     for diagnosis in diagnosis2column:
         # Build diagnosis classification TensorMaps
         name = f'ecg_2500_to_diagnosis_{diagnosis}'
@@ -1590,19 +1599,19 @@ def build_partners_tensor_maps(needed_tensor_maps: List[str]) -> Dict[str, Tenso
             name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
                                              dependent_map={f'incident_cox_{diagnosis}': name2tensormap[f'incident_cox_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
 
-        # Build survival curve TensorMaps
-        name = f'ecg_2500_to_survival_{diagnosis}'
-        if name in needed_tensor_maps:
-            tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis])
-            name2tensormap[f'survival_{diagnosis}'] = TensorMap(f'survival_{diagnosis}', Interpretation.SURVIVAL_CURVE)
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
-                                             dependent_map={f'survival_{diagnosis}': name2tensormap[f'survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
-        name = f'ecg_2500_to_incident_survival_{diagnosis}'
-        if name in needed_tensor_maps:
-            tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True)
-            name2tensormap[f'incident_survival_{diagnosis}'] = TensorMap(f'incident_survival_{diagnosis}', Interpretation.SURVIVAL_CURVE)
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
-                                             dependent_map={f'incident_survival_{diagnosis}': name2tensormap[f'incident_survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
+            # Build survival curve TensorMaps
+            name = f'ecg_2500_to_survival_{diagnosis}'
+            if name in needed_tensor_maps:
+                tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis])
+                name2tensormap[f'survival_{diagnosis}'] = TensorMap(f'survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), days_window=days_window)
+                name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+                                                 dependent_map={f'survival_{diagnosis}': name2tensormap[f'survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
+            name = f'ecg_2500_to_incident_survival_{diagnosis}'
+            if name in needed_tensor_maps:
+                tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True)
+                name2tensormap[f'incident_survival_{diagnosis}'] = TensorMap(f'incident_survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), days_window=days_window)
+                name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+                                                 dependent_map={f'incident_survival_{diagnosis}': name2tensormap[f'incident_survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
 
 
         #
