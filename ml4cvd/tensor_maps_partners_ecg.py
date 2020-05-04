ea4041fea29fb2dba51cbfed9f9f8de1d302015d
Samuel Friedman <lucidtronix@gmail.com>
2020-05-04 12:21:39 -0400

    ecg date

--- ml4cvd/tensor_maps_partners_ecg.py
+++ ml4cvd/tensor_maps_partners_ecg.py
@@ -1600,13 +1600,13 @@ def build_partners_tensor_maps(needed_tensor_maps: List[str]) -> Dict[str, Tenso
                                              dependent_map={f'incident_cox_{diagnosis}': name2tensormap[f'incident_cox_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
 
         # Build survival curve TensorMaps
-        name = f'ecg_2500_to_survival_{diagnosis}'
+        name = f'ecg_2500_to_survival_{diagnosis}_{days_window}'
         if name in needed_tensor_maps:
             tensor_from_file_fxn = tff = _survival_from_file(days_window, INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], population_normalize=2000)
             name2tensormap[f'survival_{diagnosis}'] = TensorMap(f'survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), days_window=days_window)
             name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
                                              dependent_map={f'survival_{diagnosis}': name2tensormap[f'survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
-        name = f'ecg_2500_to_incident_survival_{diagnosis}'
+        name = f'ecg_2500_to_incident_survival_{diagnosis}_{days_window}'
         if name in needed_tensor_maps:
             tensor_from_file_fxn = _survival_from_file(days_window, INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True, population_normalize=2000)
             name2tensormap[f'incident_survival_{diagnosis}'] = TensorMap(f'incident_survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), days_window=days_window)
