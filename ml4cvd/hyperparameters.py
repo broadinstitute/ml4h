37299f79401852cc4123fd7a143952ebc33a62a7
Samuel Friedman <lucidtronix@gmail.com>
2020-05-05 13:01:28 -0400

    hyper opt fix

--- ml4cvd/hyperparameters.py
+++ ml4cvd/hyperparameters.py
@@ -95,6 +95,8 @@ def hyperparameter_optimizer(args, space, param_lists={}):
             plot_metric_history(history, args.training_steps, title, fig_path)
             model.load_weights(os.path.join(args.output_folder, args.id, args.id + MODEL_EXT))
             loss_and_metrics = model.evaluate(test_data, test_labels, batch_size=args.batch_size)
+            logging.info(f'Current return type:{type(loss_and_metrics)}')
+            #if not isinstance(loss_and_metrics, h5py.Dataset):
             logging.info(f'Current architecture:\n{string_from_arch_dict(x)}\nCurrent model size: {model.count_params()}.')
             logging.info(f"Iteration {i} out of maximum {args.max_models}\nTest Loss: {loss_and_metrics}")
             generate_train.kill_workers()
