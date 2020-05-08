f919ecec3161f9c91a70ef4f6a218300e0eda906
Samuel Friedman <lucidtronix@gmail.com>
2020-05-08 09:40:32 -0400

    compare cox

--- ml4cvd/tensor_maps_partners_ecg.py
+++ ml4cvd/tensor_maps_partners_ecg.py
@@ -1576,41 +1576,41 @@ def build_partners_tensor_maps(needed_tensor_maps: List[str]) -> Dict[str, Tenso
         name = f'ecg_2500_to_diagnosis_{diagnosis}'
         if name in needed_tensor_maps:
             tensor_from_file_fxn = build_incidence_tensor_from_file(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], population_normalize=2000)
-            name2tensormap[f'diagnosis_{diagnosis}'] = TensorMap(f'diagnosis_{diagnosis}', Interpretation.CATEGORICAL, channel_map=_diagnosis_channels(diagnosis))
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+            name2tensormap[f'diagnosis_{diagnosis}'] = TensorMap(f'diagnosis_{diagnosis}', Interpretation.CATEGORICAL, cacheable=False, channel_map=_diagnosis_channels(diagnosis))
+            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS, cacheable=False,
                                              dependent_map={f'diagnosis_{diagnosis}': name2tensormap[f'diagnosis_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
         name = f'ecg_2500_to_incident_{diagnosis}'
         if name in needed_tensor_maps:
             tensor_from_file_fxn = build_incidence_tensor_from_file(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True, population_normalize=2000)
-            name2tensormap[f'incident_{diagnosis}'] = TensorMap(f'incident_{diagnosis}', Interpretation.CATEGORICAL, channel_map=_diagnosis_channels(diagnosis, incidence_only=True))
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+            name2tensormap[f'incident_{diagnosis}'] = TensorMap(f'incident_{diagnosis}', Interpretation.CATEGORICAL, channel_map=_diagnosis_channels(diagnosis, cacheable=False, incidence_only=True))
+            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS, cacheable=False,
                                              dependent_map={f'incident_{diagnosis}': name2tensormap[f'incident_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
         # Build time to event TensorMaps
         name = f'ecg_2500_to_cox_{diagnosis}'
         if name in needed_tensor_maps:
-            tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], population_normalize=2000)
+            tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], cacheable=False, population_normalize=2000)
             name2tensormap[f'cox_{diagnosis}'] = TensorMap(f'cox_{diagnosis}', Interpretation.TIME_TO_EVENT)
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS, cacheable=False,
                                              dependent_map={f'cox_{diagnosis}': name2tensormap[f'cox_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
         name = f'ecg_2500_to_incident_cox_{diagnosis}'
         if name in needed_tensor_maps:
-            tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True, population_normalize=2000)
+            tensor_from_file_fxn = loyalty_time_to_event(INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True, cacheable=False, population_normalize=2000)
             name2tensormap[f'incident_cox_{diagnosis}'] = TensorMap(f'incident_cox_{diagnosis}', Interpretation.TIME_TO_EVENT)
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS, cacheable=False,
                                              dependent_map={f'incident_cox_{diagnosis}': name2tensormap[f'incident_cox_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
 
         # Build survival curve TensorMaps
         name = f'ecg_2500_to_survival_{diagnosis}_{days_window}'
         if name in needed_tensor_maps:
             tensor_from_file_fxn = tff = _survival_from_file(days_window, INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], population_normalize=2000)
-            name2tensormap[f'survival_{diagnosis}'] = TensorMap(f'survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), days_window=days_window)
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+            name2tensormap[f'survival_{diagnosis}'] = TensorMap(f'survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), cacheable=False, days_window=days_window)
+            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS, cacheable=False,
                                              dependent_map={f'survival_{diagnosis}': name2tensormap[f'survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
         name = f'ecg_2500_to_incident_survival_{diagnosis}_{days_window}'
         if name in needed_tensor_maps:
             tensor_from_file_fxn = _survival_from_file(days_window, INCIDENCE_CSV, diagnosis_column=diagnosis2column[diagnosis], incidence_only=True, population_normalize=2000)
-            name2tensormap[f'incident_survival_{diagnosis}'] = TensorMap(f'incident_survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), days_window=days_window)
-            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS,
+            name2tensormap[f'incident_survival_{diagnosis}'] = TensorMap(f'incident_survival_{diagnosis}', Interpretation.SURVIVAL_CURVE, shape=(50,), cacheable=False, days_window=days_window)
+            name2tensormap[name] = TensorMap(name, shape=(2500, 12), path_prefix=PARTNERS_PREFIX, channel_map=ECG_REST_AMP_LEADS, cacheable=False,
                                              dependent_map={f'incident_survival_{diagnosis}': name2tensormap[f'incident_survival_{diagnosis}']}, tensor_from_file=tensor_from_file_fxn)
 
     logging.info(f'return names {list(name2tensormap.keys())}')
