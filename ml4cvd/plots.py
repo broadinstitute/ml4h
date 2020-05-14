c13a49c45cf5452aa6beb8957af0457f967fe001
Samwell Freeman <lucidtronix@gmail.com>
2020-05-14 16:30:03 -0400

    merge master

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -410,17 +410,18 @@ def subplot_comparison_scatters(
 
 
 def plot_survivorship(survived, days_follow_up, title, prefix='./figures/', ):
-    days_sorted = np.argsort(days_follow_up)
+    days_sorted_index = np.argsort(days_follow_up)
+    days_sorted = days_follow_up[days_sorted_index]
     alive_per_step = len(survived)
     sick_per_step = 0
     censored = 0
     survivorship = []
     for i, day_index in enumerate(days_sorted):
-        enroll_per_step = len(survived) - i
+        alive_per_step -= survived[day_index]
         sick_per_step += survived[day_index]
         censored += 1 - survived[day_index]
-        survivorship.append(1 - (sick_per_step / enroll_per_step))
-    plt.plot(range(0, len(survived)), survivorship, marker='o', label='Survivorship')
+        survivorship.append(1 - (sick_per_step / alive_per_step))
+    plt.plot(days_sorted, survivorship, marker='o', label='Survivorship')
     plt.title(f'{title} Enrolled:{len(survived)}, Censored:{censored}, Event:{sick_per_step}\n')
     plt.xlabel('Follow up time (days)')
     plt.ylabel('Proportion Surviving')
