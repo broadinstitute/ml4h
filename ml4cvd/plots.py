cae7e9be5dc7e86d4726a7213ec2d72811d09daa
Samuel Friedman <lucidtronix@gmail.com>
2020-05-18 16:35:47 -0400

    tf 2.1

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -413,7 +413,7 @@ def subplot_comparison_scatters(
     logging.info(f"Saved scatter comparisons together at: {figure_path}")
 
 
-def plot_survivorship(survived, days_follow_up, title, prefix='./figures/', max_follow_up=3650):
+def plot_survivorship(survived, days_follow_up, title, prefix='./figures/', max_follow_up=1825):
     days_sorted_index = np.argsort(days_follow_up)
     days_sorted = days_follow_up[days_sorted_index]
     alive_per_step = len(survived)
@@ -421,7 +421,8 @@ def plot_survivorship(survived, days_follow_up, title, prefix='./figures/', max_
     censored = 0
     survivorship = []
     for cur_day, day_index in enumerate(days_sorted_index):
-        days_sorted[day_index] > max_follow_up
+        if days_sorted[day_index] > max_follow_up:
+            break
         alive_per_step -= survived[day_index]
         sick_per_step += survived[day_index]
         censored += 1 - survived[day_index]
