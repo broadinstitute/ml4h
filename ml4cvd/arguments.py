af86d243c88c43c240957cb793b3d3565a304ce6
Samuel Friedman <lucidtronix@gmail.com>
2020-05-18 13:25:37 -0400

    tf 2.1

--- ml4cvd/arguments.py
+++ ml4cvd/arguments.py
@@ -375,3 +375,7 @@ def _process_args(args):
     if args.eager:
         import tensorflow as tf
         tf.config.experimental_run_functions_eagerly(True)
+    import tensorflow as tf
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = tf.compat.v1.InteractiveSession(config=config)
