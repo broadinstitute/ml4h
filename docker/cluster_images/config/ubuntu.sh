bd3aad5abc36a580e596f296cfe514deba1ef3d2
Paolo Di Achille <paolo.diachille@gmail.com>
2020-05-18 20:06:51 -0400

    tensorflow 2.2

--- docker/cluster_images/config/ubuntu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Other necessities
-apt-get update
-echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | debconf-set-selections
-apt-get install -y wget unzip curl python-pydot python-pydot-ng graphviz ttf-mscorefonts-installer
