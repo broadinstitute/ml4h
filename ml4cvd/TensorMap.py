226e9512b4d2a40e1ad2231a64eb43a3ff20facf
Samwell Freeman <lucidtronix@gmail.com>
2020-05-28 13:11:00 -0400

    lookup

--- ml4cvd/TensorMap.py
+++ ml4cvd/TensorMap.py
@@ -110,6 +110,7 @@ class TensorMap(object):
         tensor_from_file: Optional[Callable] = None,
         time_series_limit: Optional[int] = None,
         time_series_order: Optional[TimeSeriesOrder] = TimeSeriesOrder.NEWEST,
+        time_series_lookup: Optional[Dict[int,Tuple]] = None,
         discretization_bounds: Optional[List[float]] = None,
     ):
         """TensorMap constructor
@@ -138,6 +139,7 @@ class TensorMap(object):
         :param tensor_from_file: Function that returns numpy array from hd5 file for this TensorMap
         :param time_series_limit: If set, indicates dynamic shaping and sets the maximum number of tensors in a time series to use
         :param time_series_order: When selecting tensors in a time series, use newest, oldest, or randomly ordered tensors
+        :param time_series_lookup: Dict of time intervals filtering which tensors are used in a time series
         :param discretization_bounds: List of floats that delineate the boundaries of the bins that will be used
                                           for producing categorical values from continuous values
         """
@@ -165,6 +167,7 @@ class TensorMap(object):
         self.tensor_from_file = tensor_from_file
         self.time_series_limit = time_series_limit
         self.time_series_order = time_series_order
+        self.time_series_lookup = time_series_lookup
         self.discretization_bounds = discretization_bounds
 
         # Infer loss from interpretation
