90ec6ed769c346640f531d1ef6f35b92241a08ed
Samwell Freeman <lucidtronix@gmail.com>
2020-06-05 07:45:47 -0400

    no zoom

--- ml4cvd/recipes.py
+++ ml4cvd/recipes.py
@@ -19,7 +19,8 @@ from sksurv.metrics import concordance_index_censored
 try:
     import tensorflow as tf
     gpus = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+    if len(gpus) > 0:
+        tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
 except RuntimeError:
     pass
 
