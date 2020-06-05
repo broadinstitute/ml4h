2e9a6fdde2bcc168de3801db69c59032d0b73fab
Samwell Freeman <lucidtronix@gmail.com>
2020-06-05 08:58:06 -0400

    no zoom

--- ml4cvd/plots.py
+++ ml4cvd/plots.py
@@ -133,7 +133,7 @@ def evaluate_predictions(
         performance_metrics.update(plot_survival(y_predictions, y_truth, title, days_window=tm.days_window, prefix=folder))
         plot_survival_curves(y_predictions, y_truth, title, days_window=tm.days_window, prefix=folder, paths=test_paths)
         predictions_at_end = np.cumprod(y_predictions[:, :tm.shape[-1]//2], axis=-1)[:, -1]
-        events_at_end = np.cumsum(np.sum(y_truth[:, tm.shape[-1]//2:], axis=0))[:, -1]
+        events_at_end = np.cumsum(y_truth[:, tm.shape[-1]//2:], axis=-1)[:, -1]
         plot_prediction_calibration(predictions_at_end, events_at_end, {tm.name: 0}, title, folder)
     elif tm.is_time_to_event():
         c_index = concordance_index_censored(y_truth[:, 0] == 1.0, y_truth[:, 1], y_predictions[:, 0])
