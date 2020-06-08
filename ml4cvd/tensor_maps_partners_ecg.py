2718b8473d6621b08d6046a4ab0af0fa596498d1
Samuel Friedman <lucidtronix@gmail.com>
2020-06-08 13:48:10 -0400

    merged

--- ml4cvd/tensor_maps_partners_ecg.py
+++ ml4cvd/tensor_maps_partners_ecg.py
@@ -1733,7 +1733,7 @@ def _date_from_dates(ecg_dates, target_date=None, earliest_date=None):
     return np.random.choice(ecg_dates)
 
 
-def _field_to_index_from_map(value: str, follow_up_start: datetime.datetime, field_map: Dict[str, float] = {'F': 0, 'M': 1}) -> float:
+def _field_to_index_from_map(value: str, field_map: Dict[str, float] = {'F': 0, 'M': 1}) -> float:
     return field_map[value]
 
 
