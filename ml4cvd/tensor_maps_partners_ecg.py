bd900ff3a6d9caf65fca3e9501afbcdf0b419e00
StevieSong <steven.song98@gmail.com>
2020-06-08 11:04:41 -0400

    sex tmap cats

--- ml4cvd/tensor_maps_partners_ecg.py
+++ ml4cvd/tensor_maps_partners_ecg.py
@@ -430,7 +430,7 @@ TMAPS[task] = TensorMap(
     interpretation=Interpretation.CATEGORICAL,
     path_prefix=PARTNERS_PREFIX,
     tensor_from_file=make_partners_ecg_tensor(key="gender"),
-    channel_map={'female': 0, 'male': 1, 'other': 2},
+    channel_map={'female': 0, 'male': 1},
     time_series_limit=0,
     validator=validator_not_all_zero,
 )
