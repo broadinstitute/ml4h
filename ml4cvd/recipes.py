bf00e4d2bc71896627f1c2f527c1d05390a533d5
Samuel Friedman <lucidtronix@gmail.com>
2020-06-09 08:01:25 -0400

    merged

--- ml4cvd/recipes.py
+++ ml4cvd/recipes.py
@@ -243,7 +243,7 @@ def infer_multimodal_multitask(args):
         header = ['sample_id']
         if tsv_style_is_genetics:
             header = ['FID', 'IID']
-        for ot, otm in zip(reversed(args.output_tensors), reversed(args.tensor_maps_out)):
+        for ot, otm in zip(args.output_tensors, args.tensor_maps_out):
             logging.info(f"Got ot  {ot} and otm {otm}  ot and otm {otm.name} ot  and otm {otm.channel_map} channel_map and otm {otm.interpretation}.")
             if len(otm.shape) == 1 and otm.is_continuous():
                 header.extend([ot+'_prediction', ot+'_actual'])
@@ -262,7 +262,7 @@ def infer_multimodal_multitask(args):
                 next(generate_test)  # this prints end of epoch info
                 logging.info(f"Inference on {stats['count']} tensors finished. Inference TSV file at: {inference_tsv}")
                 break
-
+            logging.debug(f'input {input_data}')
             prediction = model.predict(input_data)
             if len(no_fail_tmaps_out) == 1:
                 prediction = [prediction]
