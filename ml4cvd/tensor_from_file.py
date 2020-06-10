fef061bfc71e9a59d06d34eacd61ff79fc21c068
Samwell Freeman <lucidtronix@gmail.com>
2020-06-10 08:09:28 -0400

    dicts

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -2067,19 +2067,19 @@ def random_text_window_tensor(text_file: str, window_size: int, one_hot: bool = 
                 tensor[i] = tm.channel_map[c]
         if tm.dependent_map is not None:
             for i, dm in enumerate(tm.dependent_map):
-                start_next_window = random_index+window_size
+                start_next_window = random_index+window_size+i
                 dependents[dm] = np.zeros(dm.shape, dtype=np.float32)
                 if dm.axes() == 1 and one_hot:
                     dependents[dm][dm.channel_map[text[start_next_window]]] = 1.0
                 elif dm.axes() == 2 or (not one_hot and dm.axes() == 1):
-                    for j, c in enumerate(text[i+start_next_window:i+start_next_window+dm.shape[0]]):
+                    for j, c in enumerate(text[start_next_window:start_next_window+dm.shape[0]]):
                         if one_hot:
                             dependents[dm][j, dm.channel_map[c]] = 1.0
                         else:
                             dependents[dm][j] = dm.channel_map[c]
                 else:
                     raise ValueError(f'No method to process dependent map:{dm.name} of shape {dm.shape}.')
-                logging.debug(f'{text[random_index:random_index+window_size]} \nand Dependent:{text[start_next_window:start_next_window+dm.shape[0]]}')
+                logging.debug(f'Input text: {text[random_index:random_index+window_size]}\n Dependent: {text[start_next_window:start_next_window+dm.shape[0]]}')
         return tensor
     return text_from_file
 
