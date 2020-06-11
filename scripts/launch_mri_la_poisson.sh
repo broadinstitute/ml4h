f88bcb2f849b0cfdabdf16bac3c94abea4dd1160
Paolo Di Achille <paolo.diachille@gmail.com>
2020-06-11 19:40:53 +0000

    Attempt

--- scripts/launch_mri_la_poisson.sh
+++ scripts/launch_mri_la_poisson.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/bash
 
-apt update; apt install git -y
-git clone --recursive git://github.com/mmolero/pypoisson.git
-cd pypoisson
-python setup.py build
-python setup.py install
+# apt update; apt install git -y
+# git clone --recursive git://github.com/mmolero/pypoisson.git
+# cd pypoisson
+# python setup.py build
+# python setup.py install
 
 mkdir -p /home/pdiachil/atria_poisson_output
 cd /home/pdiachil/atria_poisson_output
 cnt=0
-for i in {1..1200}
+for i in {0..5000}
 do
-    python /home/pdiachil/ml/scripts/mri_la_poisson.py $cnt &
-    python /home/pdiachil/ml/scripts/mri_la_poisson.py $(($cnt+1)) &
-    python /home/pdiachil/ml/scripts/mri_la_poisson.py $(($cnt+2)) &
-    python /home/pdiachil/ml/scripts/mri_la_poisson.py $(($cnt+3)) &
-    wait
-    cnt=$((cnt+4))
-    break
+    python /home/pdiachil/ml/scripts/mri_la_poisson.py $i 
 done    
 
 
