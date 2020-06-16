aa0aec6ba56cf24957e67f9e6909993c4604aad7
Samwell Freeman <lucidtronix@gmail.com>
2020-06-16 15:25:58 -0400

    caucasion

--- ml4cvd/tensor_maps_by_hand.py
+++ ml4cvd/tensor_maps_by_hand.py
@@ -674,7 +674,7 @@ TMAPS['genetic_caucasian'] = TensorMap('Genetic-ethnic-grouping_Caucasian_0_0', 
                                        channel_map={'no_caucasian': 0, 'Genetic-ethnic-grouping_Caucasian_0_0': 1})
 TMAPS['genetic_caucasian_weighted'] = TensorMap(
     'Genetic-ethnic-grouping_Caucasian_0_0', Interpretation.CATEGORICAL, path_prefix='categorical',
-    channel_map={'no_caucasian': 0, 'caucasian': 1}, loss=weighted_crossentropy([10.0, 1.0], 'caucasian_loss'),
+    channel_map={'no_caucasian': 0, 'Genetic-ethnic-grouping_Caucasian_0_0': 1}, loss=weighted_crossentropy([10.0, 1.0], 'caucasian_loss'),
 )
 
 TMAPS['mothers_age'] = TensorMap(
