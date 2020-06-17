b508ec1378d2edd245b0c2944b6b2a68e5f55dee
Samwell Freeman <lucidtronix@gmail.com>
2020-06-17 08:36:58 -0400

    strip

--- scripts/tensor_map_loop.sh
+++ scripts/tensor_map_loop.sh
@@ -5,15 +5,15 @@ output_folder="/home/sam/ml/trained_models/"
 model_files="--model_files"
 # Iterate the string variable using for loop
 for tm in $tensor_maps; do
-    echo ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-    --input_tensors ${tm} \
-    --output_tensors adjusted_myocardium_mass --training_steps 72 --validation_steps 36 --test_steps 36 --epochs 92 \
-    --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_amm \
-    --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.0001
+    /scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
+        --input_tensors ${tm} \
+        --output_tensors adjusted_myocardium_mass --training_steps 72 --validation_steps 36 --test_steps 36 --epochs 92 \
+        --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_amm \
+        --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.0001
     model_files="${model_files} ${output_folder}${tm}/${tm}.h5"
 done
-echo $model_files
-echo ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
+
+./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
     --input_tensors ${tensor_maps} \
     --output_tensors adjusted_myocardium_mass --test_steps 72 \
     --patience 32 --batch_size 32 --output_folder ${output_folder} --id compare_leads \
