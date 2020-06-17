1e8895ece708305accb3029164f7062dad330d86
Samwell Freeman <lucidtronix@gmail.com>
2020-06-17 16:18:42 -0400

    strip

--- ml4cvd/hyperparameters.py
+++ ml4cvd/hyperparameters.py
@@ -295,7 +295,7 @@ def optimize_lr_multimodal_multitask(args):
 
 def optimize_input_tensor_maps(args):
     input_tensor_map_sets = [['ecg_rest'], ['ecg_rest_raw'], ['ecg_rest_stft']]
-    space = {'input_tensors': hp.choice('input_tensor_maps', input_tensor_map_sets)}
+    space = {'input_tensors': hp.choice('input_tensors', input_tensor_map_sets)}
     param_lists = {'input_tensors': input_tensor_map_sets}
     hyperparameter_optimizer(args, space, param_lists)
 
--- scripts/tensor_map_loop.sh
+++ scripts/tensor_map_loop.sh
@@ -6,16 +6,16 @@ model_files="--model_files"
 # Iterate the string variable using for loop
 for tm in $tensor_maps; do
     ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-        --input_tensors ${tm} \
-        --output_tensors adjusted_myocardium_mass --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 132 \
-        --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_amm \
+        --input_tensors ${tm} age_2 sex bmi_21 \
+        --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 132 \
+        --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_age_sex_bmi_asym_amm \
         --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.0001
-    model_files="${model_files} ${output_folder}${tm}_amm/${tm}_amm.h5"
+    model_files="${model_files} ${output_folder}${tm}_age_sex_bmi_asym_amm/${tm}_age_sex_bmi_asym_amm.h5"
 done
 
 ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-    --input_tensors ${tensor_maps} \
-    --output_tensors adjusted_myocardium_mass --test_steps 92 \
+    --input_tensors ${tensor_maps} age_2 sex bmi_21 \
+    --output_tensors adjusted_myocardium_mass_asym_outlier --test_steps 92 \
     --patience 32 --batch_size 32 --output_folder ${output_folder} --id compare_leads \
     ${model_files} \
     --test_csv /home/sam/lvh_hold_out.txt
