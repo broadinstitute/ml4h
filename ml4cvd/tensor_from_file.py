2ab5a74ed9f2e12bc2982cd3c6191cfc0acb6458
Samwell Freeman <lucidtronix@gmail.com>
2020-06-17 07:21:00 -0400

    strip

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -1638,22 +1638,22 @@ TMAPS['cine_lax_4ch_192_16_4d'] = TensorMap(
 )
 TMAPS['cine_lax_2ch_192_16_3'] = TensorMap(
     'cine_segmented_lax_2ch', Interpretation.CONTINUOUS, shape=(192, 160, 16), path_prefix='ukb_cardiac_mri',
-    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_2ch', 192, 160, 3, pad_shape=(192, 160, 48)),
+    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_2ch', 0, 48, 3, pad_shape=(192, 160, 48)),
     normalization=ZeroMeanStd1(),
 )
 TMAPS['cine_lax_3ch_192_16_3'] = TensorMap(
     'cine_segmented_lax_3ch', Interpretation.CONTINUOUS, shape=(192, 160, 16), path_prefix='ukb_cardiac_mri',
-    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_3ch', 192, 160, 3, pad_shape=(192, 160, 48)),
+    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_3ch', 0, 48, 3, pad_shape=(192, 160, 48)),
     normalization=ZeroMeanStd1(),
 )
 TMAPS['cine_lax_4ch_192_16_3'] = TensorMap(
     'cine_segmented_lax_4ch', Interpretation.CONTINUOUS, shape=(192, 160, 16), path_prefix='ukb_cardiac_mri',
-    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_4ch', 192, 160, 3, pad_shape=(192, 160, 48)),
+    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_4ch', 0, 48, 3, pad_shape=(192, 160, 48)),
     normalization=ZeroMeanStd1(),
 )
 TMAPS['cine_lax_4ch_192_16_3_4d'] = TensorMap(
     'cine_segmented_lax_4ch', Interpretation.CONTINUOUS, shape=(192, 160, 16, 1), path_prefix='ukb_cardiac_mri',
-    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_4ch', 192, 160, 3, pad_shape=(192, 160, 48)),
+    tensor_from_file=_slice_subset_tensor('cine_segmented_lax_4ch', 0, 48, 3, pad_shape=(192, 160, 48)),
     normalization=ZeroMeanStd1(),
 )
 
