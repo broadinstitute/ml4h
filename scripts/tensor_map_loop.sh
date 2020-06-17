ad8ece4ce97f040f5397510ab5988d673689e35f
Samwell Freeman <lucidtronix@gmail.com>
2020-06-17 19:19:51 -0400

    strip

--- scripts/tensor_map_loop.sh
+++ scripts/tensor_map_loop.sh
@@ -7,15 +7,33 @@ model_files="--model_files"
 for tm in $tensor_maps; do
     ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
         --input_tensors ${tm} age_2 sex bmi_21 \
-        --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 132 \
+        --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 \
         --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_age_sex_bmi_asym_amm \
-        --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.0001
+        --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.00005
     model_files="${model_files} ${output_folder}${tm}_age_sex_bmi_asym_amm/${tm}_age_sex_bmi_asym_amm.h5"
 done
 
 ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
     --input_tensors ${tensor_maps} age_2 sex bmi_21 \
-    --output_tensors adjusted_myocardium_mass_asym_outlier --test_steps 92 \
-    --patience 32 --batch_size 32 --output_folder ${output_folder} --id compare_leads \
+    --output_tensors adjusted_myocardium_mass_asym_outlier --test_steps 128 \
+    --patience 32 --batch_size 32 --output_folder ${output_folder} --id compare_leads_asym \
+    ${model_files} \
+    --test_csv /home/sam/lvh_hold_out.txt
+
+model_files="--model_files"
+# Iterate the string variable using for loop
+for tm in $tensor_maps; do
+    ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
+        --input_tensors ${tm} age_2 sex bmi_21 \
+        --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 \
+        --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_age_sex_bmi_amm \
+        --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.00005
+    model_files="${model_files} ${output_folder}${tm}_age_sex_bmi_amm/${tm}_age_sex_bmi_amm.h5"
+done
+
+./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
+    --input_tensors ${tensor_maps} age_2 sex bmi_21 \
+    --output_tensors adjusted_myocardium_mass --test_steps 128 \
+    --patience 32 --batch_size 32 --output_folder ${output_folder} --id compare_leads_amm \
     ${model_files} \
     --test_csv /home/sam/lvh_hold_out.txt
