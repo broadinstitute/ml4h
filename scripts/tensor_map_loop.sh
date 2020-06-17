a7fdd9689a52611bdbfaacc70b255bd1853b24dc
Samwell Freeman <lucidtronix@gmail.com>
2020-06-17 08:46:07 -0400

    strip

--- scripts/tensor_map_loop.sh
+++ scripts/tensor_map_loop.sh
@@ -10,7 +10,7 @@ for tm in $tensor_maps; do
         --output_tensors adjusted_myocardium_mass --training_steps 72 --validation_steps 36 --test_steps 36 --epochs 92 \
         --patience 32 --batch_size 32 --output_folder ${output_folder} --id ${tm}_amm \
         --test_csv /home/sam/lvh_hold_out.txt --learning_rate 0.0001
-    model_files="${model_files} ${output_folder}${tm}/${tm}.h5"
+    model_files="${model_files} ${output_folder}${tm}_amm/${tm}_amm.h5"
 done
 
 ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
