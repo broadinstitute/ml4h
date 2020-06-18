2509aa0ccd96a1e933dfbaf9ed5bd6c21b85c965
Samwell Freeman <lucidtronix@gmail.com>
2020-06-18 09:04:50 -0400

    strip

--- scripts/tensor_map_loop.sh
+++ scripts/tensor_map_loop.sh
@@ -7,42 +7,23 @@ model_files="--model_files"
 learning_rate="0.0001"
 patience="16"
 # Iterate the string variable using for loop
-for tm in $tensor_maps; do
-    ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-        --input_tensors ${tm} \
-        --output_tensors adjusted_myocardium_mass \
-        --training_steps 96 --validation_steps 48 --test_steps 48 --epochs 96 --batch_size 32 \
-        --patience ${patience} --learning_rate ${learning_rate} \
-        --output_folder ${output_folder} --id ${tm}_amm \
-        --test_csv /home/sam/lvh_hold_out.txt
-    model_files="${model_files} ${output_folder}${tm}_amm/${tm}_amm.h5"
-done
-
-./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-    --input_tensors ${tensor_maps} \
-    --output_tensors adjusted_myocardium_mass --test_steps 128 \
-    --batch_size 32 --output_folder ${output_folder} --id compare_leads_amm \
-    ${model_files} \
-    --test_csv /home/sam/lvh_hold_out.txt
-
-model_files="--model_files"
-# Iterate the string variable using for loop
-for tm in $tensor_maps; do
-    ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-        --input_tensors ${tm} \
-        --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 \
-        --patience ${patience} --learning_rate ${learning_rate} \
-        --batch_size 32 --output_folder ${output_folder} --id ${tm}_asym \
-        --test_csv /home/sam/lvh_hold_out.txt
-    model_files="${model_files} ${output_folder}${tm}_asym/${tm}_asym.h5"
-done
-
-./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
-    --input_tensors ${tensor_maps} \
-    --output_tensors adjusted_myocardium_mass_asym_outlier --test_steps 128 \
-    --batch_size 32 --output_folder ${output_folder} --id compare_leads_asym \
-    ${model_files} \
-    --test_csv /home/sam/lvh_hold_out.txt
+#for tm in $tensor_maps; do
+#    ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
+#        --input_tensors ${tm} \
+#        --output_tensors adjusted_myocardium_mass \
+#        --training_steps 96 --validation_steps 48 --test_steps 48 --epochs 96 --batch_size 32 \
+#        --patience ${patience} --learning_rate ${learning_rate} \
+#        --output_folder ${output_folder} --id ${tm}_amm \
+#        --test_csv /home/sam/lvh_hold_out.txt
+#    model_files="${model_files} ${output_folder}${tm}_amm/${tm}_amm.h5"
+#done
+#
+#./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode compare_scalar --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
+#    --input_tensors ${tensor_maps} \
+#    --output_tensors adjusted_myocardium_mass --test_steps 128 \
+#    --batch_size 32 --output_folder ${output_folder} --id compare_leads_amm \
+#    ${model_files} \
+#    --test_csv /home/sam/lvh_hold_out.txt
 
 model_files="--model_files"
 # Iterate the string variable using for loop
