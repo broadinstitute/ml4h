8580ad0e1775ab5f29e8cf1cd9a1bf706b2906a0
StevieSong <steven.song98@gmail.com>
2020-06-18 17:01:33 -0400

    variable names

--- ml4cvd/tensor_generators.py
+++ ml4cvd/tensor_generators.py
@@ -789,10 +789,10 @@ def test_train_valid_tensor_generators(
         )
         weights = None
 
-    train_workers = int(training_steps / (training_steps + validation_steps) * num_workers) or (1 if num_workers else 0)
-    valid_workers = int(validation_steps / (training_steps + validation_steps) * num_workers) or (1 if num_workers else 0)
-    generate_train = TensorGenerator(batch_size, tensor_maps_in, tensor_maps_out, train_paths, train_workers, cache_size, weights, keep_paths, mixup_alpha, name='train_worker', siamese=siamese, augment=True, sample_weight=sample_weight)
-    generate_valid = TensorGenerator(batch_size, tensor_maps_in, tensor_maps_out, valid_paths, valid_workers, cache_size, weights, keep_paths, name='validation_worker', siamese=siamese, augment=False)
+    num_train_workers = int(training_steps / (training_steps + validation_steps) * num_workers) or (1 if num_workers else 0)
+    num_valid_workers = int(validation_steps / (training_steps + validation_steps) * num_workers) or (1 if num_workers else 0)
+    generate_train = TensorGenerator(batch_size, tensor_maps_in, tensor_maps_out, train_paths, num_train_workers, cache_size, weights, keep_paths, mixup_alpha, name='train_worker', siamese=siamese, augment=True, sample_weight=sample_weight)
+    generate_valid = TensorGenerator(batch_size, tensor_maps_in, tensor_maps_out, valid_paths, num_valid_workers, cache_size, weights, keep_paths, name='validation_worker', siamese=siamese, augment=False)
     generate_test = TensorGenerator(batch_size, tensor_maps_in, tensor_maps_out, test_paths, num_workers, 0, weights, keep_paths or keep_paths_test, name='test_worker', siamese=siamese, augment=False)
     return generate_train, generate_valid, generate_test
 
