659fbc6abfcaaa52293c5c92f68ba83d073dc39f
Samwell Freeman <lucidtronix@gmail.com>
2020-06-18 07:19:56 -0400

    strip

--- scripts/tensor_map_loop.sh
+++ scripts/tensor_map_loop.sh
@@ -4,13 +4,14 @@ tensor_maps="strip_I strip_II strip_III strip_V1 strip_V2 strip_V3 strip_V4 stri
 tensor_maps="strip_I strip_II strip_V5 ecg_rest_raw"
 output_folder="/home/sam/ml/trained_models/"
 model_files="--model_files"
+patience="12"
 # Iterate the string variable using for loop
 for tm in $tensor_maps; do
     ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
         --input_tensors ${tm} \
         --output_tensors adjusted_myocardium_mass \
-        --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 --batch_size 32 \
-        --patience 5 --learning_rate 0.0001 \
+        --training_steps 96 --validation_steps 48 --test_steps 48 --epochs 96 --batch_size 32 \
+        --patience ${patience} --learning_rate 0.0001 \
         --output_folder ${output_folder} --id ${tm}_amm \
         --test_csv /home/sam/lvh_hold_out.txt
     model_files="${model_files} ${output_folder}${tm}_amm/${tm}_amm.h5"
@@ -29,7 +30,7 @@ for tm in $tensor_maps; do
     ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
         --input_tensors ${tm} \
         --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 \
-        --patience 5 --learning_rate 0.0001 \
+        --patience ${patience} --learning_rate 0.0001 \
         --batch_size 32 --output_folder ${output_folder} --id ${tm}_asym \
         --test_csv /home/sam/lvh_hold_out.txt
     model_files="${model_files} ${output_folder}${tm}_asym/${tm}_asym.h5"
@@ -48,7 +49,7 @@ for tm in $tensor_maps; do
     ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
         --input_tensors ${tm} age_2 sex bmi_21 \
         --output_tensors adjusted_myocardium_mass_asym_outlier --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 \
-        --patience 5 --learning_rate 0.0001 \
+        --patience ${patience} --learning_rate 0.0001 \
         --batch_size 32 --output_folder ${output_folder} --id ${tm}_age_sex_bmi_asym_amm \
         --test_csv /home/sam/lvh_hold_out.txt
     model_files="${model_files} ${output_folder}${tm}_age_sex_bmi_asym_amm/${tm}_age_sex_bmi_asym_amm.h5"
@@ -67,7 +68,7 @@ for tm in $tensor_maps; do
     ./scripts/tf.sh /home/sam/ml/ml4cvd/recipes.py --mode train --tensors /mnt/disks/ecg-rest-38k-tensors/2020-03-14/ \
         --input_tensors ${tm} age_2 sex bmi_21 \
         --output_tensors adjusted_myocardium_mass --training_steps 92 --validation_steps 36 --test_steps 36 --epochs 96 \
-        --patience 5 --learning_rate 0.0001 \
+        --patience ${patience} --learning_rate 0.0001 \
         --batch_size 32 --output_folder ${output_folder} --id ${tm}_age_sex_bmi_amm \
         --test_csv /home/sam/lvh_hold_out.txt
     model_files="${model_files} ${output_folder}${tm}_age_sex_bmi_amm/${tm}_age_sex_bmi_amm.h5"
