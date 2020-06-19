96a3235747b192bc6b5e4215a96127394bf6128a
Samwell Freeman <lucidtronix@gmail.com>
2020-06-19 10:05:05 -0400

    merge master

--- ml4cvd/models.py
+++ ml4cvd/models.py
@@ -1098,7 +1098,7 @@ def _get_callbacks(
 ) -> List[Callback]:
     callbacks = [
         ModelCheckpoint(filepath=model_file, verbose=1, save_best_only=True),
-        EarlyStopping(monitor='val_loss', patience=patience * 2, verbose=1),
+        EarlyStopping(monitor='val_loss', patience=patience * 3, verbose=1),
         ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=patience, verbose=1),
     ]
     return callbacks
