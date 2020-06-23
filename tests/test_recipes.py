1d1a65c61a5090e31c9dece855155a62a3ae131d
ndiamant <ndiamant@broadinstitute.org>
2020-06-23 17:24:11 -0400

    updating tests

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -1,7 +1,11 @@
+import os
 import pytest
 import pandas as pd
+from typing import List
 
-from ml4cvd.defines import SAMPLE_ID
+from ml4cvd.defines import SAMPLE_ID, MODEL_EXT
+from ml4cvd.models import make_multimodal_multitask_model
+
 from ml4cvd.recipes import inference_file_name, hidden_inference_file_name
 from ml4cvd.recipes import train_multimodal_multitask, compare_multimodal_multitask_models
 from ml4cvd.recipes import infer_multimodal_multitask, infer_hidden_layer_multimodal_multitask
@@ -11,6 +15,16 @@ from ml4cvd.recipes import test_multimodal_multitask as tst_multimodal_multitask
 from ml4cvd.recipes import test_multimodal_scalar_tasks as tst_multimodal_scalar_tasks
 
 
+def init_models(args, num_models: int) -> List[str]:
+    paths = []
+    for i in range(num_models):
+        m = make_multimodal_multitask_model(**args)
+        path = os.path.join(args.output_folder, f'test_model_{i}{MODEL_EXT}')
+        m.save(path)
+        paths.append(path)
+    return paths
+
+
 class TestRecipes:
     """Smoke tests"""
 
@@ -18,19 +32,24 @@ class TestRecipes:
         train_multimodal_multitask(default_arguments)
 
     def test_test(self, default_arguments):
+        default_arguments.model_file = init_models(default_arguments, 1)[0]
         tst_multimodal_multitask(default_arguments)
 
     def test_test_scalar(self, default_arguments):
+        default_arguments.model_file = init_models(default_arguments, 1)[0]
         tst_multimodal_scalar_tasks(default_arguments)
 
     def test_infer(self, default_arguments):
-        # TODO: needs existing model files to test
+        default_arguments.model_files = init_models(default_arguments, 2)
+        default_arguments.model_ids = ['m0', 'm1']
         infer_multimodal_multitask(default_arguments)
         tsv = inference_file_name(default_arguments.output_folder, default_arguments.id)
         inferred = pd.read_csv(tsv, sep='\t')
         assert len(set(inferred[SAMPLE_ID])) == pytest.N_TENSORS
 
     def test_infer_genetics(self, default_arguments):
+        default_arguments.model_files = init_models(default_arguments, 2)
+        default_arguments.model_ids = ['m0', 'm1']
         default_arguments.tsv_style = 'genetics'
         infer_multimodal_multitask(default_arguments)
         default_arguments.tsv_style = 'standard'
@@ -39,13 +58,16 @@ class TestRecipes:
         assert len(set(inferred['FID'])) == pytest.N_TENSORS
 
     def test_infer_hidden(self, default_arguments):
-        # TODO: needs existing model files to test
+        default_arguments.model_files = init_models(default_arguments, 2)
+        default_arguments.model_ids = ['m0', 'm1']
         infer_hidden_layer_multimodal_multitask(default_arguments)
         tsv = hidden_inference_file_name(default_arguments.output_folder, default_arguments.id)
         inferred = pd.read_csv(tsv, sep='\t')
         assert len(set(inferred[SAMPLE_ID])) == pytest.N_TENSORS
 
     def test_infer_hidden_genetics(self, default_arguments):
+        default_arguments.model_files = init_models(default_arguments, 2)
+        default_arguments.model_ids = ['m0', 'm1']
         default_arguments.tsv_style = 'genetics'
         infer_hidden_layer_multimodal_multitask(default_arguments)
         default_arguments.tsv_style = 'standard'
