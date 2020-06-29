9474bcb42f358f0b74efdf8ca3177d0e01f8c44d
Samuel Friedman <lucidtronix@gmail.com>
2020-06-29 17:42:30 -0400

    sex

--- ml4cvd/tensor_generators.py
+++ ml4cvd/tensor_generators.py
@@ -567,7 +567,6 @@ def _sample_csv_to_set(sample_csv: Optional[str] = None) -> Union[None, Set[str]
     return set(sample_ids)
 
 
-
 def get_train_valid_test_paths(
         tensors: str,
         sample_csv: str,
@@ -617,7 +616,7 @@ def get_train_valid_test_paths(
 
     # parse csv's to disjoint sets, None if csv was None
     sample_set = _sample_csv_to_set(sample_csv)
-
+    logging.info(f'Sample set has {len(sample_set)}')
     train_set = _sample_csv_to_set(train_csv)
     valid_set = _sample_csv_to_set(valid_csv)
     test_set = _sample_csv_to_set(test_csv)
