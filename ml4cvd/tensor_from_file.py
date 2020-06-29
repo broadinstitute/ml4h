c3c239f536dcf44077ab8b2a61ed40f9628b9bb3
Samuel Friedman <lucidtronix@gmail.com>
2020-06-29 13:30:40 -0400

    sex

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -1903,21 +1903,31 @@ def sax_tensor(b_series_prefix):
         tensor = np.zeros(tm.shape, dtype=np.float32)
         if tm.dependent_map is not None:
             dependents[tm.dependent_map] = np.zeros(tm.dependent_map.shape, dtype=np.float32)
-        for b in range(tm.shape[-2]):
-            try:
-                tm_shape = (tm.shape[0], tm.shape[1])
-                tensor[:, :, b, 0] = _pad_or_crop_array_to_shape(tm_shape, np.array(hd5[f'{tm.path_prefix}/{b_series_prefix}_frame_b{b}/instance_0'], dtype=np.float32))
-                index_tensor = _pad_or_crop_array_to_shape(tm_shape, np.array(hd5[f'{tm.path_prefix}/{b_series_prefix}_mask_b{b}/instance_0'], dtype=np.float32))
-                if tm.dependent_map is not None:
-                    dependents[tm.dependent_map][:, :, b, :] = to_categorical(index_tensor, tm.dependent_map.shape[-1])
-            except KeyError:
-                missing += 1
-                tensor[:, :, b, 0] = 0
-                if tm.dependent_map is not None:
-                    dependents[tm.dependent_map][:, :, b, MRI_SEGMENTED_CHANNEL_MAP['background']] = 1
-        if missing == tm.shape[-2]:
-            raise ValueError(f'Could not find any slices in {tm.name} was hoping for {tm.shape[-2]} looked at: {tm.path_prefix}/{b_series_prefix}')
-        return tensor
+        if tm.axes() == 3:
+            for b in range(tm.shape[-1]):
+                try:
+                    tm_shape = (tm.shape[0], tm.shape[1])
+                    tensor[:, :, b] = _pad_or_crop_array_to_shape(tm_shape, np.array(hd5[f'{tm.path_prefix}/{b_series_prefix}_frame_b{b}/instance_0'], dtype=np.float32))
+                    index_tensor = _pad_or_crop_array_to_shape(tm_shape, np.array(hd5[f'{tm.path_prefix}/{b_series_prefix}_mask_b{b}/instance_0'], dtype=np.float32))
+                except KeyError:
+                    missing += 1
+                    tensor[:, :, b] = 0
+        else:
+            for b in range(tm.shape[-2]):
+                try:
+                    tm_shape = (tm.shape[0], tm.shape[1])
+                    tensor[:, :, b, 0] = _pad_or_crop_array_to_shape(tm_shape, np.array(hd5[f'{tm.path_prefix}/{b_series_prefix}_frame_b{b}/instance_0'], dtype=np.float32))
+                    index_tensor = _pad_or_crop_array_to_shape(tm_shape, np.array(hd5[f'{tm.path_prefix}/{b_series_prefix}_mask_b{b}/instance_0'], dtype=np.float32))
+                    if tm.dependent_map is not None:
+                        dependents[tm.dependent_map][:, :, b, :] = to_categorical(index_tensor, tm.dependent_map.shape[-1])
+                except KeyError:
+                    missing += 1
+                    tensor[:, :, b, 0] = 0
+                    if tm.dependent_map is not None:
+                        dependents[tm.dependent_map][:, :, b, MRI_SEGMENTED_CHANNEL_MAP['background']] = 1
+            if missing == tm.shape[-2]:
+                raise ValueError(f'Could not find any slices in {tm.name} was hoping for {tm.shape[-2]} looked at: {tm.path_prefix}/{b_series_prefix}')
+            return tensor
     return sax_tensor_from_file
 
 
