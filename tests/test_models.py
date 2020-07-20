1208a8a049b679e49debcac031ee105c11878367
Samwell Freeman <lucidtronix@gmail.com>
2020-07-20 09:31:16 -0400

    add test

--- tests/test_models.py
+++ tests/test_models.py
@@ -83,6 +83,30 @@ class TestMakeMultimodalMultitaskModel:
     @pytest.mark.parametrize(
         'input_output_tmaps',
         [
+            (LANGUAGE_TMAP_1HOT_WINDOW, LANGUAGE_TMAP_1HOT_SOFTMAX),
+        ],
+    )
+    def test_language_models(self, input_output_tmaps, tmpdir):
+        params = DEFAULT_PARAMS.copy()
+        m = make_multimodal_multitask_model(
+            input_output_tmaps[0],
+            input_output_tmaps[1],
+            **params
+        )
+        assert_model_trains(input_output_tmaps[0], input_output_tmaps[1], m)
+        m.save(os.path.join(tmpdir, 'lstm.h5'))
+        path = os.path.join(tmpdir, f'm{MODEL_EXT}')
+        m.save(path)
+        make_multimodal_multitask_model(
+            input_output_tmaps[0],
+            input_output_tmaps[1],
+            model_file=path,
+            **DEFAULT_PARAMS,
+        )
+        
+    @pytest.mark.parametrize(
+        'input_output_tmaps',
+        [
             (CONTINUOUS_TMAPS[:1], CONTINUOUS_TMAPS[1:2]), (CONTINUOUS_TMAPS[1:2], CONTINUOUS_TMAPS[:1]),
             (CONTINUOUS_TMAPS[:2], CONTINUOUS_TMAPS[:2]),
         ],
@@ -265,30 +289,6 @@ class TestMakeMultimodalMultitaskModel:
             **params,
         )
         assert_model_trains([SEGMENT_IN, TMAPS_UP_TO_4D[0]], [SEGMENT_OUT], m)
-
-    @pytest.mark.parametrize(
-        'input_output_tmaps',
-        [
-            (LANGUAGE_TMAP_1HOT_WINDOW, LANGUAGE_TMAP_1HOT_SOFTMAX),
-        ],
-    )
-    def test_language_models(self, input_output_tmaps, tmpdir):
-        params = DEFAULT_PARAMS.copy()
-        m = make_multimodal_multitask_model(
-            input_output_tmaps[0],
-            input_output_tmaps[1],
-            **params
-        )
-        assert_model_trains(input_output_tmaps[0], input_output_tmaps[1], m)
-        m.save(os.path.join(tmpdir, 'lstm.h5'))
-        path = os.path.join(tmpdir, f'm{MODEL_EXT}')
-        m.save(path)
-        make_multimodal_multitask_model(
-            input_output_tmaps[0],
-            input_output_tmaps[1],
-            model_file=path,
-            **DEFAULT_PARAMS,
-        )
 
     def test_no_dense_layers(self):
         params = DEFAULT_PARAMS.copy()
