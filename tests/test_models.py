d0bf636ee1f9a911721c09fe54194746d3e0d6f2
Samwell Freeman <lucidtronix@gmail.com>
2020-07-20 09:48:01 -0400

    add test

--- tests/test_models.py
+++ tests/test_models.py
@@ -89,12 +89,12 @@ class TestMakeMultimodalMultitaskModel:
     )
     def test_language_models(self, input_output_tmaps):
         params = DEFAULT_PARAMS.copy()
-    #     m = make_multimodal_multitask_model(
-    #         input_output_tmaps[0],
-    #         input_output_tmaps[1],
-    #         **params
-    #     )
-    #     assert_model_trains(input_output_tmaps[0], input_output_tmaps[1], m)
+        m = make_multimodal_multitask_model(
+            input_output_tmaps[0],
+            input_output_tmaps[1],
+            **params
+        )
+        assert_model_trains(input_output_tmaps[0], input_output_tmaps[1], m)
     #     m.save(os.path.join(tmpdir, 'lstm.h5'))
     #     path = os.path.join(tmpdir, f'm{MODEL_EXT}')
     #     m.save(path)
