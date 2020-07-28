e9fc4959fdec99a8d49ab8759c4e35dc18f35999
Paolo Di Achille <paolo.diachille@gmail.com>
2020-07-28 16:00:59 -0400

    Explore run on CCDS

--- launch_explore.sh
+++ launch_explore.sh
@@ -1,4 +1,4 @@
-/home/lia.harrington/ml4cvd/ml4cvd/recipes.py \
+/home/${USER}/ml4cvd/ml4cvd/recipes.py \
         --mode explore \
         --tensors ${SLURM_JOB_SCRATCHDIR}/mgh/ \
         --input_tensors partners_ecg_rate_pc \
@@ -7,5 +7,13 @@
                         partners_ecg_qrs_md \
                         partners_ecg_pr_pc \
                         partners_ecg_pr_md \
-        --output_folder /home/lia.harrington/explore_ecg_features/ \
+                        partners_ecg_qt_pc \
+                        partners_ecg_qt_md \
+                        partners_ecg_paxis_pc \
+                        partners_ecg_paxis_md \
+                        partners_ecg_raxis_pc \
+                        partners_ecg_raxis_md \
+                        partners_ecg_taxis_pc \
+                        partners_ecg_taxis_md \
+        --output_folder /home/${USER}/explore_ecg_features/ \
         --id explore_ecg_features
