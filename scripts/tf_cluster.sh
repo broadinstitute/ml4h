b3b1dace536e83305f8a3f34190ede6717042694
Paolo Di Achille <paolo.diachille@gmail.com>
2020-07-29 12:03:10 -0400

    Removed unnecessary semicolon

--- scripts/tf_cluster.sh
+++ scripts/tf_cluster.sh
@@ -115,7 +115,7 @@ LAUNCH_MESSAGE
 ## Download bucket
 cd $SLURM_JOB_SCRATCHDIR
 # s3cmd sync s3://${MOUNT_BUCKETS}/ ./
-for i in {1..9};
+for i in {1..9}
     s3cmd sync s3://${MOUNT_BUCKETS}/mgh_tar_$i.tar ./ &
 done 
 wait
