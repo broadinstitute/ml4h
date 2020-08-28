12ac930c9433e6881d51922baf74da0ee417cc99
Paolo Di Achille <paolo.diachille@gmail.com>
2020-08-28 14:51:01 +0000

    New compares

--- scripts/tf.sh
+++ scripts/tf.sh
@@ -139,4 +139,4 @@ ${GPU_DEVICE} \
 -v ${WORKDIR}/:${WORKDIR}/ \
 -v ${HOME}/:${HOME}/ \
 ${MOUNTS} \
-${DOCKER_IMAGE} /bin/bash -c "pip install ${WORKDIR}; cd ${WORKDIR}; mkdir -p atria; cd atria; ${PYTHON_COMMAND} ${PYTHON_ARGS}"
+${DOCKER_IMAGE} /bin/bash -c "pip install ${WORKDIR}; pip install opencv-python ; cd /home/pdiachil/pypoisson ; python setup.py install ; cd ${WORKDIR}/notebooks/mri; ${PYTHON_COMMAND} ${PYTHON_ARGS}"
