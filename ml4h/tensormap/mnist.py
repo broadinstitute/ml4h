c1427938bc89421f6d8eae7d8331fa66fd7e2854
Samwell Freeman <lucidtronix@gmail.com>
2020-09-09 05:53:09 -0400

    merge master

--- ml4h/tensormap/mnist.py
+++ ml4h/tensormap/mnist.py
@@ -1,22 +1,24 @@
 # MNIST Hand written digit tensor maps
 import os
+import sys
+import gzip
 import pickle
-import logging
-from typing import Dict, Tuple, Callable
+from typing import Dict
 
 import h5py
 import numpy as np
+
 from ml4h.TensorMap import TensorMap, Interpretation
 
 
-def mnist_image_from_hd5(tm, hd5, dependents={}):
+def mnist_image_from_hd5(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
     return np.array(hd5['mnist_image'])
 
 
 mnist_image = TensorMap('mnist_image', shape=(28, 28, 1), tensor_from_file=mnist_image_from_hd5)
 
 
-def mnist_label_from_hd5(tm, hd5, dependents={}):
+def mnist_label_from_hd5(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
     one_hot = np.zeros(tm.shape, dtype=np.float32)
     one_hot[int(hd5['mnist_label'][0])] = 1.0
     return one_hot
@@ -26,7 +28,7 @@ mnist_label = TensorMap('mnist_label', Interpretation.CATEGORICAL, tensor_from_f
                         channel_map={f'digit_{i}': i for i in range(10)})
 
 
-def mnist_label_as_time_to_event(tm, hd5, dependents={}):
+def mnist_label_as_time_to_event(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
     tensor = np.zeros(tm.shape, dtype=np.float32)
     label = float(hd5['mnist_label'][0])
     tensor[0] = 1.0 if np.random.rand() > (label / 10) else 0.0
@@ -38,7 +40,7 @@ mnist_time_to_event = TensorMap('mnist_time_to_event', Interpretation.TIME_TO_EV
                                 tensor_from_file=mnist_label_as_time_to_event)
 
 
-def mnist_label_as_survival_curve(tm, hd5, dependents={}):
+def mnist_label_as_survival_curve(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
     label = float(hd5['mnist_label'][0])
     has_disease = 1.0 if np.random.rand() > (label / 10) else 0.0
     days_follow_up = np.random.randint(1, 3650)
@@ -62,9 +64,9 @@ def mnist_as_hd5(hd5_folder):
     mnist_images = train[0].reshape((-1, 28, 28, 1))
     if not os.path.exists(hd5_folder):
         os.makedirs(hd5_folder)
-    for i, mnist_image in enumerate(mnist_images):
+    for i, mnist_example in enumerate(mnist_images):
         with h5py.File(os.path.join(hd5_folder, f'{i}.hd5'), 'w') as hd5:
-            hd5.create_dataset('mnist_image', data=mnist_image)
+            hd5.create_dataset('mnist_image', data=mnist_example)
             hd5.create_dataset('mnist_label', data=[train[1][i]])
         if (i+1) % 5000 == 0:
             print(f'Wrote {i+1} MNIST images and labels as HD5 files')
