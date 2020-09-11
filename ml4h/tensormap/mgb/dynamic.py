fedda1505cb04de95547b1659871a3d0060d9558
Samuel Friedman <lucidtronix@gmail.com>
2020-09-11 10:16:05 -0400

    old wide

--- ml4h/tensormap/mgb/dynamic.py
+++ ml4h/tensormap/mgb/dynamic.py
@@ -24,6 +24,7 @@ INCIDENCE_CSV = '/media/erisone_snf13/lc_outcomes.csv'
 CARDIAC_SURGERY_OUTCOMES_CSV = '/data/sts-data/mgh-preop-ecg-outcome-labels.csv'
 PARTNERS_PREFIX = 'partners_ecg_rest'
 WIDE_FILE = '/home/sam/ml/hf-wide-2020-08-18-with-lvh-and-lbbb.tsv'
+WIDE_FILE = '/home/sam/ml/mgh-wide-2020-06-25-with-mrn.tsv'
 
 
 def make_mgb_dynamic_tensor_maps(desired_map_name: str) -> TensorMap:
@@ -562,8 +563,11 @@ def tensor_from_wide(
             try:
                 patient_key = int(float(row[patient_index]))
                 patient_data[patient_key] = {
-                    'age': _days_to_years_float(row[age_index]), 'bmi': _to_float_or_none(row[bmi_index]), 'sex': row[sex_index],
-                    'hf_age': _days_to_years_float(row[hf_index]), 'end_age': _days_to_years_float(row[end_index]),
+                    'age': _to_float_or_none(row[age_index]), #_days_to_years_float(row[age_index]),
+                    'bmi': _to_float_or_none(row[bmi_index]),
+                    'sex': row[sex_index],
+                    'hf_age': _to_float_or_none(row[hf_index]), #_days_to_years_float(row[hf_index]),
+                    'end_age': _to_float_or_none(row[end_index]), #_days_to_years_float(row[end_index]),
                     'start_date': datetime.datetime.strptime(row[start_index], CARDIAC_SURGERY_DATE_FORMAT),
                 }
 
