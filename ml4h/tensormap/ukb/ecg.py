ce4e3c697d17759020f59dac2eb86c0e09c500f8
Paolo Di Achille <paolo.diachille@gmail.com>
2020-09-15 20:41:33 +0000

    Moving ECG interval tmaps to continuous

--- ml4h/tensormap/ukb/ecg.py
+++ ml4h/tensormap/ukb/ecg.py
@@ -866,7 +866,7 @@ qrs_num = TensorMap(
     normalization={'mean': 9.61, 'std': 1.64},
 )
 qt_interval = TensorMap(
-    'QTInterval', Interpretation.CONTINUOUS, path_prefix='ukb_ecg_rest', channel_map={'QTInterval': 0}, loss='logcosh', validator=make_range_validator(300, 600),
+    'QTInterval', Interpretation.CONTINUOUS, path_prefix='continuous', channel_map={'QTInterval': 0}, loss='logcosh', validator=make_range_validator(300, 600),
     normalization={'mean': 426.1, 'std': 32.24},
 )
 qt_interval_quintiles = TensorMap(
