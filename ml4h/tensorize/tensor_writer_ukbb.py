dc2b8bdbabb9b1114199e70d6b94d0945a0b26af
Samwell Freeman <lucidtronix@gmail.com>
2020-09-21 16:44:17 -0400

    instance

--- ml4h/tensorize/tensor_writer_ukbb.py
+++ ml4h/tensorize/tensor_writer_ukbb.py
@@ -697,11 +697,9 @@ def create_tensor_in_hd5(
 ):
     hd5_path = tensor_path(path_prefix, name)
     if instance is not None:
-        hd5_path = f'{hd5_path}instance_{instance}'
+        hd5_path = f'{hd5_path}instance_{instance}/'
 
-    if hd5_path in hd5 and 'instance' in hd5_path:
-        hd5_path = f'{hd5_path}/{len(hd5[hd5_path])}'
-    elif hd5_path in hd5:
+    if hd5_path in hd5:
         hd5_path = f'{hd5_path}instance_{len(hd5[hd5_path])}'
     else:
         hd5_path = f'{hd5_path}instance_0'
