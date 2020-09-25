df425a79ad060962bc2286c203f8b466fa7feb73
ndiamant <ndiamant@broadinstitute.org>
2020-09-25 13:18:14 -0400

    compare compression schemes

--- benchmarks/data.py
+++ benchmarks/data.py
@@ -57,21 +57,21 @@ def get_hd5_paths(overwrite: bool, num_hd5s: int) -> List[str]:
 
 def write_in_hd5_ukbb(
     name: str, storage_type: StorageType, value: np.ndarray, hd5: h5py.File,
+    compression: str,
 ):
     """Replicates storage behavior in tensor_writer_ukbb"""
     if storage_type == StorageType.STRING:
         hd5.create_dataset(name, data=value, dtype=h5py.special_dtype(vlen=str))
     elif storage_type == StorageType.CONTINUOUS:
-        hd5.create_dataset(name, data=value, compression='gzip')
-        # TODO: try some different compression schemes
+        hd5.create_dataset(name, data=value, compression=compression)
     else:
         raise NotImplementedError(f'{storage_type} cannot be automatically written yet')
 
 
 def build_hd5s_ukbb(
         data_descriptions: List[Tuple[str, Shape, StorageType]], num_hd5s: int,
-        overwrite: bool = True,
-) -> List[str]:
+        overwrite: bool = True, compression: str = 'gzip',
+):
     paths = get_hd5_paths(overwrite, num_hd5s)
     start_time = time.time()
     print(f'Beginning to write {len(paths)} hd5s')
@@ -79,12 +79,11 @@ def build_hd5s_ukbb(
         with h5py.File(path, 'w') as hd5:
             for name, shape, storage_type in data_descriptions:
                 data = build_example(shape, storage_type)
-                write_in_hd5_ukbb(name, storage_type, data, hd5)
+                write_in_hd5_ukbb(name, storage_type, data, hd5, compression)
         print(f'{(i + 1) / len(paths):.1%} done', end='\r')
     print()
     delta = time.time() - start_time
     print(f'Wrote {len(paths)} hd5s in {delta:.1f} seconds at {len(paths) / delta:.1f} paths/s')
-    return paths
 
 
 STORAGE_TYPE_TO_INTERPRETATION = {
