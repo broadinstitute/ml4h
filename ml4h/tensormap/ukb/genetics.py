1574102fcd5048856955b7105069f5dccf49cf0f
Samwell Freeman <lucidtronix@gmail.com>
2020-10-07 13:43:06 -0400

    paired

--- ml4h/tensormap/ukb/genetics.py
+++ ml4h/tensormap/ukb/genetics.py
@@ -22,8 +22,8 @@ ttn_lof = TensorMap('TTN', Interpretation.CATEGORICAL, channel_map={'no_ttn_lof'
 def _ttn_tensor_from_file(tm, hd5, dependents={}):
     index = 0
     categorical_data = np.zeros(tm.shape, dtype=np.float32)
-    if 'has_exome' not in hd5['categorical']:
-        raise ValueError('Skipping people without exome sequencing.')
+    # if 'has_exome' not in hd5['categorical']:
+    #     raise ValueError('Skipping people without exome sequencing.')
     if tm.name in hd5['categorical'] and int(hd5['categorical'][tm.name][0]) != 0:
         index = 1
     categorical_data[index] = 1.0
