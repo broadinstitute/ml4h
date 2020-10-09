32ae73acab3b96f8a4b446924f022715ff3d9c51
Samwell Freeman <lucidtronix@gmail.com>
2020-10-09 08:21:11 -0400

    paired

--- ml4h/tensormap/ukb/genetics.py
+++ ml4h/tensormap/ukb/genetics.py
@@ -21,17 +21,17 @@ ttn_lof = TensorMap('TTN', Interpretation.CATEGORICAL, channel_map={'no_ttn_lof'
 
 
 def _ttn_tensor_from_file(tm, hd5, dependents={}):
-    index = 1
+    index = 0
     categorical_data = np.zeros(tm.shape, dtype=np.float32)
     if tm.name in hd5['categorical'] and int(hd5['categorical'][tm.name][0]) != 0:
-        index = 0
+        index = 1
     categorical_data[index] = 1.0
     return categorical_data
 
 
 has_ttntv = TensorMap(
-    'has_ttntv',  Interpretation.CATEGORICAL,
-    channel_map={'has_ttntv': 0, 'no_ttntv': 1}, tensor_from_file=_ttn_tensor_from_file,
+    'has_ttntv',  Interpretation.CATEGORICAL, path_prefix='categorical',
+    channel_map={'no_ttntv': 0, 'has_ttntv': 1}, #tensor_from_file=_ttn_tensor_from_file,
 )
 ttntv_10x = TensorMap(
     'has_ttntv',  Interpretation.CATEGORICAL, channel_map={
