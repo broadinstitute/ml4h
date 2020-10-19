d3989243801cb4a8217c7f63b7abfbe4ec31bd22
ndiamant <ndiamant@broadinstitute.org>
2020-10-19 12:28:58 -0400

    hangar with pytorch and tf data

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import time
+# import zarr
+import shutil
 import datetime
 import pandas as pd
 import seaborn as sns
+import tensorflow as tf
 from itertools import product
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
@@ -14,6 +17,10 @@ from contextlib import contextmanager
 from multiprocessing import cpu_count
 
 from data import build_tensor_maps, build_hd5s_ukbb, get_hd5_paths, DataDescription, SYNTHETIC_DATA_PATH
+# from data import ZarrGenerator, build_zarr, build_zarr_giant_array
+# from data import tf_data_zarr, ZarrSequence, _ZarrMap
+from data import build_hangar_repo, make_hangar_tf_dset
+from data import make_hangar_torch_dset
 
 
 DELTA_COL = 'step_delta_seconds'
@@ -68,7 +75,138 @@ class TensorGeneratorFactory(GeneratorFactory):
         del gen
 
 
+class ZarrFactory(GeneratorFactory):
+    def __init__(self, store_type, store_name: str, giant: bool):
+        self.num_samples = None
+        self.data_descriptions = None
+        self.store_name = store_name
+        self.store_type = store_type
+        self.giant = giant
+
+    def get_name(self) -> str:
+        return f'zarr_{self.store_name}{"_giant" if self.giant else ""}'
+
+    def setup(self, num_samples: int, data_descriptions: List[DataDescription]):
+        if self.giant:
+            build_zarr_giant_array(data_descriptions, num_samples, self.store_type)
+        else:
+            build_zarr(data_descriptions, num_samples, self.store_type)
+        self.num_samples = num_samples
+        self.data_descriptions = data_descriptions
+
+    @contextmanager
+    def __call__(self, batch_size: int, num_workers: int) -> Generator:
+        gen = ZarrGenerator(
+            self.num_samples,
+            self.data_descriptions,
+            num_workers,
+            batch_size,
+            self.giant,
+            self.store_type,
+        )
+        yield gen
+        gen.kill()
+
+
+class TFFactory(GeneratorFactory):
+    def __init__(self, store_type, store_name: str):
+        self.num_samples = None
+        self.data_descriptions = None
+        self.store_name = store_name
+        self.store_type = store_type
+
+    def get_name(self) -> str:
+        return f'tf_data_zarr_{self.store_name}'
+
+    def setup(self, num_samples: int, data_descriptions: List[DataDescription]):
+        build_zarr(data_descriptions, num_samples, self.store_type)
+        self.num_samples = num_samples
+        self.data_descriptions = data_descriptions
+
+    @contextmanager
+    def __call__(self, batch_size: int, num_workers: int) -> Generator:
+        yield tf_data_zarr(self.num_samples, self.data_descriptions, num_workers, batch_size, self.store_type)
+
+
+class ZarrSequenceFactory(GeneratorFactory):
+    def __init__(self, store_type, store_name: str):
+        self.num_samples = None
+        self.data_descriptions = None
+        self.store_name = store_name
+        self.store_type = store_type
+
+    def get_name(self) -> str:
+        return f'keras_sequence_zarr_{self.store_name}'
+
+    def setup(self, num_samples: int, data_descriptions: List[DataDescription]):
+        build_zarr_giant_array(data_descriptions, num_samples, self.store_type)
+        self.num_samples = num_samples
+        self.data_descriptions = data_descriptions
+
+    @contextmanager
+    def __call__(self, batch_size: int, num_workers: int) -> Generator:
+        sequence = ZarrSequence(self.num_samples, self.data_descriptions, batch_size, self.store_type)
+        enq = tf.keras.utils.OrderedEnqueuer(sequence, use_multiprocessing=True, shuffle=True)
+        enq.start(workers=num_workers)
+        gen = enq.get()
+        yield gen
+        enq.stop()
+
+
+class TFMultiMapFactory(GeneratorFactory):
+    def __init__(self, store_type, store_name: str):
+        self.num_samples = None
+        self.data_descriptions = None
+        self.store_name = store_name
+        self.store_type = store_type
+
+    def get_name(self) -> str:
+        return f'pyfunc_tf_map_zarr_{self.store_name}'
+
+    def setup(self, num_samples: int, data_descriptions: List[DataDescription]):
+        build_zarr_giant_array(data_descriptions, num_samples, self.store_type)
+        self.num_samples = num_samples
+        self.data_descriptions = data_descriptions
+
+    @contextmanager
+    def __call__(self, batch_size: int, num_workers: int) -> Generator:
+        zmap = _ZarrMap(self.num_samples, self.data_descriptions, self.store_type)
+        dset = zmap.build_dataset(num_workers, batch_size)
+        yield dset
+        zmap.close()
+
+
+class HangarFactory(GeneratorFactory):
+    def __init__(self, torch: bool):
+        self.torch = torch
+
+    def get_name(self) -> str:
+        return f'hangar_{"torch" if self.torch else "tf_dataset"}'
+
+    def setup(self, num_samples: int, data_descriptions: List[DataDescription]):
+        build_hangar_repo(num_samples, data_descriptions)
+        self.data_descriptions = data_descriptions
+
+    @contextmanager
+    def __call__(self, batch_size: int, num_workers: int) -> Generator:
+        if self.torch:
+            with make_hangar_torch_dset(self.data_descriptions, batch_size, num_workers) as dset:
+                yield dset
+        else:
+            with make_hangar_tf_dset(self.data_descriptions, batch_size) as dset:
+                yield dset
+
+
 FACTORIES = [
+    # ZarrFactory(zarr.DirectoryStore, 'directory_store', True),
+    # ZarrFactory(zarr.LMDBStore, 'lmdb_store', True),
+    # ZarrFactory(zarr.DirectoryStore, 'directory_store', False),
+    # ZarrFactory(zarr.LMDBStore, 'lmdb_store', False),
+    # TFFactory(zarr.DirectoryStore, 'directory_store'),
+    # ZarrSequenceFactory(zarr.DirectoryStore, 'directory_store'),
+    # TFMultiMapFactory(zarr.DirectoryStore, 'directory_store'),
+    HangarFactory(False),
+    HangarFactory(True),
     TensorGeneratorFactory('gzip'),
 ]
 
@@ -118,14 +256,19 @@ class Benchmark:
     def run(self, factories: List[GeneratorFactory]) -> pd.DataFrame:
         performance_dfs = []
         for factory in factories:
-            name = factory.get_name()
-            print(f'------------ {name} ------------')
-            factory.setup(self.num_samples, self.data_descriptions)
-            performance_df = benchmark_generator_factory(
-                factory, self.batch_sizes, self.num_workers, self.num_samples,
-            )
-            performance_df[NAME_COL] = name
-            performance_dfs.append(performance_df)
+            os.makedirs(SYNTHETIC_DATA_PATH, exist_ok=True)
+            try:
+                name = factory.get_name()
+                print(f'------------ {name} ------------')
+                factory.setup(self.num_samples, self.data_descriptions)
+                performance_df = benchmark_generator_factory(
+                    factory, self.batch_sizes, self.num_workers, self.num_samples,
+                )
+                performance_df[NAME_COL] = name
+                performance_dfs.append(performance_df)
+            finally:
+                print('Emptying synthetic data')
+                shutil.rmtree(SYNTHETIC_DATA_PATH)
         return pd.concat(performance_dfs)
 
 
@@ -139,9 +282,9 @@ ECG_BENCHMARK = Benchmark(
 MRI_3D_BENCHMARK = Benchmark(
     [
         ('mri', (256, 160, 16), StorageType.CONTINUOUS),
-        ('segmentation', (256, 160, 16, 13), StorageType.CONTINUOUS),
+        ('segmentation', (256, 160, 333), StorageType.CONTINUOUS),
     ],
-    num_samples=256, batch_sizes=[4, 8, 16], num_workers=[1, 2, 4, 8],
+    num_samples=256, batch_sizes=[4, 8, 16], num_workers=[1, 2, 4],
 )
 MRI_4D_BENCHMARK = Benchmark(
     [
@@ -151,10 +294,7 @@ MRI_4D_BENCHMARK = Benchmark(
     num_samples=256, batch_sizes=[4, 8, 16], num_workers=[1, 2, 4, 8],
 )
 ECG_MULTITASK_BENCHMARK = Benchmark(
-    [
-        [('ecg', (5000, 12), StorageType.CONTINUOUS)]
-        + [(f'interval_{i}', (1,), StorageType.CONTINUOUS) for i in range(20)],
-    ],
+    [('ecg', (5000, 12), StorageType.CONTINUOUS)] + [(f'interval_{i}', (1,), StorageType.CONTINUOUS) for i in range(20)],
     num_samples=4096, batch_sizes=[64, 128, 256], num_workers=[1, 2, 4, 8],
 )
 TEST_BENCHMARK = Benchmark(
@@ -164,20 +304,17 @@ TEST_BENCHMARK = Benchmark(
     num_samples=16, batch_sizes=[1, 2], num_workers=[1, 2, 4],
 )
 BENCHMARKS = {
-    'test': TEST_BENCHMARK,
-    'ecg_single_task': ECG_BENCHMARK,
     'mri_3d': MRI_3D_BENCHMARK,
-    'mri_4d': MRI_4D_BENCHMARK,
     'ecg_multi_task': ECG_MULTITASK_BENCHMARK,
+    'ecg_single_task': ECG_BENCHMARK,
 }
 
 
 def plot_benchmark(performance_df: pd.DataFrame, save_path: str, benchmark_name: str):
-    performance_df['samples / sec'] = 1 / performance_df[DELTA_COL] * performance_df[BATCH_SIZE_COL]
     plt.figure(figsize=(performance_df[BATCH_SIZE_COL].nunique() * 6, 6))
     sns.catplot(
-        data=performance_df, kind='point',
-        hue=NAME_COL, y='samples / sec', x=WORKER_COL, col=BATCH_SIZE_COL,
+        data=performance_df, kind='bar',
+        hue=NAME_COL, y=DELTA_COL, x=WORKER_COL, col=BATCH_SIZE_COL,
     )
     plt.suptitle(f'Benchmark: {benchmark_name}', weight='bold', size='large', y=1.05)
     print(f'Saving figure to {save_path}')
