9dd3f15c2a417a6537e8356349f03d32e46f7db3
Samwell Freeman <lucidtronix@gmail.com>
2020-10-21 09:53:33 -0400

    test

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -120,6 +120,17 @@ def _mask_subset_tensor(tensor_key, start, stop, step=1, pad_shape=None):
     return mask_subset_from_file
 
 
+def _gaussian_noise(img, mean=0, sigma=0.03):
+    img = img.copy()
+    noise = np.random.normal(mean, sigma, img.shape)
+    # mask_overflow_upper = img+noise >= 1.0
+    # mask_overflow_lower = img+noise < 0
+    # noise[mask_overflow_upper] = 1.0
+    # noise[mask_overflow_lower] = 0
+    img += noise
+    return img
+
+
 def _combined_subset_tensor(
     tensor_keys,
     start,
@@ -1329,6 +1340,11 @@ sax_slice_jamesp = TensorMap(
     'sax_slice_jamesp', shape=(224, 224, 1), normalization=ZeroMeanStd1(),
     tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_jamesp_annotated_', sax_series=True),
 )
+sax_slice_jamesp_gauss = TensorMap(
+    'sax_slice_jamesp', shape=(224, 224, 1), normalization=ZeroMeanStd1(), augmentations=[_gaussian_noise],
+    tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_jamesp_annotated_', sax_series=True),
+)
+
 
 
 def _segmented_dicom_slice(dicom_key_prefix, path_prefix='ukb_cardiac_mri', max_slices=50, sax_series=False):
