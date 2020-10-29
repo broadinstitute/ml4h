519dc4341bfb3af1ce768835f722a978c156eacf
Samwell Freeman <lucidtronix@gmail.com>
2020-10-29 16:42:54 -0400

    test

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1417,12 +1417,12 @@ sax_slices_both = TensorMap(
     'sax_slices_both', shape=(224, 224, 5), normalization=ZeroMeanStd1(),
     tensor_from_file=_slices_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True),
 )
-sax_slices_both_192 = TensorMap(
+sax_slices_both_5_frame = TensorMap(
     'sax_slices_both_192', shape=(192, 192, 5), normalization=ZeroMeanStd1(),
     tensor_from_file=_slices_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True, steps=4),
 )
-sax_slices_both_192_16_frame = TensorMap(
-    'sax_slices_both_192_16_frame', shape=(192, 192, 16), normalization=ZeroMeanStd1(),
+sax_slices_both_16_frame = TensorMap(
+    'sax_slices_both_16_frame', shape=(224, 224, 16), normalization=ZeroMeanStd1(),
     tensor_from_file=_slices_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True, steps=3),
 )
 sax_slices_both_gauss = TensorMap(
