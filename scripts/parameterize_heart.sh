6f06a61131be74e6e416895937ca2711771c1ab3
Paolo Di Achille <paolo.diachille@gmail.com>
2020-11-03 16:53:31 +0000

    Script

--- scripts/parameterize_heart.sh
+++ scripts/parameterize_heart.sh
@@ -4,18 +4,21 @@ VMTAG=$1
 STEP=$2
 
 cd /home/pdiachil/ml
+git checkout pd_atria
+git pull
+
 sudo mount -o norecovery,discard,defaults /dev/sdb /mnt/disks/segmented-sax-lax-v20200901/
 
 cnt1=$((VMTAG*STEP))
 cnt2=$((VMTAG*STEP+STEP))
 
-/home/pdiachil/ml/scripts/tf.sh -c /home/pdiachil/ml/notebooks/mri/parameterize_heart_geom.py $cnt1 $cnt2
+/home/pdiachil/ml/scripts/tf.sh -c /home/pdiachil/ml/notebooks/mri/parameterize_rv_geom.py $cnt1 $cnt2
 
 cd /home/pdiachil/ml/notebooks/mri
-/snap/bin/gsutil cp *processed* gs://ml4cvd/pdiachil/leftheart_boundary_v20201006/
+/snap/bin/gsutil cp *processed* gs://ml4cvd/pdiachil/rightheart_boundary_v20201102/
 
 cd /home/pdiachil/projects/chambers
-/snap/bin/gsutil cp poisson* gs://ml4cvd/pdiachil/leftheart_boundary_xdmf_v20201006/
-/snap/bin/gsutil cp /home/pdiachil/out* gs://ml4cvd/pdiachil/leftheart_boundary_logs/
+/snap/bin/gsutil cp poisson* gs://ml4cvd/pdiachil/rightheart_boundary_xdmf_v20201102/
+/snap/bin/gsutil cp /home/pdiachil/out* gs://ml4cvd/pdiachil/rightheart_boundary_logs/
 
 yes | /snap/bin/gcloud compute instances delete $(hostname) --zone ${gcp_zone}
