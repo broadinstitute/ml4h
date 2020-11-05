1fe1f60625c93895fe0364a1c1d87f787a216325
Samwell Freeman <lucidtronix@gmail.com>
2020-11-05 08:04:59 -0500

    b window

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1384,6 +1384,18 @@ sax_slice_both = TensorMap(
     'sax_slice_both', shape=(224, 224, 1), normalization=ZeroMeanStd1(),
     tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True),
 )
+sax_slice_both_sharpen = TensorMap(
+    'sax_slice_both', shape=(224, 224, 1), normalization=ZeroMeanStd1(), augmentations=[_sharpen],
+    tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True),
+)
+sax_slice_both_median = TensorMap(
+    'sax_slice_both', shape=(224, 224, 1), normalization=ZeroMeanStd1(), augmentations=[_median_filter],
+    tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True),
+)
+sax_slice_both_median = TensorMap(
+    'sax_slice_both', shape=(224, 224, 1), normalization=ZeroMeanStd1(), augmentations=[_gaussian_noise],
+    tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True),
+)
 sax_slice_both_all = TensorMap(
     'sax_slice_both', shape=(224, 224, 1), normalization=ZeroMeanStd1(), augmentations=[_sharpen, _gaussian_noise, _median_filter],
     tensor_from_file=_slice_tensor_with_segmentation('cine_segmented_sax_b*/2/instance_0', 'cine_segmented_sax_b*_both_annotated_', sax_series=True),
