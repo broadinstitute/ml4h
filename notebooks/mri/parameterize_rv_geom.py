743f5c759e76d371ee895481ddd05be22767b598
Paolo Di Achille <paolo.diachille@gmail.com>
2020-11-06 00:34:04 +0000

    no images

--- notebooks/mri/parameterize_rv_geom.py
+++ notebooks/mri/parameterize_rv_geom.py
@@ -23,7 +23,7 @@ hd5s = glob.glob('/mnt/disks/segmented-sax-lax-v20201102/2020-11-02/*.hd5')
 start = int(sys.argv[1])
 end = int(sys.argv[2])
 
-# start = 2032446
+# start = 4
 # end = start+1
 version='v20201102'
 # hd5s = ['/mnt/disks/segmented-sax-lax-v20200901/2020-11-02/2032446.hd5']
@@ -98,16 +98,16 @@ for i, hd5 in enumerate(sorted(hd5s)):
                         save_path=None, order='F',
                     )[0],
                 )
-                orig_datasets.append(
-                    _mri_hd5_to_structured_grids(
-                        ff_trad, view_format_string.format(view=view),
-                        view_name=view_format_string.format(view=view),
-                        concatenate=False, annotation=False,
-                        save_path=None, order='F',
-                    )[0],
-                )
-                to_xdmf(annot_datasets[-1], f'{sample_id}_{view}_annotated', squash=True)
-                to_xdmf(orig_datasets[-1], f'{sample_id}_{view}_original', squash=True)
+                # orig_datasets.append(
+                #     _mri_hd5_to_structured_grids(
+                #         ff_trad, view_format_string.format(view=view),
+                #         view_name=view_format_string.format(view=view),
+                #         concatenate=False, annotation=False,
+                #         save_path=None, order='F',
+                #     )[0],
+                # )
+                # to_xdmf(annot_datasets[-1], f'{sample_id}_{view}_annotated', squash=True)
+                # to_xdmf(orig_datasets[-1], f'{sample_id}_{view}_original', squash=True)
 
       # except:
       #     pass
