c8f8943216a0ef44d958d220e56bfe4a98c7c124
Paolo Di Achille <paolo.diachille@gmail.com>
2020-11-10 16:06:58 +0000

    IOU

--- notebooks/genetics/outcome_association.py
+++ notebooks/genetics/outcome_association.py
@@ -45,7 +45,7 @@ diseases = pd.read_csv('bq_diseases.tsv', sep='\t')
 diseases['censor_date'] = pd.to_datetime(diseases['censor_date'])
 
 # %%
-la_volumes_petersen = pd.read_csv('/home/pdiachil/ml/notebooks/mri/all_rv_boundary_v20201102.csv')
+la_volumes_petersen = pd.read_csv('/home/pdiachil/ml/notebooks/mri/all_boundary_rv_fastai_ml4h.csv')
 
 from scipy.ndimage import median_filter
 def get_min(data, window=5):
@@ -133,7 +133,7 @@ for i, la_volume_set in enumerate(la_volume_sets):
 la_volumes_petersen, = la_volume_sets
 
 # %%
-la_volumes_petersen.to_csv('/home/pdiachil/ml/notebooks/genetics/all_rv_boundaries_covariates_v20201102.csv', index=False)
+la_volumes_petersen.to_csv('/home/pdiachil/ml/notebooks/genetics/all_boundary_rv_fastai_ml4h_covariates.csv', index=False)
 
 # %%
 import matplotlib.pyplot as plt
---
+++ scripts/parameterize_iou.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+gcp_zone=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4)
+VMTAG=$1
+STEP=$2
+
+cd /home/pdiachil/ml
+git checkout pd_atria
+git pull
+
+sudo mkdir -p /mnt/disks/segmented-sax-lax-ml4h-v20201102
+sudo mount -o norecovery,discard,defaults /dev/sdb /mnt/disks/segmented-sax-lax-v20201102/
+
+cnt1=$((VMTAG*STEP))
+cnt2=$((VMTAG*STEP+STEP-1))
+
+for i in $(seq $cnt1 $cnt2)
+do
+    end=$((i+1))
+    /home/pdiachil/ml/scripts/tf.sh -c /home/pdiachil/ml/notebooks/mri/iou_rv_geom.py $i $end
+done
+
+cd /home/pdiachil/ml/notebooks/mri
+/snap/bin/gsutil cp *iou* gs://ml4cvd/pdiachil/rightheart_boundary_40k_v20201102/
+# /snap/bin/gsutil cp *hd5 gs://ml4cvd/pdiachil/rightheart_boundary_images_v20201102/
+# /snap/bin/gsutil cp *xmf gs://ml4cvd/pdiachil/rightheart_boundary_images_v20201102/
+
+# cd /home/pdiachil/projects/chambers
+# /snap/bin/gsutil cp poisson* gs://ml4cvd/pdiachil/rightheart_boundary_xdmf_40k_ml4h_v20201102/
+# /snap/bin/gsutil cp /home/pdiachil/out* gs://ml4cvd/pdiachil/rightheart_boundary_logs_ml4h_40k/
+
+yes | /snap/bin/gcloud compute instances delete $(hostname) --zone ${gcp_zone}
