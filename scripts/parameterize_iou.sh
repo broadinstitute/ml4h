45784d536b3bb0cf7d0217b0465bd1f2bbf58453
Paolo Di Achille <paolo.diachille@gmail.com>
2020-11-10 16:20:02 +0000

    correct disk

--- scripts/parameterize_iou.sh
+++ scripts/parameterize_iou.sh
@@ -7,7 +7,7 @@ cd /home/pdiachil/ml
 git checkout pd_atria
 git pull
 
-sudo mkdir -p /mnt/disks/segmented-sax-lax-ml4h-v20201102
+sudo mkdir -p /mnt/disks/segmented-sax-lax-v20201102
 sudo mount -o norecovery,discard,defaults /dev/sdb /mnt/disks/segmented-sax-lax-v20201102/
 
 cnt1=$((VMTAG*STEP))
