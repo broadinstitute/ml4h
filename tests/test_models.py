be2a47d1d03d58af09b03ab6f73bc72dc7681450
Samwell Freeman <lucidtronix@gmail.com>
2020-11-16 07:53:59 -0500

    fix tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -248,21 +248,21 @@ class TestMakeMultimodalMultitaskModel:
         m.save(os.path.join(tmpdir, 'vae.h5'))
         path = os.path.join(tmpdir, f'm{MODEL_EXT}')
         m.save(path)
+        params['model_file'] = path
         make_multimodal_multitask_model(
             input_output_tmaps[0],
             input_output_tmaps[1],
-            model_file=path,
-            **DEFAULT_PARAMS,
+            **params,
         )
 
     def test_u_connect_adaptive_normalization(self):
         params = DEFAULT_PARAMS.copy()
         params['pool_x'] = params['pool_y'] = 2
         params['bottleneck_type'] = BottleneckType.GlobalAveragePoolStructured
+        params['u_connect'] = defaultdict(set, {SEGMENT_IN: {SEGMENT_OUT}})
         m = make_multimodal_multitask_model(
             [SEGMENT_IN, TMAPS_UP_TO_4D[0]],
             [SEGMENT_OUT],
-            u_connect=defaultdict(set, {SEGMENT_IN: {SEGMENT_OUT}}),
             **params,
         )
         assert_model_trains([SEGMENT_IN, TMAPS_UP_TO_4D[0]], [SEGMENT_OUT], m)
@@ -271,10 +271,10 @@ class TestMakeMultimodalMultitaskModel:
         params = DEFAULT_PARAMS.copy()
         params['pool_x'] = params['pool_y'] = 2
         params['bottleneck_type'] = BottleneckType.NoBottleNeck
+        params['u_connect'] = defaultdict(set, {SEGMENT_IN: {SEGMENT_OUT}}),
         m = make_multimodal_multitask_model(
             [SEGMENT_IN, TMAPS_UP_TO_4D[0]],
             [SEGMENT_OUT],
-            u_connect=defaultdict(set, {SEGMENT_IN: {SEGMENT_OUT}}),
             **params,
         )
         assert_model_trains([SEGMENT_IN, TMAPS_UP_TO_4D[0]], [SEGMENT_OUT], m)
@@ -311,14 +311,13 @@ class TestMakeMultimodalMultitaskModel:
             **params
         )
         assert_model_trains(input_output_tmaps[0], input_output_tmaps[1], m)
-        m.save(os.path.join(tmpdir, 'lstm.h5'))
-        path = os.path.join(tmpdir, f'm{MODEL_EXT}')
+        path = os.path.join(tmpdir, f'lstm{MODEL_EXT}')
         m.save(path)
+        params['model_file'] = path
         make_multimodal_multitask_model(
             input_output_tmaps[0],
             input_output_tmaps[1],
-            model_file=path,
-            **DEFAULT_PARAMS,
+            **params,
         )
 
     @pytest.mark.parametrize(
@@ -386,6 +385,7 @@ class TestMakeMultimodalMultitaskModel:
             tensor_maps_out=pair_list+output_tmaps,
             **params
         )
+
 
 @pytest.mark.parametrize(
     'tmaps',
