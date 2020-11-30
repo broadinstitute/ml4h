3cefe5cf8829d76a89250628b9ca1200c7b53ead
Samwell Freeman <lucidtronix@gmail.com>
2020-11-30 16:39:58 -0500

    bump

--- ml4h/plots.py
+++ ml4h/plots.py
@@ -1803,7 +1803,8 @@ def plot_precision_recall_per_class(prediction, truth, labels, title, prefix='./
         label_text = f'{k} mean precision:{average_precision:.3f} n={true_sums[labels[k]]:.0f}'
         plt.plot(recall, precision, lw=lw, color=c, label=label_text)
         segmented = np.argmax(prediction, axis=-1)
-        dice = (np.sum(segmented[truth[:, labels[k]]] == k) * 2.0) / ((np.sum(segmented[segmented == k]) + np.sum(truth[:, labels[k]]))*truth.shape[0])
+        logging.info(f'Segment {segmented.shape} and dice score of: {truth[:, labels[k]].shape}')
+        dice = (np.sum(segmented[truth[:, labels[k]].astype(int)] == k) * 2.0) / ((np.sum(segmented[segmented == k]) + np.sum(truth[:, labels[k]]))*truth.shape[0])
         logging.info(f'prAUC Label {label_text} and dice score of: {dice}')
         labels_to_areas[k] = average_precision
 
