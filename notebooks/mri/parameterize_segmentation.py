3f9084c66529a9ce1ba19ad3fdc2821053d9a160
Paolo Di Achille <paolo.diachille@gmail.com>
2020-12-04 03:45:43 +0000

    Parameterize heart

--- notebooks/mri/parameterize_segmentation.py
+++ notebooks/mri/parameterize_segmentation.py
@@ -433,9 +433,11 @@ def annotation_to_poisson(
                 if isinstance(channels[projection_ds_idx], list):
                     iou_projection_channel = channels[projection_ds_idx][0]
                 if isinstance(channel, list):
+                    # for channel_elem in channel[1:]:
+                    #     projected_array[projected_array==channel_elem] = channel[0]
                     iou_projected_channel = channel[0]
                 iou = intersection_over_union(projection_array, projected_array[:, :, t], iou_projection_channel, iou_projected_channel)
-                if iou < 0.3 :
+                if iou < 0.0 :
                     logging.info(f'Skipping {view}')
                     continue
 
