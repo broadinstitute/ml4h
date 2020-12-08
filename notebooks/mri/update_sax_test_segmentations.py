ba57d84744d0f8842069046e6375e2b01f439cc3
Paolo Di Achille <paolo.diachille@gmail.com>
2020-12-08 22:35:05 +0000

    Updated tsv

--- notebooks/mri/update_sax_test_segmentations.py
+++ notebooks/mri/update_sax_test_segmentations.py
@@ -16,8 +16,9 @@ sax_version='v20201202'
 lax_version='v20201122'
 storage_client = storage.Client('broad-ml4cvd')
 bucket = storage_client.get_bucket('ml4cvd')
+logging.getLogger().setLevel('INFO')
 
-df_sax_4ch_petersen = pd.read_csv('/home/pdiachil/projects/manifests/manifest_sax_4ch.tsv', sep='\t',
+df_sax_4ch_petersen = pd.read_csv('/home/pdiachil/projects/manifests/manifest_sax_4ch_tar.csv',
                                   usecols=['sample_id', 'instance', 'dicom_file', 'series', 'instance_number'])
 # %%
 
