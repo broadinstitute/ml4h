410cbd965868b52bd1b549e1859211be8b47d16e
Paolo Di Achille <paolo.diachille@gmail.com>
2020-12-09 05:36:13 +0000

    Parameterize script

--- scripts/parameterize_heart.sh
+++ scripts/parameterize_heart.sh
@@ -7,8 +7,8 @@ cd /home/pdiachil/ml
 git checkout pd_atria
 git pull
 
-sudo mkdir -p /mnt/disks/segmented-ml4h-v20201203-v20201122-petersen
-sudo mount -o norecovery,discard,defaults /dev/sdb /mnt/disks/segmented-ml4h-v20201203-v20201122-petersen/
+# sudo mkdir -p /mnt/disks/segmented-ml4h-v20201203-v20201122-petersen
+# sudo mount -o norecovery,discard,defaults /dev/sdb /mnt/disks/segmented-ml4h-v20201203-v20201122-petersen/
 
 cnt1=$((VMTAG*STEP))
 cnt2=$((VMTAG*STEP+STEP-1))
@@ -20,12 +20,12 @@ do
 done
 
 cd /home/pdiachil/ml/notebooks/mri
-/snap/bin/gsutil cp *processed* gs://ml4cvd/pdiachil/surface_reconstruction/sax_4ch/ml4h_v20201203_v20201122/csv/
+/snap/bin/gsutil cp *processed* gs://ml4cvd/pdiachil/surface_reconstruction/sax_4ch/fastai_v20201202_v20201122/csv/
 # /snap/bin/gsutil cp *hd5 gs://ml4cvd/pdiachil/rightheart_boundary_images_v20201102/
 # /snap/bin/gsutil cp *xmf gs://ml4cvd/pdiachil/rightheart_boundary_images_v20201102/
 
 cd /home/pdiachil/projects/chambers
-/snap/bin/gsutil cp poisson* gs://ml4cvd/pdiachil/surface_reconstruction/sax_4ch/ml4h_v20201203_v20201122/xdmf/
-/snap/bin/gsutil cp /home/pdiachil/out* gs://ml4cvd/pdiachil/surface_reconstruction/sax_4ch/ml4h_v20201203_v20201122/log/
+/snap/bin/gsutil cp poisson* gs://ml4cvd/pdiachil/surface_reconstruction/sax_4ch/fastai_v20201202_v20201122/xdmf/
+/snap/bin/gsutil cp /home/pdiachil/out* gs://ml4cvd/pdiachil/surface_reconstruction/sax_4ch/fastai_v20201202_v20201122/log/
 
 yes | /snap/bin/gcloud compute instances delete $(hostname) --zone ${gcp_zone}
