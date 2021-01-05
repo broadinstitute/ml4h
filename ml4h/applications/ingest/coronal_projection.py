ccede055527ad78b19fc944934cde984677531dc
ndiamant <ndiamant@broadinstitute.org>
2021-01-05 16:10:12 -0500

    added sagittal projections and smarter overlapping of stations

--- ml4h/applications/ingest/coronal_projection.py
+++ ml4h/applications/ingest/coronal_projection.py
@@ -1,37 +1,88 @@
 import time
 import json
 from multiprocessing import Pool, cpu_count
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import os
 import h5py
 import numpy as np
 import pandas as pd
+from scipy.ndimage import zoom
 from fastparquet import ParquetFile
 import matplotlib.pyplot as plt
 
 from ingest_mri import compress_and_store, read_compressed
 
 
-def project(x):
+def project_coronal(x):
     return x.mean(axis=0).T
+
+
+def project_sagittal(x):
+    return x.mean(axis=1).T
+
+
+def normalize(projection):
+    projection = 255 * projection / np.sort(projection)[:-50].mean()
+    return projection.astype(np.uint16)
 
 
 def stack(xs):
     return np.vstack(xs)
 
 
-def _num_images_to_drop(
-        series_above_max_z: float, series_above_min_z: float, series_above_num_slices: int,
-        series_below_max_z: float,
-) -> int:
+def center_pad(x, width: int):
+    """pad an image on the left and right with 0s to a target width"""
+    new_x = np.zeros((x.shape[0], width))
+    offset = (width - x.shape[1]) // 2
+    new_x[:, offset: width - offset] = x
+    return new_x
+
+
+def center_pad_stack(xs):
+    """center and pad then stack images with different widths"""
+    max_width = max(x.shape[1] for x in xs)
+    return np.vstack([center_pad(x, max_width) for x in xs])
+
+
+def build_z_slices(
+        num_slices: List[int],
+        z_pos: List[Tuple[float, float]],
+) -> List[slice]:
     """
-    Calculates the number of images to drop from the series above
-    so that the series align in the final image
+    Finds which images to take from each station.
+    Takes a few noisy images from the top of each station
+    and removes any remaining overlap between stations.
+
+    num_slices: number of slices of each station
+    z_pos: min z, max z of each station
+
+    Example calculating the top and bottom z position of each station:
+        z_pos = [50, 100], [90, 140]
+        -> z_pos = [54, 100], [94, 140]  # remove images from the top
+        -> overlap = [94, 100]
+        -> z_pos = [54, 100], [100, 140]  # remove overlapping images
     """
-    overlap_size = series_below_max_z - series_above_min_z
-    overlap_frac = overlap_size / (series_above_max_z - series_above_min_z)
-    return int(max(0., overlap_frac) * series_above_num_slices)
+    # remove a few slices from the top of each station
+    top_remove_num = 4
+    slice_starts = []
+    for i in range(len(num_slices)):
+        slice_starts.append(top_remove_num)
+        top_remove_frac = top_remove_num / num_slices[i]
+        lo, hi = z_pos[i]
+        z_pos[i] = lo, hi - (hi - lo) * top_remove_frac
+
+    # remove remaining overlaps from the bottom of each station
+    slice_ends = []
+    for i in range(0, len(num_slices) - 1):
+        series_below_min_z, series_below_max_z = z_pos[i + 1]
+        series_above_min_z, series_above_max_z = z_pos[i]
+        overlap_size = series_below_max_z - series_above_min_z
+        overlap_frac = overlap_size / (series_above_max_z - series_above_min_z)
+        slice_ends.append(int((1 - overlap_frac) * num_slices[i]))
+    slice_ends.append(num_slices[-1])  # the last station gets nothing removed from the bottom
+    # build slices
+    return [slice(start, end) for start, end in zip(slice_starts, slice_ends)]
 
 
 def build_projections(data: Dict[int, np.ndarray], meta_data: pd.DataFrame) -> Dict[str, np.ndarray]:
@@ -41,26 +92,31 @@ def build_projections(data: Dict[int, np.ndarray], meta_data: pd.DataFrame) -> D
     :param meta_data: meta data loaded from parquet file
     :return: {series type: coronal projection}
     """
+    station_z_scales = 3.0, 4.5, 4.5, 4.5, 3.5, 4.0
+    station_z_scales = [scale / 3 for scale in station_z_scales]
+
     z_pos = meta_data.groupby('series_number')['image_position_z'].agg(['min', 'max'])
     projections = {}
+    slices = build_z_slices(
+        [data[i].shape[-1] for i in range(1, 25, 4)],
+        [z_pos.loc[i] for i in range(1, 25, 4)],
+    )
     for type_idx, series_type_name in zip(range(4), ('in', 'opp', 'f', 'w')):
-        to_stack = []
+        coronal_to_stack = []
+        sagittal_to_stack = []
         for station_idx in range(1, 25, 4):  # neck, upper ab, lower ab, legs
             series_num = station_idx + type_idx
-            if station_idx == 21:  # don't drop anything from the last station
-                to_stack.append(project(data[series_num]))
-                continue
-            z_lo, z_hi = z_pos.loc[series_num]
-            next_z_lo, next_z_hi = z_pos.loc[series_num + 4]
-            drop_num = _num_images_to_drop(
-                series_above_max_z=z_hi, series_above_min_z=z_lo,
-                series_above_num_slices=data[series_num].shape[-1],
-                series_below_max_z=next_z_hi,
-            )
-            if drop_num <= 0:  # in this case don't drop anything
-                drop_num = data[series_num].shape[-1]
-            to_stack.append(project(data[series_num][..., :-drop_num]))
-        projections[series_type_name] = stack(to_stack).astype(np.uint16)
+            station_slice = slices[station_idx // 4]
+            scale = station_z_scales[station_idx // 4]
+            coronal = project_coronal(data[series_num][..., station_slice])
+            coronal = zoom(coronal, (scale, 1.), order=1)
+            coronal_to_stack.append(coronal)
+            sagittal = project_sagittal(data[series_num][..., station_slice])
+            sagittal = zoom(sagittal, (scale, 1.), order=1)
+            sagittal_to_stack.append(sagittal)
+
+        projections[f'{series_type_name}_coronal'] = normalize(stack(coronal_to_stack))
+        projections[f'{series_type_name}_sagittal'] = normalize(center_pad_stack(sagittal_to_stack))
     return projections
 
 
@@ -68,13 +124,20 @@ def visualize_projections(
     projections: Dict[str, np.ndarray],
     output_path: str,
 ):
-    h, w = projections['in'].shape
+    h, w = projections['in_coronal'].shape
     w *= 4
-    fig, axes = plt.subplots(1, 4, figsize=(w // 40, h // 40))
-    for ax, (name, im) in zip(axes, projections.items()):
+    h *= 2
+    fig, (axes1, axes2) = plt.subplots(2, 4, figsize=(w // 40, h // 40))
+    cor = sorted(name for name in projections if 'cor' in name)
+    sag = sorted(name for name in projections if 'sag' in name)
+    for ax, name in zip(axes1, cor):
         ax.set_title(name)
         ax.set_axis_off()
-        ax.imshow(im, cmap='gray')
+        ax.imshow(projections[name], cmap='gray')
+    for ax, name in zip(axes2, sag):
+        ax.set_title(name)
+        ax.set_axis_off()
+        ax.imshow(projections[name], cmap='gray')
     fig.savefig(output_path, bbox_inches='tight', pad_inches=0)
 
 
