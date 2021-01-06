64996e4b8c39d99f70897692ccc8df427a214fc9
Paolo Di Achille <paolo.diachille@gmail.com>
2021-01-06 08:55:38 +0000

    Remove preclip surface generation

--- notebooks/mri/parameterize_rv_geom.py
+++ notebooks/mri/parameterize_rv_geom.py
@@ -162,12 +162,13 @@ for i, hd5 in enumerate(hd5s):
                     annot_datasets, channel, views,
                     annot_time_format_string,
                     range(MRI_FRAMES), 0,
-                    save_path='/home/pdiachil/ml/1000107_normals'
+                    save_path=None
+                    # save_path='/home/pdiachil/ml/1000107_normals'
                 )
-                for t, atrium in enumerate(atria):
-                    write_footer = True if t == MRI_FRAMES-1 else False
-                    append = False if t == 0 else True
-                    to_xdmf(atrium, f'/home/pdiachil/projects/chambers/poisson_preclip_{version}_{chamber}_{sample_id}_{instance}', append=append, append_time=t, write_footer=write_footer)
+                # for t, atrium in enumerate(atria):
+                #     write_footer = True if t == MRI_FRAMES-1 else False
+                #     append = False if t == 0 else True
+                #     to_xdmf(atrium, f'/home/pdiachil/projects/chambers/poisson_preclip_{version}_{chamber}_{sample_id}_{instance}', append=append, append_time=t, write_footer=write_footer)
                 atria, volumes = clip_by_separation_plane(annot_datasets[0], 
                                                         annot_datasets[1:3],
                                                         [MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP['RV_cavity'], 
