7198114c9275e393f3fc5964c76b4cd81755db0e
Paolo Di Achille <paolo.diachille@gmail.com>
2021-01-09 16:43:17 +0000

    LVOT bash

---
+++ scripts/lvot_diameter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+gcp_zone=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4)
+VMTAG=$1
+STEP=$2
+
+cd /home/pdiachil/ml
+git checkout pd_atria
+git pull
+
+cnt1=$((VMTAG*STEP))
+cnt2=$((VMTAG*STEP+STEP))
+
+/home/pdiachil/ml/scripts/tf.sh -c /home/pdiachil/ml/notebooks/mri/lvot_diameter.py $cnt1 $cnt2
+
+/snap/bin/gsutil cp /home/pdiachil/chambers/*.gif gs://ml4cvd/pdiachil/lvot_diameter/gif/
+/snap/bin/gsutil cp /home/pdiachil/chambers/*.csv gs://ml4cvd/pdiachil/lvot_diameter/csv/
+
+yes | /snap/bin/gcloud compute instances delete $(hostname) --zone ${gcp_zone}
