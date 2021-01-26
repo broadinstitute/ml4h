67f0d94a6b7c6d6c94dd73a7d0a3e23ffec4adac
mklarqvist <marcus.klarqvist@gmail.com>
2021-01-26 13:53:46 -0500

    Comments of shell script

--- ml4h/applications/jpp_inference_rv/infer_to_hd5_local.sh
+++ ml4h/applications/jpp_inference_rv/infer_to_hd5_local.sh
@@ -1,3 +1,7 @@
+target_disk = annotated-cardiac-tensors-45k
+target_vm_snapshot = rv-parameterization
+vm_base_name = pdiachil-rv-inference
+
 for j in {0..4}
 do
     start=$((j*10))
@@ -6,27 +10,29 @@ do
     do
     for i in $(seq $start $end)
     do
-	gcloud compute instances create pdiachil-rv-inference-${i} \
+    # Spawn a VM instance given prepared VM snapshot.
+	gcloud compute instances create $vm_base_name-${i} \
 	       --machine-type=n1-standard-8 \
 	       --boot-disk-size=150 \
-	       --image=rv-parameterization \
+	       --image=$target_vm_snapshot \
 	       --maintenance-policy=TERMINATE \
 	       --accelerator=type=nvidia-tesla-p4,count=1 &
-	sleep 1
-    done
-    wait
-    done
-    wait
+	sleep 1  # Sleep for a second
+    done # End loop i
+    wait # Wait until completion
+    done # End loop k
+    wait # Wait until completion
     for i in $(seq $start $end)
     do
-	gcloud compute instances attach-disk pdiachil-rv-inference-${i} --disk=annotated-cardiac-tensors-45k --mode ro &
-	sleep 1
-    done
-    wait
-    sleep 25
+    # Attach disk(s) to the spawned VM instance.
+	gcloud compute instances attach-disk $vm_base_name-${i} --disk=$target_disk --mode ro &
+	sleep 1 # Sleep for a second
+    done # End loop i
+    wait # Wait until completeion
+    sleep 25 # Sleep for 25 seconds 
     for i in $(seq $start $end)
     do
-	gcloud compute ssh pdiachil-rv-inference-${i} --command="cd /home/pdiachil;cd ml;git pull;git checkout pd_sf_blox;git pull;nohup bash /home/pdiachil/ml/scripts/infer_to_hd5.sh $i > /home/pdiachil/out_${i}.out 2> /home/pdiachil/out_${i}.err < /dev/null &" &
-	sleep 1
-    done
-done
+	gcloud compute ssh $vm_base_name-${i} --command="cd /home/pdiachil;cd ml;git pull;git checkout pd_sf_blox;git pull;nohup bash /home/pdiachil/ml/scripts/infer_to_hd5.sh $i > /home/pdiachil/out_${i}.out 2> /home/pdiachil/out_${i}.err < /dev/null &" &
+	sleep 1 # Sleep for a second
+    done # End loop i
+done # End outer VM loop
