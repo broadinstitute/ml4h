dca9f23cc575af2b274209aacf5322ca68ceb4c5
Paolo Di Achille <pdiachil@broadinstitute.org>
2021-03-04 03:29:06 +0000

    Probs

--- notebooks/ppg/ppgs_probability.py
+++ notebooks/ppg/ppgs_probability.py
@@ -35,19 +35,19 @@ batch_size = 32
 train_dataset = (
     train_loader.shuffle(len(x_train))
     .batch(batch_size)
-    .prefetch(2)
+    .prefetch(10)
 )
 
 validation_dataset = (
     validation_loader.shuffle(len(x_val))
     .batch(batch_size)
-    .prefetch(2)
+    .prefetch(10)
 )
 
 test_dataset = (
     test_loader.shuffle(len(x_test))
     .batch(batch_size)
-    .prefetch(2)
+    .prefetch(10)
 )
 
 # %%
@@ -67,26 +67,28 @@ from tensorflow import keras
 # Residual unit
 class ResidualUnit(keras.layers.Layer):
     def __init__(self, filters, strides=1, activation='relu', **kwargs):
-        super().__init__(**kwargs)
-        self.activation = keras.activations.get(activation)
+        super().__init__(**kwargs)        
+        self.activation = activation
+        self.filters = filters
+        self.strides = strides
         self.main_layers = [
             keras.layers.Conv1D(filters, 3, strides=strides, padding='same', use_bias=False),
             keras.layers.BatchNormalization(),
-            self.activation,
+            keras.activations.get(self.activation),
             keras.layers.Conv1D(filters, 3, strides=1, padding='same', use_bias=False),
             keras.layers.BatchNormalization()]
         self.skip_layers = []
         if strides > 1:
             self.skip_layers = [
                 keras.layers.Conv1D(filters, 1, strides=strides, padding='same', use_bias=False),
-                keras.layers.BatchNormalization()]
+                keras.layers.BatchNormalization()]       
 
     def get_config(self):
         config = super().get_config().copy()
         config.update({
             'activation': self.activation,
-            'main_layers': self.main_layers,
-            'skip_layers': self.skip_layers
+            'filters': self.filters,
+            'strides': self.strides
         })
         return config
             
@@ -97,7 +99,7 @@ class ResidualUnit(keras.layers.Layer):
         skip_Z = inputs
         for layer in self.skip_layers:
             skip_Z = layer(skip_Z)
-        return self.activation(Z + skip_Z)
+        return keras.activations.get(self.activation)(Z + skip_Z)
 
 model = keras.models.Sequential()
 model.add(keras.layers.Conv1D(64, 7, strides=2, input_shape=[100, 1], padding='same', use_bias=False))
@@ -127,14 +129,14 @@ model.compile(
 
 # Define callbacks.
 checkpoint_cb = keras.callbacks.ModelCheckpoint(
-    "3d_image_classification.h5", save_best_only=True
+    "ppg_notch_classification.h5", save_best_only=True
 )
 early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_acc", patience=15)
 
 # %%
 # Train the model, doing validation at the end of each epoch
 epochs = 100
-model.fit(
+history = model.fit(
     train_dataset,
     validation_data=validation_dataset,
     epochs=epochs,
@@ -142,5 +144,68 @@ model.fit(
     callbacks=[checkpoint_cb, early_stopping_cb],
 )
 
+# %%
+model.load_weights("ppg_notch_classification.h5")
 
+
+# %%
+print("Evaluate on test data")
+results = model.evaluate(x_test, y_test, batch_size=128)
+print("test loss, test acc:", results)
+
+# %%
+print("Generate predictions for 3 samples")
+predictions = model.predict(x_test)
+print("predictions shape:", predictions.shape)
+
+
+# %%
+import seaborn as sns
+
+sns.distplot(predictions)
+
+# %%
+probs = model.output.op.inputs[0]
+func = keras.backend.function([model.input], [probs])
+probs_test = func([x.reshape(-1, 100, 1)])
+# sns.distplot(probs_test-predictions)
+# %%
+import scipy.stats as ss
+
+def rank_to_normal(rank, c, n):
+    # Standard quantile function
+    x = (rank - c) / (n - 2*c + 1)
+    return ss.norm.ppf(x)
+
+c=3.0/8
+rank = ss.rankdata(probs_test, method="average")
+rank = pd.Series(rank)
+transformed = rank.apply(rank_to_normal, c=c, n=len(rank))
+# %%
+sns.distplot(transformed)
+# %%
+sorted_idxs = np.argsort(transformed)
+f, ax = plt.subplots(5, 5)
+j = 0
+for i in range(99, 1, -4):
+    pos = int(len(sorted_idxs)*i/100.)
+    ax[j//5, j%5].plot(x[sorted_idxs[pos]], linewidth=3, color='black')
+    label = 'abs.' if y[sorted_idxs[pos]] > 0.5 else 'pres.'
+    title_str = f'{label} | {i}%'
+    ax[j//5, j%5].set_title(title_str)
+    ax[j//5, j%5].set_xticklabels([])
+    ax[j//5, j%5].set_yticklabels([])
+
+    j += 1
+plt.tight_layout()
+plt.savefig('ppg_notch_grade_resnet.png', dpi=500)
+
+
+# %%
+transformed
+# %%
+df['resnet_notch'] = probs_test[0]
+df['resnet_notch_grade'] = transformed
+# %%
+df.to_csv('/home/pdiachil/projects/ppgs/instance0_notch_vector_paolo_ml_grade_012121.csv')
 # %%
