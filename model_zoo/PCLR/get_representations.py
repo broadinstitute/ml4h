5723bfcee81371e0f376e52ef9110359c6df6122
Nathaniel Diamant <ndiamant@hmc.edu>
2021-04-07 18:55:59 -0400

    moved non-model out of lfs (#437)

--- model_zoo/PCLR/get_representations.py
+++ model_zoo/PCLR/get_representations.py
@@ -1,3 +1,35 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4cf4b3747066b98f518e051ed5132cc030a81728b579daf7743ae3ce6ec5d9ce
-size 1056
+from typing import List, Dict
+
+import numpy as np
+from tensorflow.keras.models import load_model, Model
+
+from preprocess_ecg import process_ecg, LEADS
+
+
+def get_model() -> Model:
+    """Get PCLR embedding model"""
+    return load_model("./PCLR.h5")
+
+
+def get_representations(ecgs: List[Dict[str, np.ndarray]]) -> np.ndarray:
+    """
+    Uses PCLR trained model to build representations of ECGs
+    :param ecgs: A list of dictionaries mapping lead name to lead values.
+                 The lead values should be measured in milli-volts.
+                 Each lead should represent 10s of samples.
+    :return:
+    """
+    model = get_model()
+    ecgs = np.stack(list(map(process_ecg, ecgs)))
+    return model.predict(ecgs)
+
+
+def test_get_representations():
+    """Test to make sure get_representations works as expected"""
+    fake_ecg = {
+        lead: np.zeros(2500)
+        for lead in LEADS
+    }
+    fake_ecgs = [fake_ecg for _ in range(10)]
+    representations = get_representations(fake_ecgs)
+    assert representations.shape == (len(fake_ecgs), 320)
