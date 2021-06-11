49bd556a1c4007e2d874cb24d65f3774f637f5f8
Paolo Di Achille <paolo.diachille@gmail.com>
2021-06-11 02:48:32 +0000

    LAX changes

--- notebooks/mri/ra_simpson_view.py
+++ notebooks/mri/ra_simpson_view.py
@@ -24,7 +24,41 @@ ax[1].set_yticklabels([])
 plt.tight_layout()
 f.savefig('ra_simpson_dist.png', dpi=500)
 # %%
-all_cleaned_simpson = pd.read_csv('/home/pdiachil/projects/ra_simpson/all_cleaned_RA_simpson_fastai_sax-v20201202-2ch-v20200809-3ch-v20200603-4ch-v20201122.csv')
+all_cleaned_simpson = pd.read_csv('/home/pdiachil/projects/ra_simpson/all_clean_RA_simpson.csv')
 # %%
-all_cleaned_simpson[all_cleaned_simpson[cols].max(axis=1)<1]
+james_ra = pd.read_csv('/home/pdiachil/projects/ra_simpson/right_left_heart_combined.pheno.tsv', sep='\t')
+# %%
+all_cleaned_ra = all_cleaned_simpson.merge(james_ra, on=['sample_id', 'instance'])
+# %%
+cols = [f'RA_simpson_{t}' for t in range(50)]
+all_cleaned_ra['RA_simpson_max'] = all_cleaned_ra[cols].max(axis=1)
+all_cleaned_ra['RA_simpson_min'] = all_cleaned_ra[cols].min(axis=1)
+all_cleaned_ra['max_ra_vol'] = np.sqrt(all_cleaned_ra['max_ra'] / np.pi)**3.0*4.0/3.0*np.pi
+all_cleaned_ra['min_ra_vol'] = np.sqrt(all_cleaned_ra['min_ra'] / np.pi)**3.0*4.0/3.0*np.pi
+# %%
+import numpy as np
+all_cleaned_ra['RA_max_diff'] = np.abs((all_cleaned_ra['RA_simpson_max'] - all_cleaned_ra['max_ra_vol'])/all_cleaned_ra['max_ra_vol'])
+all_cleaned_ra['RA_min_diff'] = np.abs((all_cleaned_ra['RA_simpson_min'] - all_cleaned_ra['min_ra_vol'])/all_cleaned_ra['min_ra_vol'])
+# %%
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(all_cleaned_ra['RA_max_diff'].dropna())
+# %%
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(all_cleaned_ra['RA_min_diff'].dropna())
+
+# %%
+all_cleaned_ra[(all_cleaned_ra['RA_min_diff']<all_cleaned_ra['RA_min_diff'].quantile(0.998)+0.01) & \
+               (all_cleaned_ra['RA_min_diff']>all_cleaned_ra['RA_min_diff'].quantile(0.998)-0.01)]
+# %%
+sample_id = 2725878
+f, ax = plt.subplots()
+ax.plot(all_cleaned_ra[all_cleaned_ra['sample_id']==sample_id][cols].values[0], label='Simpson')
+ax.plot([0, 50], [all_cleaned_ra[all_cleaned_ra['sample_id']==sample_id]['min_ra_vol'].values, all_cleaned_ra[all_cleaned_ra['sample_id']==sample_id]['min_ra_vol'].values], label='min_ra_sphere')
+ax.plot([0, 50], [all_cleaned_ra[all_cleaned_ra['sample_id']==sample_id]['max_ra_vol'].values, all_cleaned_ra[all_cleaned_ra['sample_id']==sample_id]['max_ra_vol'].values], label='max_ra_sphere')
+ax.legend()
+f.savefig(f'{sample_id}.png')
 # %%
