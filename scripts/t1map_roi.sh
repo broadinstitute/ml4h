97f84a22d53259ccceb101e47f7c7dc649649eb6
Paolo Di Achille <paolo.diachille@gmail.com>
2021-06-25 14:23:13 +0000

    Scripts

--- scripts/t1map_roi.sh
+++ scripts/t1map_roi.sh
@@ -23,8 +23,8 @@ do
     cd /home/pdiachil/ml
     /home/pdiachil/ml/scripts/tf.sh -c /home/pdiachil/ml/notebooks/mri/t1map_inference.py $start $end    
     cd /home/pdiachil/projects/t1map/inference
-    /snap/bin/gsutil cp *.csv gs://ml4cvd/pdiachil/t1map-pngs/inference_rois_iqr_3px/
-    /snap/bin/gsutil cp *.png gs://ml4cvd/pdiachil/t1map-pngs/inference_rois_iqr_3px/
+    /snap/bin/gsutil cp *.csv gs://ml4cvd/pdiachil/t1map-pngs/inference_rois_iqr_3px/csv/
+    /snap/bin/gsutil cp *.png gs://ml4cvd/pdiachil/t1map-pngs/inference_rois_iqr_3px/png/
     cd /home/pdiachil/ml/notebooks/mri
     rm -f *.hd5
     # /snap/bin/gsutil cp *.png gs://ml4cvd/pdiachil/t1map-pngs/inference/
