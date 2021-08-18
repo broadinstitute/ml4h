9fbb516bef1bdc76fb95cc6b3d548b311c435127
Samuel Friedman <lucidtronix@gmail.com>
2021-08-18 06:27:31 -0400

    gatk

--- ml4h/tensormap/ukb/demographics.py
+++ ml4h/tensormap/ukb/demographics.py
@@ -235,6 +235,10 @@ bmi_ukb = TensorMap(
     'bmi', Interpretation.CONTINUOUS, path_prefix='continuous', channel_map={'23104_Body-mass-index-BMI_0_0': 0}, annotation_units=1,
     validator=make_range_validator(0, 300), normalization={'mean': 27.432061533712652, 'std': 4.785244772462738}, loss='logcosh',
 )
+bmi_2 = TensorMap(
+    '21001_Body-mass-index-BMI_2_0', Interpretation.CONTINUOUS, path_prefix='continuous', channel_map={'21001_Body-mass-index-BMI_2_0': 0}, annotation_units=1,
+    validator=make_range_validator(0, 300), normalization=Standardize(mean=27.3397, std=4.7721), loss='logcosh',
+)
 bmi_21 = TensorMap(
     '21001_Body-mass-index-BMI_0_0', Interpretation.CONTINUOUS, path_prefix='continuous', channel_map={'21001_Body-mass-index-BMI_0_0': 0}, annotation_units=1,
     validator=make_range_validator(0, 300), normalization=Standardize(mean=27.3397, std=4.7721), loss='logcosh',
