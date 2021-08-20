86830fe15a55ab27bad44b3174cba42f4014d75d
Samuel Friedman <lucidtronix@gmail.com>
2021-08-20 12:06:49 -0400

    gatk tensor maps

--- ml4h/recipes.py
+++ ml4h/recipes.py
@@ -333,6 +333,7 @@ def infer_multimodal_multitask(args):
                             logging.debug(f'index error at {tm.name} item {i} key {k} with cm: {tm.channel_map} y is {y.shape} y is {y}')
                 elif tm.axes() > 1:
                     hd5_path = os.path.join(args.output_folder, 'inferred_hd5s', f'{sample_id}_inferred{TENSOR_EXT}')
+                    os.makedirs(os.path.dirname(hd5_path), exist_ok=True)
                     with h5py.File(hd5_path, 'a') as hd5:
                         hd5.create_dataset(f'{hd5_path}/{tm.name}_truth', data=output_data[tm.output_name()][0], compression='gzip')
                         hd5.create_dataset(f'{hd5_path}/{tm.name}_prediction', data=y[0], compression='gzip')
