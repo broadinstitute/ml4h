c7ef4b11204758765cc5983d9a5b53f1e6c23000
Samuel Friedman <lucidtronix@gmail.com>
2021-11-03 05:46:45 -0400

    patientage

--- ml4h/tensormap/ukb/ecg.py
+++ ml4h/tensormap/ukb/ecg.py
@@ -427,7 +427,7 @@ ecg_rest = TensorMap(
 )
 ecg_rest_mgb = TensorMap(
     'ecg_5000_std', Interpretation.CONTINUOUS, shape=(5000, 12), path_prefix='ukb_ecg_rest', tensor_from_file=_make_ecg_rest(),
-    channel_map=ECG_REST_AMP_LEADS_UKB, normalization=ZeroMeanStd1(),
+    channel_map=ECG_REST_MGB_LEADS, normalization=ZeroMeanStd1(),
 )
 ecg_rest_mgb_2500 = TensorMap(
     'ecg_2500_std', Interpretation.CONTINUOUS, shape=(2500, 12), path_prefix='ukb_ecg_rest', tensor_from_file=_make_ecg_rest(),
