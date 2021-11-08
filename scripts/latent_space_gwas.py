abea17d55ade1eb75c31b66aae8cff78329897f0
Samuel Friedman <lucidtronix@gmail.com>
2021-11-08 08:15:14 -0500

    start

--- scripts/latent_space_gwas.py
+++ scripts/latent_space_gwas.py
@@ -9,7 +9,7 @@ from statsmodels.multivariate.manova import MANOVA
 from sklearn.linear_model import LogisticRegression, LinearRegression, ElasticNet, Ridge
 
 
-LATENT_COLS=[f'new_latent_24_{i}' for i in range(86)]
+LATENT_COLS=[f'new_latent_17_{i}' for i in range(39)]
 ADJUST_COLS=['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10', 'PC11',
                'PC12', 'PC13', 'PC14', 'PC15', 'PC16', 'PC17', 'PC18', 'PC19', 'PC20', 'PC21',
                'gt_array_axiom', 'gt_batch', 'assessment_center', 'age', 'age_squared', 'sex']
