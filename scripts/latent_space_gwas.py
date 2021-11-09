aa2ddc23f925854648e1ae00b0c53eb792a2189f
Samuel Friedman <lucidtronix@gmail.com>
2021-11-09 15:49:28 -0500

    start

--- scripts/latent_space_gwas.py
+++ scripts/latent_space_gwas.py
@@ -10,8 +10,6 @@ from sklearn.linear_model import LogisticRegression, LinearRegression, ElasticNe
 
 
 ADJUST_COLS=['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10', 'PC11',
-             'PC12', 'PC13', 'PC14', 'PC15', 'PC16', 'PC17', 'PC18', 'PC19', 'PC20', 'PC21',
-             'PC22', 'PC23', 'PC24', 'PC25', 'PC26', 'PC27', 'PC28', 'PC29', 'PC30',
              'gt_array_axiom', 'gt_batch', 'assessment_center', 'age', 'age_squared', 'sex']
 
 def run():
