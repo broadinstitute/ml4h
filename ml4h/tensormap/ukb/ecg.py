c44dd47d6a6790696141f951e8496a7f4e35a967
Samuel Friedman <lucidtronix@gmail.com>
2021-12-20 12:26:22 -0500

    update

--- ml4h/tensormap/ukb/ecg.py
+++ ml4h/tensormap/ukb/ecg.py
@@ -481,7 +481,7 @@ ecg_rest_mgb_2500 = TensorMap(
     tensor_from_file=_make_ecg_rest(downsample_steps=2), channel_map=ECG_REST_AMP_LEADS_UKB, normalization=ZeroMeanStd1(),
 )
 ecg_rest_mgb_2500_test = TensorMap(
-    'ecg_5000_std', Interpretation.CONTINUOUS, shape=(2500, 12), path_prefix='ukb_ecg_rest',
+    'ecg_2500_std', Interpretation.CONTINUOUS, shape=(2500, 12), path_prefix='ukb_ecg_rest',
     tensor_from_file=_make_ecg_rest(downsample_steps=2), channel_map=ECG_REST_AMP_LEADS_UKB, normalization=ZeroMeanStd1(),
 )
 
