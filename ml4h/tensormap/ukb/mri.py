45f99f3e71d47c4b158860acbb896f0e651b711a
Samuel Friedman <lucidtronix@gmail.com>
2022-02-10 06:43:20 -0500

    update

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -463,6 +463,15 @@ t1_dicom_16_slices_144_160 = TensorMap(
         't1_p2_1mm_fov256_sag_ti_880_1', 144, 160, 1, pad_shape=(192, 256, 208),
     ),
 )
+t1_dicom_16_slices_128_144 = TensorMap(
+    't1_dicom_16_slices_128_144',
+    shape=(192, 256, 16),
+    path_prefix='ukb_brain_mri',
+    normalization=ZeroMeanStd1(),
+    tensor_from_file=_slice_subset_tensor(
+        't1_p2_1mm_fov256_sag_ti_880_1', 128, 144, 1, pad_shape=(192, 256, 208),
+    ),
+)
 t2_40_slices_1 = TensorMap(
     't2_40_slices_1',
     shape=(256, 256, 40),
