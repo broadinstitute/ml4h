4e087ebcaab2c27c8a1df7765581a50f1a53fda6
Samuel Friedman <lucidtronix@gmail.com>
2022-02-15 05:59:12 -0500

    cleanup

--- ml4h/plots.py
+++ ml4h/plots.py
@@ -461,9 +461,9 @@ def plot_metric_history(history, training_steps: int, title: str, prefix="./figu
         os.makedirs(os.path.dirname(figure_path))
     plt.savefig(figure_path)
     if 'loss' in history.history:
-        logging.info(f'Starting training loss:   {history.history["loss"][0]:0.3f}, Final training loss:  {history.history["loss"][-1]:0.3f}')
+        logging.info(f'Starting training loss:   {history.history["loss"][0]:0.3f}, Final training loss:   {history.history["loss"][-1]:0.4f}')
     if 'val_loss' in history.history:
-        logging.info(f'Starting validation loss: {history.history["val_loss"][0]:0.3f}, Final validation loss: {history.history["val_loss"][-1]:0.3f}')
+        logging.info(f'Starting validation loss: {history.history["val_loss"][0]:0.3f}, Final validation loss: {history.history["val_loss"][-1]:0.4f}')
     logging.info(f"Saved learning curves at:{figure_path}")
 
 
