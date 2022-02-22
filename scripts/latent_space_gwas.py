56c65b80c587720cdccb53afee714f48a7c5d27d
Samuel Friedman <lucidtronix@gmail.com>
2022-02-22 15:27:46 -0500

    cleanup

--- scripts/latent_space_gwas.py
+++ scripts/latent_space_gwas.py
@@ -10,11 +10,11 @@ from sklearn.linear_model import LogisticRegression, LinearRegression, ElasticNe
 
 
 ADJUST = [
-    'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10', 'PC11',
-    'PC12', 'PC13', 'PC14', 'PC15', 'PC16', 'PC17', 'PC18', 'PC19', 'PC20',
-    'PC21', 'PC22', 'PC23', 'PC24', 'PC25', 'PC26', 'PC27', 'PC28', 'PC29', 'PC30',
-    'PC31', 'PC32', 'PC33', 'PC34', 'PC35', 'PC36', 'PC37', 'PC38', 'PC39', 'PC40',
-    'gt_batch', 'assessment_center', 'age', 'sex',
+    #'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10',
+    #'PC11', 'PC12', 'PC13', 'PC14', 'PC15', 'PC16', 'PC17', 'PC18', 'PC19', 'PC20',
+    #'PC21', 'PC22', 'PC23', 'PC24', 'PC25', 'PC26', 'PC27', 'PC28', 'PC29', 'PC30',
+    #'PC31', 'PC32', 'PC33', 'PC34', 'PC35', 'PC36', 'PC37', 'PC38', 'PC39', 'PC40',
+    #'gt_batch', 'assessment_center', 'age', 'sex',
     #'bmi',
 ]
 
