34dd550818047b7291e1ce820db9f0a9f3e47785
Paolo Di Achille <paolo.diachille@gmail.com>
2022-03-15 10:48:08 +0000

    Add tf

--- scripts/aorta_projections.sh
+++ scripts/aorta_projections.sh
@@ -11,13 +11,15 @@ git pull
 # sudo mount -o norecovery,discard,defaults /dev/sdb /mnt/disks/segmented-ml4h-v20201203-v20201122-petersen/
 
 cnt1=$((VMTAG*STEP))
-cnt2=$((VMTAG*STEP+STEP))
+cnt2=$((VMTAG*STEP+STEP-1))
 
 # cnt1=$1
 # cnt2=$2
 
-/home/pdiachil/ml/scripts/tf.sh -c /home/pdiachil/ml/notebooks/mri/aorta_projections.py $cnt1 $cnt2
+/home/pdiachil/ml/scripts/tf.sh -c \
+"for i in $(seq $cnt1 $cnt2); do python /home/pdiachil/ml/notebooks/mri/aorta_projections.py $i $((i+1)); done"
+
 cd /home/pdiachil/ml/notebooks/mri
 /snap/bin/gsutil cp *.npy gs://ml4cvd/pdiachil/aorta_for_marcus/aorta_numpy_46k/
 /snap/bin/gsutil cp -r bodymri* gs://ml4cvd/pdiachil/aorta_for_marcus/aorta_vti_46k/
-#yes | /snap/bin/gcloud compute instances delete $(hostname) --zone ${gcp_zone}
+yes | /snap/bin/gcloud compute instances delete $(hostname) --zone ${gcp_zone}
