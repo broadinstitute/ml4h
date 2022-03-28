a7861b79af78559a46f6c0febcb83476cc071d5b
Samuel Friedman <lucidtronix@gmail.com>
2022-03-28 14:51:03 -0400

    cleanup

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1956,7 +1956,6 @@ segmented_lax_4ch_50_frame_4d = TensorMap(
     shape=(96, 96, 50),
     path_prefix='ukb_cardiac_mri', channel_map=MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP,
     loss='sparse_categorical_crossentropy', metrics=['mse'],
-    normalization=None,
     tensor_from_file=_segmented_heart_mask_instances('cine_segmented_lax_4ch_annotated_', LAX_4CH_HEART_LABELS, one_hot=False),
 )
 segmented_lax_4ch_48_frame_4d = TensorMap(
