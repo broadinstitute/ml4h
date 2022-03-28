ceec8bb9cda6e401c4901b4d56c8b2dffc77d3f9
Samuel Friedman <lucidtronix@gmail.com>
2022-03-28 13:33:36 -0400

    cleanup

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1939,17 +1939,16 @@ segmented_lax_4ch_50_frame = TensorMap(
     tensor_from_file=_segmented_heart_mask_instances('cine_segmented_lax_4ch_annotated_', LAX_4CH_HEART_LABELS, one_hot=False),
 )
 segmented_lax_4ch_diastole_frame = TensorMap(
-    'segmented_lax_4ch_diastole_frame', Interpretation.CONTINUOUS,
+    'segmented_lax_4ch_diastole_frame', Interpretation.CATEGORICAL,
     shape=(96, 96, len(MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP)),
     path_prefix='ukb_cardiac_mri', channel_map=MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP,
-    loss='sparse_categorical_crossentropy',
     tensor_from_file=_segmented_heart_mask_instances('cine_segmented_lax_4ch_annotated_', LAX_4CH_HEART_LABELS, frames=1),
 )
 segmented_lax_4ch_48_frame = TensorMap(
     'segmented_lax_4ch_48_frame', Interpretation.CONTINUOUS,
     shape=(96, 96, 48),
     path_prefix='ukb_cardiac_mri',
-    loss='sparse_categorical_crossentropy',
+    loss='mse',
     tensor_from_file=_segmented_heart_mask_instances('cine_segmented_lax_4ch_annotated_', LAX_4CH_HEART_LABELS, frames=48, one_hot=False),
 )
 segmented_lax_4ch_50_frame_4d = TensorMap(
