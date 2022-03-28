00f45ec20e21fe6dc5bbdb618c72852054a51047
Samuel Friedman <lucidtronix@gmail.com>
2022-03-28 11:27:25 -0400

    cleanup

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1942,7 +1942,7 @@ segmented_lax_4ch_diastole_frame = TensorMap(
     'segmented_lax_4ch_diastole_frame', Interpretation.CONTINUOUS,
     shape=(96, 96, len(MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP)),
     path_prefix='ukb_cardiac_mri', channel_map=MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP,
-    loss=tf.keras.losses.SparseCategoricalCrossentropy,
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(name='segmented_lax_4ch_diastole_frame'),
     tensor_from_file=_segmented_heart_mask_instances('cine_segmented_lax_4ch_annotated_', LAX_4CH_HEART_LABELS, frames=1),
 )
 segmented_lax_4ch_48_frame = TensorMap(
