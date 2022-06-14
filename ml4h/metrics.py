8189c3af5dd07a7f9ed5427c64eeb8a70058eb1c
Samuel Friedman <lucidtronix@gmail.com>
2022-06-14 09:44:16 -0400

    fix bug

--- ml4h/metrics.py
+++ ml4h/metrics.py
@@ -169,7 +169,15 @@ def pearson(y_true, y_pred):
 
 
 def abs_pearson(y_true, y_pred):
-    return tf.math.abs(pearson(y_true, y_pred))
+    # normalizing stage - setting a 0 mean.
+    y_true -= K.mean(y_true, axis=-1)
+    y_pred -= K.mean(y_pred, axis=-1)
+    # normalizing stage - setting a 1 variance
+    y_true = K.l2_normalize(y_true, axis=-1)
+    y_pred = K.l2_normalize(y_pred, axis=-1)
+    # final result
+    pearson_correlation = K.sum(y_true * y_pred, axis=-1)
+    return tf.math.abs(pearson_correlation)
 
 
 def _make_riskset(follow_up_times):
