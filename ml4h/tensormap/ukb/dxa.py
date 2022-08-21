7531a4df37f2118d10a5435005093b8546b9202e
Samuel Friedman <lucidtronix@gmail.com>
2022-08-21 16:21:22 -0400

    dxa

---
+++ ml4h/tensormap/ukb/dxa.py
@@ -0,0 +1,14 @@
+import h5py
+import numpy as np
+
+from ml4h.normalizer import ZeroMeanStd1
+from ml4h.TensorMap import TensorMap, Interpretation
+from ml4h.tensormap.general import get_tensor_at_first_date, normalized_first_date, pad_or_crop_array_to_shape
+
+dxa_12 = TensorMap(
+    'dxa_1_12',
+    shape=(760, 292, 1),
+    path_prefix='ukb_dxa',
+    tensor_from_file=normalized_first_date,
+    normalization=ZeroMeanStd1(),
+)
