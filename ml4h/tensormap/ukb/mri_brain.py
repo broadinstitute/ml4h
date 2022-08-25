6f143f27f929c948c3e279e9445e48d4065f1939
Samuel Friedman <lucidtronix@gmail.com>
2022-08-25 11:16:49 -0400

    dxa

--- ml4h/tensormap/ukb/mri_brain.py
+++ ml4h/tensormap/ukb/mri_brain.py
@@ -266,6 +266,14 @@ t1_mni_cerebellum_white_matter_30_60 = TensorMap(
     tensor_from_file=_mni_label_masked({'Left_cerebellum_white_matter': 90, 'Right_cerebellum_white_matter': 39}),
     normalization=ZeroMeanStd1(),
 )
+t1_mni_cerebellum_gray_matter_30_60 = TensorMap(
+    't1_mni_cerebellum_gray_matter_30_60',
+    shape=(176, 216, 30),
+    path_prefix='ukb_brain_mri/T1_brain_to_MNI/',
+    tensor_from_file=_mni_label_masked({'Left_cerebellum_gray_matter': 97, 'Right_cerebellum_gray_matter': 46}),
+    normalization=ZeroMeanStd1(),
+)
+
 t1_mni_cerebellum_gray_matter_10_73 = TensorMap(
     't1_mni_cerebellum_gray_matter_10_73',
     shape=(176, 216, 63),
