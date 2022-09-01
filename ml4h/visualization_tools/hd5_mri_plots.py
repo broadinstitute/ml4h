fb89daa2aff83938e8c7a47888ded201eefe3456
Nicole Deflaux <deflaux@verily.com>
2022-09-01 21:43:28 +0000

    Pass a list of tuples instead of a dictionary.

--- ml4h/visualization_tools/hd5_mri_plots.py
+++ ml4h/visualization_tools/hd5_mri_plots.py
@@ -118,7 +118,7 @@ def choose_mri_tmap(
     default_vmin_value, default_vmax_value = compute_color_range(hd5, default_tmap_value)
 
     tmap_chooser = widgets.Dropdown(
-        options=sample_tmaps,
+        options=list(sample_tmaps.items()),
         value=default_tmap_value,
         description='Choose the MRI tensor TMAP name to visualize:',
         style={'description_width': 'initial'},
