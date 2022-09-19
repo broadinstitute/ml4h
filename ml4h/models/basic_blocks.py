e3a22da5a273a2cb40c6c10cd2d43e8bce077cc3
Samuel Friedman <lucidtronix@gmail.com>
2022-09-19 13:03:04 -0400

    update dl image

--- ml4h/models/basic_blocks.py
+++ ml4h/models/basic_blocks.py
@@ -138,8 +138,7 @@ class PartitionedLinearDecoder(Block):
         self.tensor_map = tensor_map
         if not self.can_apply():
             return
-        self.dense = Dense(units=tensor_map.shape[0], input_shape=(tensor_map.annotation_units,),
-                           name=tensor_map.output_name(), activation=tensor_map.activation)
+        self.dense = Dense(units=tensor_map.shape[0], name=tensor_map.output_name(), activation=tensor_map.activation)
         self.begin = tensor_map.days_window
         self.end = tensor_map.annotation_units + tensor_map.days_window
 
@@ -150,7 +149,7 @@ class PartitionedLinearDecoder(Block):
         if not self.can_apply():
             return x
 
-        x = self.dense(x[self.begin:self.end])
+        x = self.dense(x[:, self.begin:self.end])
         intermediates[self.tensor_map].append(x)
         return x
 
