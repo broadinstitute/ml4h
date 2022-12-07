d0d3f33a600d5e26347819e29eed5737bec44af4
Samuel Friedman <lucidtronix@gmail.com>
2022-12-07 08:51:24 -0500

    ecg 576

--- ml4h/tensormap/ukb/ecg.py
+++ ml4h/tensormap/ukb/ecg.py
@@ -572,6 +572,13 @@ ecg_rest_median_raw_10 = TensorMap(
     activation='linear', tensor_from_file=_make_ecg_rest(),
     metrics=['mse', 'mae'], channel_map=ECG_REST_MEDIAN_LEADS, normalization=Standardize(mean=0, std=10),
 )
+
+ecg_rest_median_576 = TensorMap(
+    'ecg_rest_median_576', Interpretation.CONTINUOUS, path_prefix='ukb_ecg_rest', shape=(576, 12), loss='logcosh',
+    activation='linear', tensor_from_file=_make_ecg_rest(), channel_map=ECG_REST_MEDIAN_LEADS,
+    normalization=Standardize(mean=0, std=10),
+)
+
 ecg_rest_median_raw_10_no_poor = TensorMap(
     'ecg_rest_median_raw_10', Interpretation.CONTINUOUS, path_prefix='ukb_ecg_rest', shape=(600, 12), loss='logcosh', activation='linear',
     tensor_from_file=_make_ecg_rest(skip_poor=True), metrics=['mse', 'mae'], channel_map=ECG_REST_MEDIAN_LEADS, normalization=Standardize(mean=0, std=10),
