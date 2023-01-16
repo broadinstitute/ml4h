783f3216690fa568ddbb98b4f74eb122f26dada1
Samuel Friedman <lucidtronix@gmail.com>
2023-01-16 14:13:46 -0500

    dataflow

--- ml4h/__init__.py
+++ ml4h/__init__.py
@@ -1,2 +1,2 @@
 from . import defines
-from .tensorize.dataflow.bigquery_ukb_queries import tensorize_sql_fields, write_tensor_from_sql
+#from .tensorize.dataflow.bigquery_ukb_queries import tensorize_sql_fields, write_tensor_from_sql
