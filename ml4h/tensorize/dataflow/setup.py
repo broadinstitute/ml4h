178de8420cc329892ca912573a3d175649fd95a3
Samuel Friedman <lucidtronix@gmail.com>
2023-01-16 11:58:49 -0500

    dataflow cleanups

--- ml4h/tensorize/dataflow/setup.py
+++ ml4h/tensorize/dataflow/setup.py
@@ -2,13 +2,10 @@ import pathlib
 from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ml4h',
     version='0.0.2',
     description='Machine Learning for Health python package',
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',
     url='https://github.com/broadinstitute/ml4h',
     python_requires='>=3.6',
     packages=find_packages(),
