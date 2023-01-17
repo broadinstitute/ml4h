57ab11279b3e16357e2826c9b324ad07d0dc448b
Samuel Friedman <lucidtronix@gmail.com>
2023-01-17 11:21:16 -0500

    dataflow

--- ml4h/models/pretrained_blocks.py
+++ ml4h/models/pretrained_blocks.py
@@ -47,6 +47,37 @@ class ResNetEncoder(Block):
         return x
 
 
+class ConvNeXtBaseEncoder(Block):
+    def __init__(
+            self,
+            *,
+            tensor_map: TensorMap,
+            pretrain_trainable: bool,
+            **kwargs,
+    ):
+        self.tensor_map = tensor_map
+        if not self.can_apply():
+            return
+        self.base_model = keras.applications.ConvNeXtBase(
+            input_shape=self.tensor_map.shape,
+            weights="imagenet",  # Load weights pre-trained on ImageNet.
+            #pooling = "avg",
+            include_top=False,
+        )  # Do not include the ImageNet classifier at the top.
+        self.base_model.trainable = pretrain_trainable
+
+    def can_apply(self):
+        return self.tensor_map.axes() == 3 and self.tensor_map.shape[-1] == 3
+
+    def __call__(self, x: Tensor, intermediates: Dict[TensorMap, List[Tensor]]) -> Tensor:
+        if not self.can_apply():
+            return x
+        x = self.base_model(x, training=False)
+        x = keras.layers.GlobalAveragePooling2D()(x)
+        intermediates[self.tensor_map].append(x)
+        return x
+
+
 class MoviNetEncoder(Block):
     def __init__(
             self,
