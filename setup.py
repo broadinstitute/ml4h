43e0364b11affa6f7d692dcaf4c148699365e8cd
Samuel Friedman <lucidtronix@gmail.com>
2023-01-17 05:40:20 -0500

    dataflow

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ml4h',
-    version='0.0.2dev1',
+    version='0.0.3',
     description='Machine Learning for Health python package',
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',
