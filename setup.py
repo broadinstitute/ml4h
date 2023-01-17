09eaff00273c81eb6f59638f7ff22ddcd38f4a1b
Samuel Friedman <lucidtronix@gmail.com>
2023-01-17 05:33:47 -0500

    dataflow

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
 # Get the requirements from the requirements file
-#requirements = (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
-#requirements = (here / 'env/requirements_ml4h_dataflow.txt').read_text(encoding='utf-8')
+requirements = (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ml4h',
-    version='0.0.2',
+    version='0.0.2dev1',
     description='Machine Learning for Health python package',
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',
     url='https://github.com/broadinstitute/ml4h',
     python_requires='>=3.6',
-    #install_requires=requirements,
+    install_requires=requirements,
     packages=find_packages(),
 )
