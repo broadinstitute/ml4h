96acd5575664cea6beb3b132ac3e8af24d1d8e7d
Samuel Friedman <lucidtronix@gmail.com>
2023-01-24 06:39:33 -0500

    fix

--- tests/ml4ht_integration/test_tensor_map.py
+++ tests/ml4ht_integration/test_tensor_map.py
@@ -74,8 +74,8 @@ class TestTensorMapSampleGetter:
     def test_train_model(self, expected_data, model):
         paths = [k[0] for k in expected_data]
         sample_getter = TensorMapSampleGetter(
-            TMAPS_UP_TO_4D,
-            TMAPS_UP_TO_4D,
+            TMAPS_UP_TO_4D[1:],
+            TMAPS_UP_TO_4D[1:],
         )
         dataset = SampleGetterIterableDataset(
             sample_getter=sample_getter,
@@ -93,7 +93,7 @@ class TestTensorMapSampleGetter:
             cycle(data_loader), validation_data=cycle(val_loader),
             steps_per_epoch=10, validation_steps=2, epochs=3,
         ).history
-        for tmap in TMAPS_UP_TO_4D:
+        for tmap in TMAPS_UP_TO_4D[1:]:
             for metric in tmap.metrics:
                 metric_name = metric if type(metric) == str else metric.__name__
                 name = f'{tmap.output_name()}_{metric_name}'
