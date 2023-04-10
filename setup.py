d26c6ed13d3f98715c01628fdb24ce1603487bae
Samuel Friedman <lucidtronix@gmail.com>
2023-04-10 12:51:34 -0400

    ecg phewas done

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import pathlib
 from setuptools import setup, find_packages
 
-#here = pathlib.Path(__file__).parent.resolve()
+here = pathlib.Path(__file__).parent.resolve()
 # Get the requirements from the requirements file
-#requirements =  (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
-#long_description = (here / 'README.md').read_text(encoding='utf-8')
+requirements =  (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ml4h',
-    version='0.0.4dev4',
+    version='0.0.5',
     description='Machine Learning for Health python package',
-    #long_description=long_description,  # Optional
-    #long_description_content_type='text/markdown',
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',
     url='https://github.com/broadinstitute/ml4h',
     python_requires='>=3.6',
-    #install_requires=requirements,
-    #install_requires=["ml4ht", "tensorflow"],
+    install_requires=requirements, #install_requires=["ml4ht", "tensorflow"],
     packages=find_packages(),
 )
