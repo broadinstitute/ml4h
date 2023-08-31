6ba66bb5e22f3915f03059cc4563269ea1794f22
Sam Freesun Friedman <lucidtronix@gmail.com>
2023-08-31 10:57:48 -0400

    add data descriptions

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
 # Get the requirements from the requirements file
-requirements =  (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
+requirements = (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ml4h',
-    version='0.0.5',
+    version='0.0.6',
     description='Machine Learning for Health python package',
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',
