74d439e456c008350725e20742a8f69da86fbbd0
Christopher Reeder <creeder@broadinstitute.org>
2023-09-01 16:52:03 -0400

    echo training

---
+++ model_zoo/DROID/data_descriptions/wide_file.py
@@ -0,0 +1,71 @@
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+from ml4ht.data.data_description import DataDescription
+
+from echo import VIEW_OPTION_KEY
+
+
+class EcholabDataDescription(DataDescription):
+    # DataDescription for a wide file
+
+    def __init__(
+            self,
+            wide_df: pd.DataFrame,
+            sample_id_column: str,
+            column_names: str,
+            name: str,
+            categories: Dict = None,
+            transforms=None,
+    ):
+        """
+        """
+        self.wide_df = wide_df
+        self._name = name
+        self.sample_id_column = sample_id_column
+        self.column_names = column_names
+        self.categories = categories
+        self.prep_df()
+        self.transforms = transforms or []
+
+    def prep_df(self):
+        self.wide_df.index = self.wide_df[self.sample_id_column]
+        self.wide_df = self.wide_df.drop_duplicates()
+
+    def get_loading_options(self, sample_id):
+        row = self.wide_df.loc[sample_id]
+
+        # a loading option is a dictionary of options to use at loading time
+        # we use DATE_OPTION_KEY to make the date selection utilities work
+        loading_options = [{VIEW_OPTION_KEY: row}]
+
+        # it's get_loading_options, not get loading_option, so we return a list
+        return loading_options
+
+    def get_raw_data(self, sample_id, loading_option=None):
+        try:
+            if sample_id.shape[0] > 1:
+                sample_id = sample_id[0]
+        except AttributeError:
+            pass
+        try:
+            sample_id = sample_id.decode('UTF-8')
+        except (UnicodeDecodeError, AttributeError):
+            pass
+        row = self.wide_df.loc[sample_id]
+        data = row[self.column_names].values
+        label_noise = np.zeros(len(self.column_names))
+        for transform in self.transforms:
+            label_noise += transform()
+        if self.categories:
+            output_data = np.zeros(len(self.categories), dtype=np.float32)
+            output_data[self.categories[data[0]]['index']] = 1.0
+            return output_data
+        return np.squeeze(np.array(data, dtype=np.float32))
+
+    @property
+    def name(self):
+        # if we have multiple wide file DataDescriptions at the same time,
+        # this will allow us to differentiate between them
+        return self._name
--- model_zoo/DROID/echo_supervised_training_recipe.py
+++ model_zoo/DROID/echo_supervised_training_recipe.py
@@ -1,62 +1,51 @@
-import os
-import json
-import logging
 import argparse
 import datetime
+import json
+import logging
+import os
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-from ml4h_ccds.data_descriptions.wide_file import EcholabDataDescription
-from ml4h_ccds.data_descriptions.echo import LmdbEchoStudyVideoDataDescription
-
-from ml4h_ccds.data_descriptions.util import upload_folder_to_s3
-from ml4h_ccds.model_descriptions.echo import create_movinet_classifier, create_regressor, train_model, DDGenerator
-
-from ml4h_ccds.echo_defines import category_dictionaries, DATA_FOLDERS
-from ml4h_ccds.echo_utils import download_s3_folders
+from data_descriptions.echo import LmdbEchoStudyVideoDataDescription
+from data_descriptions.wide_file import EcholabDataDescription
+from echo_defines import category_dictionaries
+from model_descriptions.echo import create_movinet_classifier, create_regressor, train_model, DDGenerator
 
 logging.basicConfig(level=logging.INFO)
 tf.get_logger().setLevel(logging.ERROR)
 
 USER = os.getenv('USER')
 
-def main(
-    n_input_frames,
-    output_labels,
-    wide_file,
-    splits_file,
-    selected_views,
-    selected_doppler,
-    selected_quality,
-    selected_canonical,
-    n_train_patients,
-    batch_size,
-    epochs,
-    skip_modulo,
-    fine_tune,
-    model_params,
-    model_signature,
-    adam,
-    scale_outputs,
-    output_signature_labels
-):
 
+def main(
+        n_input_frames,
+        output_labels,
+        wide_file,
+        splits_file,
+        selected_views,
+        selected_doppler,
+        selected_quality,
+        selected_canonical,
+        n_train_patients,
+        batch_size,
+        epochs,
+        skip_modulo,
+        lmdb_folder,
+        fine_tune,
+        model_params,
+        pretrained_chkp_dir,
+        movinet_chkp_dir,
+        output_dir,
+        adam,
+        scale_outputs,
+        output_signature_labels
+):
     lmdb_vois = '_'.join(selected_views)
     olabels = '_'.join(output_labels)
-    DATA_FOLDERS['echo_lmdb']['source'] = f'stage3_trainvalid_{lmdb_vois}'
-    DATA_FOLDERS['echo_lmdb']['target'] = f'{os.path.expanduser("~")}/data/stage3_trainvalid_{lmdb_vois}/'
 
-    # Download echo folders to scratch space
-    download_s3_folders(DATA_FOLDERS, USER)
-
-    lmdb_folder = DATA_FOLDERS['echo_lmdb']['target']
-    wide_folder = DATA_FOLDERS['wide']['target']
-    splits_folder = DATA_FOLDERS['splits']['target']
-    model_folder = DATA_FOLDERS['supervised_models']['target']
-
-    wide_df = pd.read_parquet(f'{wide_folder}/{wide_file}')  
+    wide_df = pd.read_parquet(wide_file)
 
     # Select only view(s) of interest
     selected_views_idx = [category_dictionaries['view'][v] for v in selected_views]
@@ -64,28 +53,28 @@ def main(
     selected_quality_idx = [category_dictionaries['quality'][v] for v in selected_quality]
     selected_canonical_idx = [category_dictionaries['canonical'][v] for v in selected_canonical]
     wide_df_selected = wide_df[
-        (wide_df['view_prediction'].isin(selected_views_idx)) & \
-        (wide_df['doppler_prediction'].isin(selected_doppler_idx)) & \
-        (wide_df['quality_prediction'].isin(selected_quality_idx)) & \
-        (wide_df['canonical_prediction'].isin(selected_canonical_idx))    
-    ]
+        (wide_df['view_prediction'].isin(selected_views_idx)) &
+        (wide_df['doppler_prediction'].isin(selected_doppler_idx)) &
+        (wide_df['quality_prediction'].isin(selected_quality_idx)) &
+        (wide_df['canonical_prediction'].isin(selected_canonical_idx))
+        ]
 
     # Drop entries without echolab measurements and get all sample_ids
     wide_df_selected = wide_df_selected.dropna(subset=output_labels)
-    working_ids = wide_df_selected['sample_id'].values.tolist() 
+    working_ids = wide_df_selected['sample_id'].values.tolist()
 
     # Read splits and partition dataset
-    with open(f'{splits_folder}/{splits_file}', 'r') as json_file:
+    with open(splits_file, 'r') as json_file:
         splits = json.load(json_file)
 
     patient_train = splits['patient_train']
     patient_valid = splits['patient_valid']
 
     if n_train_patients != 'all':
-        patient_train = patient_train[:int(int(n_train_patients)*0.9)]
-        patient_valid = patient_valid[:int(int(n_train_patients)*0.1)]
+        patient_train = patient_train[:int(int(n_train_patients) * 0.9)]
+        patient_valid = patient_valid[:int(int(n_train_patients) * 0.1)]
 
-    train_ids = [t for t in working_ids if int(t.split('_')[0]) in patient_train]  
+    train_ids = [t for t in working_ids if int(t.split('_')[0]) in patient_train]
     valid_ids = [t for t in working_ids if int(t.split('_')[0]) in patient_valid]
 
     # If scale_outputs, normalize by summary stats of training set
@@ -96,7 +85,6 @@ def main(
         wide_df_selected.loc[:, output_labels] = (wide_df_selected[output_labels].values - mean_outputs) / std_outputs
         logging.info(mean_outputs)
         logging.info(std_outputs)
-        
 
     train_ids = list(set(train_ids).intersection(set(working_ids)))
     valid_ids = list(set(valid_ids).intersection(set(working_ids)))
@@ -108,16 +96,20 @@ def main(
         n_input_frames,
         skip_modulo
     )
-        
-    OUTPUT_DD = EcholabDataDescription(
-        wide_df = wide_df_selected[['sample_id'] + output_labels].drop_duplicates(),
-        sample_id_column = 'sample_id',
-        column_names = output_labels,
-        name = 'echolab'
-    )                     
 
-    body_train_ids = tf.data.Dataset.from_tensor_slices(train_ids).shuffle(len(train_ids), reshuffle_each_iteration=True).batch(batch_size, drop_remainder=True)            
-    body_valid_ids = tf.data.Dataset.from_tensor_slices(valid_ids).shuffle(len(valid_ids), reshuffle_each_iteration=True).batch(batch_size, drop_remainder=True)
+    OUTPUT_DD = EcholabDataDescription(
+        wide_df=wide_df_selected[['sample_id'] + output_labels].drop_duplicates(),
+        sample_id_column='sample_id',
+        column_names=output_labels,
+        name='echolab'
+    )
+
+    body_train_ids = tf.data.Dataset.from_tensor_slices(train_ids).shuffle(len(train_ids),
+                                                                           reshuffle_each_iteration=True).batch(
+        batch_size, drop_remainder=True)
+    body_valid_ids = tf.data.Dataset.from_tensor_slices(valid_ids).shuffle(len(valid_ids),
+                                                                           reshuffle_each_iteration=True).batch(
+        batch_size, drop_remainder=True)
 
     n_train_steps = len(train_ids) // batch_size
     n_valid_steps = len(valid_ids) // batch_size
@@ -131,11 +123,11 @@ def main(
                 INPUT_DD,
                 OUTPUT_DD
             ),
-            output_signature = (
+            output_signature=(
                 tf.TensorSpec(shape=(batch_size, n_input_frames, 224, 224, 3), dtype=tf.float32),
                 tf.TensorSpec(shape=output_batch_shape, dtype=tf.float32),
             ),
-            args = (sample_ids,)
+            args=(sample_ids,)
         )
     ).repeat(epochs).prefetch(8)
 
@@ -145,21 +137,21 @@ def main(
                 INPUT_DD,
                 OUTPUT_DD
             ),
-            output_signature = (
+            output_signature=(
                 tf.TensorSpec(shape=(batch_size, n_input_frames, 224, 224, 3), dtype=tf.float32),
                 tf.TensorSpec(shape=output_batch_shape, dtype=tf.float32),
             ),
-            args = (sample_ids,)
+            args=(sample_ids,)
         )
-    ).repeat(epochs).prefetch(8)           
-    
+    ).repeat(epochs).prefetch(8)
+
     mirrored_strategy = tf.distribute.MirroredStrategy()
     with mirrored_strategy.scope():
         _, backbone = create_movinet_classifier(
             n_input_frames,
             batch_size,
             num_classes=600,
-            checkpoint_dir=f'{model_folder}/movinet_a2_base',
+            checkpoint_dir=movinet_chkp_dir,
             freeze_backbone=fine_tune
         )
         backbone_output = backbone.layers[-1].output[0]
@@ -170,40 +162,40 @@ def main(
             encoder,
             input_shape=(n_input_frames, 224, 224, 3),
             n_output_features=len(output_labels),
-            trainable=not(fine_tune)
+            trainable=not fine_tune
         )
 
-        if model_signature:
+        if pretrained_chkp_dir:
             model = create_regressor(
                 encoder,
                 input_shape=(n_input_frames, 224, 224, 3),
                 n_output_features=len(output_signature_labels),
-                trainable=not(fine_tune)
+                trainable=not fine_tune
             )
-            model.load_weights(f'{DATA_FOLDERS["supervised_models"]["target"]}/{model_signature}/model/chkp')        
+            model.load_weights(pretrained_chkp_dir)
 
             if set(output_labels) != set(output_signature_labels):
                 model = create_regressor(
                     encoder,
                     input_shape=(n_input_frames, 224, 224, 3),
                     n_output_features=len(output_labels),
-                    trainable=not(fine_tune)
-                )               
+                    trainable=not fine_tune
+                )
 
         if adam:
             optimizer = tf.keras.optimizers.Adam(learning_rate=adam)
         else:
-            initial_learning_rate = 0.00005 * batch_size 
+            initial_learning_rate = 0.00005 * batch_size
             learning_rate = tf.keras.optimizers.schedules.CosineDecay(
-                initial_learning_rate, 
-                decay_steps = n_train_steps * epochs,
+                initial_learning_rate,
+                decay_steps=n_train_steps * epochs,
             )
 
             optimizer = tf.keras.optimizers.RMSprop(
-                learning_rate, 
-                rho=0.9, 
-                momentum=0.9, 
-                epsilon=1.0, 
+                learning_rate,
+                rho=0.9,
+                momentum=0.9,
+                epsilon=1.0,
                 clipnorm=1.0
             )
 
@@ -223,8 +215,9 @@ def main(
     io_valid_ds = io_valid_ds.with_options(options)
 
     fine_tune_string = f'_fine_tune' if fine_tune else ''
-    output_folder = f'{model_folder}{datetime.datetime.now().strftime("%Y%m%d%H%M")}_{lmdb_vois}_{olabels}_{n_input_frames}frames{fine_tune_string}_{n_train_patients}'
-    
+    output_folder = os.path.join(output_dir,
+                                 f'{datetime.datetime.now().strftime("%Y%m%d%H%M")}_{lmdb_vois}_{olabels}_{n_input_frames}frames{fine_tune_string}_{n_train_patients}')
+
     os.makedirs(output_folder, exist_ok=True)
     with open(f'{output_folder}/model_params.json', 'w') as json_file:
         json.dump(model_params, json_file)
@@ -240,89 +233,45 @@ def main(
         output_folder
     )
 
-    # Upload model folder to the supervised model bucket
-    upload_folder_to_s3(
-        '2017P001650',
-        f'echos/{DATA_FOLDERS["supervised_models"]["source"]}',
-        output_folder
-    )
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_input_frames', type=int, default=32)
-    parser.add_argument(
-        '-o',
-        '--output_labels', 
-        action='append', 
-        choices=[
-            'LV_LVEF_Measurement', # LVEF BETTER
-            'LV_RawPercent', # LVEF 
-            'LV_DiastolicDimension', # LVEDD
-            'LV_SystolicDimension', # LVESD
-            'LV_SeptalThicknessMm', # IVS Thickness
-            'LV_PosteriorWallThicknessMm', #PW thickness
-            'RV_RV_Dimension',
-            'converted_RVEDV',
-            'converted_RVEF',
-            'top3_discrepancy',
-            'bottom3_discrepancy'
-            'RV_RV_BasalDiameter',
-            'LA_A_P',
-            'RV_RV_FAC',
-            'RV_CavitySize',
-            'RV_SystolicFunction'
-        ], 
-        type=str
-    )
-    parser.add_argument('--wide_file', type=str, default='stage3_trainvalid_LVEF.pq')
-    parser.add_argument('-v', '--selected_views', action='append', choices=category_dictionaries['view'].keys(), required=True)
-    parser.add_argument('-d', '--selected_doppler', action='append', choices=category_dictionaries['doppler'].keys(), required=True)
-    parser.add_argument('-q', '--selected_quality', action='append', choices=category_dictionaries['quality'].keys(), required=True)
-    parser.add_argument('-c', '--selected_canonical', action='append', choices=category_dictionaries['canonical'].keys(), required=True)
+    parser.add_argument('-o', '--output_labels', action='append', type=str)
+    parser.add_argument('--wide_file', type=str)
+    parser.add_argument('-v', '--selected_views', action='append', choices=category_dictionaries['view'].keys(),
+                        required=True)
+    parser.add_argument('-d', '--selected_doppler', action='append', choices=category_dictionaries['doppler'].keys(),
+                        required=True)
+    parser.add_argument('-q', '--selected_quality', action='append', choices=category_dictionaries['quality'].keys(),
+                        required=True)
+    parser.add_argument('-c', '--selected_canonical', action='append',
+                        choices=category_dictionaries['canonical'].keys(), required=True)
     parser.add_argument('-n', '--n_train_patients', type=str, required=True)
     parser.add_argument('--batch_size', default=16, type=int)
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--skip_modulo', type=int, default=2)
+    parser.add_argument('--lmdb_folder', type=str)
     parser.add_argument('--fine_tune', action='store_true')
-    parser.add_argument('--model_signature', type=str)
-    parser.add_argument('--splits_file', default='stage3_splits_050222.json')
+    parser.add_argument('--pretrained_chkp_dir', type=str)
+    parser.add_argument('--movinet_chkp_dir', type=str)
+    parser.add_argument('--output_dir', type=str)
+    parser.add_argument('--splits_file')
     parser.add_argument('--adam', default=None, type=float)
     parser.add_argument('--scale_outputs', action='store_true')
-    parser.add_argument(
-        '-os',
-        '--output_signature_labels', 
-        action='append', 
-        choices=[
-            'LV_RawPercent', 
-            'LV_DiastolicDimension', 
-            'LV_SystolicDimension', 
-            'LV_SeptalThicknessMm', 
-            'LV_PosteriorWallThicknessMm',
-            'RV_RV_Dimension',
-            'converted_RVEDV',
-            'converted_RVEF',
-            'top3_discrepancy',
-            'bottom3_discrepancy'
-            'RV_RV_BasalDiameter',
-            'LA_A_P',
-            'RV_RV_FAC',
-            'RV_CavitySize',
-            'RV_SystolicFunction'
-        ], 
-        type=str
-    )
+    parser.add_argument('-os', '--output_signature_labels', action='append', type=str)
 
     args = parser.parse_args()
 
     root = logging.getLogger()
     root.setLevel(logging.INFO)
 
-    model_params = {}
+    model_params_dict = {}
     for arg, value in sorted(vars(args).items()):
         logging.info(f"Argument {arg}: {value}")
-        model_params[arg] = value
-    
+        model_params_dict[arg] = value
+
     main(
         n_input_frames=args.n_input_frames,
         output_labels=args.output_labels,
@@ -336,9 +285,12 @@ if __name__ == "__main__":
         batch_size=args.batch_size,
         epochs=args.epochs,
         skip_modulo=args.skip_modulo,
+        lmdb_folder=args.lmdb_folder,
         fine_tune=args.fine_tune,
-        model_params=model_params,
-        model_signature=args.model_signature,
+        model_params=model_params_dict,
+        pretrained_chkp_dir=args.pretrained_chkp_dir,
+        movinet_chkp_dir=args.movinet_chkp_dir,
+        output_dir=args.output_dir,
         adam=args.adam,
         scale_outputs=args.scale_outputs,
         output_signature_labels=args.output_signature_labels
