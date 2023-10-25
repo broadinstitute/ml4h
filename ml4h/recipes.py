481f5348f452aaf01f24b656e32a47b971d0f77d
Sam Freesun Friedman <lucidtronix@gmail.com>
2023-10-25 10:00:13 -0400

    xdl recipe

--- ml4h/recipes.py
+++ ml4h/recipes.py
@@ -428,14 +428,13 @@ def infer_from_dataloader(dataloader, model, tensor_maps_out, max_batches=125000
 
 
 def infer_xdl(args):
-    import torch
-    torch.multiprocessing.set_sharing_strategy('file_system')
     mrn_df = pd.read_csv(args.app_csv)
     if 'start_fu_age' in mrn_df:
         mrn_df['age_in_days'] = pd.to_timedelta(mrn_df.start_fu_age).dt.days
     elif 'start_fu' in mrn_df:
         mrn_df['age_in_days'] = pd.to_timedelta(mrn_df.start_fu).dt.days
     mrn_df = mrn_df.rename(columns={'Dem.Gender.no_filter_x': 'sex'})
+    mrn_df['is_c3po'] = mrn_df.cohort == 'c3po'
     for ot in args.tensor_maps_out:
         mrn_df = mrn_df[mrn_df[ot.name].notna()]
     mrn_df = mrn_df.set_index('sample_id')
@@ -475,8 +474,12 @@ def infer_xdl(args):
     )
 
     dataset = SampleGetterIterableDataset(sample_ids=list(mrn_df.index), sample_getter=sg, get_epoch=shuffle_get_epoch)
-    dataloader = DataLoader(dataset, num_workers=0, batch_size=args.batch_size)
-
+    #dataloader = DataLoader(dataset, num_workers=0, batch_size=args.batch_size)
+    dataloader = TensorMapDataLoader2(
+        batch_size=args.batch_size, input_maps=args.tensor_maps_in, output_maps=args.tensor_maps_out,
+        dataset=dataset,
+        num_workers=0,
+    )
     model, _, _, _ = make_multimodal_multitask_model(**args.__dict__)
     logging.info(f'Try to infer on dataloader.')
     infer_df = infer_from_dataloader(dataloader, model, args.tensor_maps_out)
