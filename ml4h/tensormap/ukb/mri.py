17d8f51fc08dc5131a13ca925109c96071d29707
Sam Freesun Friedman <lucidtronix@gmail.com>
2023-11-03 13:10:07 -0400

    cleanup

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -415,10 +415,30 @@ lax_2ch_diastole_slice0_3d = TensorMap(
         'ukb_cardiac_mri/cine_segmented_lax_2ch/2/instance_0', 0,
     ),
 )
+lax_3ch_diastole_slice_224_160_3d = TensorMap(
+    'lax_2ch_diastole_slice_224_160_3d',
+    Interpretation.CONTINUOUS,
+    shape=(224, 160, 1),
+    loss='logcosh',
+    normalization=ZeroMeanStd1(),
+    tensor_from_file=_slice_tensor(
+        'ukb_cardiac_mri/cine_segmented_lax_2ch/2/instance_0', 0,
+    ),
+)
 lax_3ch_diastole_slice0_3d = TensorMap(
     'lax_3ch_diastole_slice0_3d',
     Interpretation.CONTINUOUS,
     shape=(200, 160, 1),
+    loss='logcosh',
+    normalization=ZeroMeanStd1(),
+    tensor_from_file=_slice_tensor(
+        'ukb_cardiac_mri/cine_segmented_lax_3ch/2/instance_0', 0,
+    ),
+)
+lax_3ch_diastole_slice_224_160_3d = TensorMap(
+    'lax_3ch_diastole_slice_224_160_3d',
+    Interpretation.CONTINUOUS,
+    shape=(224, 160, 1),
     loss='logcosh',
     normalization=ZeroMeanStd1(),
     tensor_from_file=_slice_tensor(
