fe93385c8b78e4bac03eac624c82e2f4dcbccc50
abaumann <abaumann@broadinstitute.org>
2024-02-06 13:58:12 -0500

    Changed parallelization within tensorize.sh so it runs in parallel within docker rather than parallel docker - about a 10x speed up.  Added hdf5-tools to the docker for testing the hd5 files.  Added validate_tensors.sh that can test that a generated hd5 file is complete.

--- docker/vm_boot_images/config/ubuntu.sh
+++ docker/vm_boot_images/config/ubuntu.sh
@@ -3,4 +3,4 @@
 # Other necessities
 apt-get update
 echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | debconf-set-selections
-apt-get install -y wget unzip curl python3-pydot python3-pydot-ng graphviz ttf-mscorefonts-installer git pip ffmpeg
+apt-get install -y wget unzip curl python3-pydot python3-pydot-ng graphviz ttf-mscorefonts-installer git pip ffmpeg hdf5-tools
