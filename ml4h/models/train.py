f0b0997e4d7b7670ebb0170c1c075a7c536517c9
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-03-08 15:54:43 -0800

    cleanup

--- ml4h/models/train.py
+++ ml4h/models/train.py
@@ -141,9 +141,9 @@ def train_diffusion_model(args):
             show_layer_activations=False,
         )
 
-    if os.path.exists(checkpoint_path):
-        model.load_weights(checkpoint_path)
-        logging.info(f'Loaded weights from model checkpoint at: {checkpoint_path}')
+    #if os.path.exists(checkpoint_path):
+    model.load_weights(checkpoint_path)
+    logging.info(f'Loaded weights from model checkpoint at: {checkpoint_path}')
 
     history = model.fit(
         generate_train,
