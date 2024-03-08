fe1e287bc0e61cb9b93a2dbd80e20d858fa82e52
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-03-08 12:29:51 -0800

    cleanup

--- ml4h/tensormap/tensor_map_maker.py
+++ ml4h/tensormap/tensor_map_maker.py
@@ -269,7 +269,6 @@ def generate_categorical_tensor_map_from_file(
 def _space_tensor_from_file(df: pd.DataFrame, dimensions: int, sample_column: str = 'sample_id'):
     def tensor_from_file(tm: TensorMap, hd5: h5py.File, dependents=None):
         sample_id = os.path.basename(hd5.filename).replace('.hd5', '')
-        print(f'Got a sample_id {sample_id}, now try row...')
         row = df[df[sample_column] == sample_id]
         if len(row) == 0:
             raise KeyError(f'Sample id not in dataframe.')
