8432f456fec6af61f8c064bd51bb2079613d9d00
Andrey Smirnov <asmirnov2009@gmail.com>
2024-04-16 17:00:46 -0400

    Updated SQL queries to produce materialized* tables. Updated UKBB disease table generation code to output met_exclusion field. Also added scripts to automatically run disease table generation code on all TAB files.

--- phenotype_labels/disease/materialized_special_dates.sql
+++ phenotype_labels/disease/materialized_special_dates.sql
@@ -5,9 +5,9 @@ WITH dated_fields AS (
       WHEN cod.meaning LIKE ('%unknown%') THEN SAFE.PARSE_DATE("%E4Y-%m-%d", denroll.value)
       ELSE SAFE.PARSE_DATE("%E4Y-%m-%d", d.value)
     END vdate
-  FROM `broad-ml4cvd.ukbb7089_2023_07_25.phenotype` p
-  JOIN `broad-ml4cvd.ukbb7089_2023_07_25.phenotype` denroll ON denroll.FieldID=53 AND denroll.sample_id=p.sample_id AND denroll.instance = 0 AND denroll.array_idx = 0
-  JOIN `broad-ml4cvd.ukbb7089_2023_07_25.phenotype` d ON d.sample_id=p.sample_id AND d.instance = p.instance AND d.array_idx = p.array_idx
+  FROM `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` p
+  JOIN `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` denroll ON denroll.FieldID=53 AND denroll.sample_id=p.sample_id AND denroll.instance = 0 AND denroll.array_idx = 0
+  JOIN `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` d ON d.sample_id=p.sample_id AND d.instance = p.instance AND d.array_idx = p.array_idx
     AND (
       FALSE
       OR (p.FieldID=42013 AND d.FieldID=42012)
@@ -16,31 +16,48 @@ WITH dated_fields AS (
       OR (p.FieldID=42007 AND d.FieldID=42006)
       OR (p.FieldID=42001 AND d.FieldID=42000)
     )
-  LEFT JOIN `broad-ml4cvd.ukbb7089_2023_07_25.coding` cod ON cod.coding_file_id = d.coding_file_id AND cod.coding = d.value
-), 
-dated_fields_fractional AS (
+  LEFT JOIN `broad-ml4cvd.ukbb7089_2024_01_20.coding` cod ON cod.coding_file_id = d.coding_file_id AND cod.coding = d.value
+),dated_death_cause_fields AS (
+  SELECT p.FieldID, p.sample_id eid, p.value code, cod.meaning,
+    CASE
+      WHEN SAFE.PARSE_DATE("%E4Y-%m-%d", d.value) IS NULL THEN SAFE.PARSE_DATE("%E4Y-%m-%d", denroll.value)
+      WHEN cod.meaning LIKE ('%unknown%') THEN SAFE.PARSE_DATE("%E4Y-%m-%d", denroll.value)
+      ELSE SAFE.PARSE_DATE("%E4Y-%m-%d", d.value)
+    END vdate
+  FROM `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` p
+  JOIN `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` denroll ON denroll.FieldID=53 AND denroll.sample_id=p.sample_id AND denroll.instance = 0 AND denroll.array_idx = 0
+  JOIN `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` d ON d.sample_id=p.sample_id AND d.instance = p.instance
+    AND (
+      FALSE
+      OR (p.FieldID=40001 AND d.FieldID=40000)
+      OR (p.FieldID=40002 AND d.FieldID=40000)
+    )
+  LEFT JOIN `broad-ml4cvd.ukbb7089_2024_01_20.coding` cod ON cod.coding_file_id = d.coding_file_id AND cod.coding = d.value
+),dated_fields_fractional AS (
   SELECT p.FieldID, p.sample_id eid, p.value code, cod.meaning,
   CASE
       WHEN SAFE.PARSE_DATE("%Y", d.value) IS NULL THEN SAFE.PARSE_DATE("%E4Y-%m-%d", denroll.value)
       WHEN cod.meaning LIKE ('%unknown%') THEN SAFE.PARSE_DATE("%E4Y-%m-%d", denroll.value)
       ELSE SAFE.PARSE_DATE("%Y", d.value)
     END vdate
-  FROM `broad-ml4cvd.ukbb7089_2023_07_25.phenotype` p
-  JOIN `broad-ml4cvd.ukbb7089_2023_07_25.phenotype` denroll ON denroll.FieldID=53 AND denroll.sample_id=p.sample_id AND denroll.instance = 0 AND denroll.array_idx = 0
-  JOIN `broad-ml4cvd.ukbb7089_2023_07_25.phenotype` d ON d.sample_id=p.sample_id AND d.instance = p.instance AND d.array_idx = p.array_idx
+  FROM `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` p
+  JOIN `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` denroll ON denroll.FieldID=53 AND denroll.sample_id=p.sample_id AND denroll.instance = 0 AND denroll.array_idx = 0
+  JOIN `broad-ml4cvd.ukbb7089_2024_01_20.phenotype` d ON d.sample_id=p.sample_id AND d.instance = p.instance AND d.array_idx = p.array_idx
     AND (
       FALSE
       OR (p.FieldID=20004 AND d.FieldID=20010)
       OR (p.FieldID=20002 AND d.FieldID=20008)
       OR (p.FieldID=20001 AND d.FieldID=20006)
     )
-  LEFT JOIN `broad-ml4cvd.ukbb7089_2023_07_25.coding` cod ON cod.coding_file_id = d.coding_file_id AND cod.coding = d.value
+  LEFT JOIN `broad-ml4cvd.ukbb7089_2024_01_20.coding` cod ON cod.coding_file_id = d.coding_file_id AND cod.coding = d.value
 )
 
 SELECT 
   diagnostics.eid sample_id, diagnostics.FieldID, diagnostics.code value, MIN(vdate) first_date
 FROM (
   SELECT * FROM dated_fields
+  UNION DISTINCT
+  SELECT * FROM dated_death_cause_fields
   UNION DISTINCT
   SELECT * FROM dated_fields_fractional
 ) diagnostics
