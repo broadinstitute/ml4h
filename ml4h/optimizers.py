00bc685e4df122b921cb5870c91d40a6a7a0a646
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-04-16 11:05:09 -0700

    tensorflow 2.16 and keras 3

--- ml4h/optimizers.py
+++ ml4h/optimizers.py
@@ -5,7 +5,7 @@ from typing import Optional
 from tensorflow.keras import optimizers
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
-from tensorflow_addons.optimizers import RectifiedAdam, TriangularCyclicalLearningRate, Triangular2CyclicalLearningRate
+#from tensorflow_addons.optimizers import RectifiedAdam, TriangularCyclicalLearningRate, Triangular2CyclicalLearningRate
 from tensorflow.keras.optimizers.schedules import CosineDecay
 
 from ml4h.plots import plot_find_learning_rate
@@ -23,24 +23,24 @@ def get_optimizer(name: str, learning_rate: float, steps_per_epoch: int = None, 
         return opt
     except ValueError:
         pass
-    if name in NON_KERAS_OPTIMIZERS:
-        return NON_KERAS_OPTIMIZERS[name](rate_or_schedule, **optimizer_kwargs)
+    # if name in NON_KERAS_OPTIMIZERS:
+    #     return NON_KERAS_OPTIMIZERS[name](rate_or_schedule, **optimizer_kwargs)
     raise ValueError(f'Unknown optimizer {name}.')
 
 
 def _get_learning_rate_schedule(learning_rate: float, learning_rate_schedule: str = None, steps_per_epoch: int = None):
     if learning_rate_schedule is None:
         return learning_rate
-    if learning_rate_schedule == 'triangular':
-        return TriangularCyclicalLearningRate(
-            initial_learning_rate=learning_rate / 5, maximal_learning_rate=learning_rate,
-            step_size=steps_per_epoch * 5,
-        )
-    if learning_rate_schedule == 'triangular2':
-        return Triangular2CyclicalLearningRate(
-            initial_learning_rate=learning_rate / 5, maximal_learning_rate=learning_rate,
-            step_size=steps_per_epoch * 5,
-        )
+    # if learning_rate_schedule == 'triangular':
+    #     return TriangularCyclicalLearningRate(
+    #         initial_learning_rate=learning_rate / 5, maximal_learning_rate=learning_rate,
+    #         step_size=steps_per_epoch * 5,
+    #     )
+    # if learning_rate_schedule == 'triangular2':
+    #     return Triangular2CyclicalLearningRate(
+    #         initial_learning_rate=learning_rate / 5, maximal_learning_rate=learning_rate,
+    #         step_size=steps_per_epoch * 5,
+    #     )
     if learning_rate_schedule == 'cosine_decay':
         return CosineDecay(initial_learning_rate=learning_rate, decay_steps=steps_per_epoch)
     else:
