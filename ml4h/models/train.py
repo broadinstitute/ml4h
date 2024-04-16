e031e0eb0724b0a43bb94bae9661f3a941a38959
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-04-16 11:09:21 -0700

    tensorflow 2.16 and keras 3

--- ml4h/models/train.py
+++ ml4h/models/train.py
@@ -6,7 +6,7 @@ from typing import List, Tuple, Iterable, Union
 
 import tensorflow as tf
 import keras
-import tensorflow_addons as tfa
+#import tensorflow_addons as tfa
 from tensorflow.keras.callbacks import History
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, Callback
