481dcdd976e71606d99b6a2fd1407d8bf0cf4e60
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-04-16 11:55:15 -0700

    tensorflow 2.16 and keras 3

--- ml4h/models/train.py
+++ ml4h/models/train.py
@@ -79,7 +79,7 @@ def train_model_from_generators(
 
     logging.info('Model weights saved at: %s' % model_file)
     custom_dict = get_custom_objects(output_tensor_maps)
-    model = keras.saving.load_model(model_file, custom_objects=custom_dict, compile=False)
+    model = tf.saved_model.load(model_file, custom_objects=custom_dict, compile=False)
     model.compile(optimizer='adam', loss='mse')
     if plot:
         plot_metric_history(history, training_steps, run_id, os.path.dirname(model_file))
