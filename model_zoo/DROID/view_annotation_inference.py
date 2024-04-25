70923b998866d870fd29fa9e40374b78ffd09f26
Christopher Reeder <creeder@broadinstitute.org>
2024-04-24 21:53:50 -0400

    Update CSV output format in view_annotation_inference.py

--- model_zoo/DROID/view_annotation_inference.py
+++ model_zoo/DROID/view_annotation_inference.py
@@ -3,7 +3,7 @@ import logging
 
 import tensorflow as tf
 
-from echo_defines import category_dictionaries, view_annotation_tasks
+from echo_defines import category_dictionaries
 from data_descriptions.echo import LmdbEchoStudyVideoDataDescriptionBWH
 from model_descriptions.echo import DDGenerator, create_movinet_classifier
 from model_descriptions.models import create_classifier, create_video_encoder
@@ -23,6 +23,7 @@ def main(
     output_file,
     pretrained_ckpt_dir,
     skip_modulo,
+    view_annotation_tasks,
 ):
     input_dd = LmdbEchoStudyVideoDataDescriptionBWH(
         lmdb_folder,
@@ -33,10 +34,13 @@ def main(
     )
 
     # Load the IDs
-    log_df = pd.read_csv('/data/ewok/bwh_lmdbs/00003665_EVS0399897.lmdb/log_00003665_EVS0399897.tsv', sep='\t')
-    log_df = log_df[log_df['stored']]
-    log_df['sample_id'] = log_df['view'].apply(lambda x: f'00003665_EVS0399897_{x}')
-    working_ids = sorted(log_df['sample_id'].values.tolist())
+    log_df = pd.read_csv(
+        "/data/ewok/bwh_lmdbs/00003665_EVS0399897.lmdb/log_00003665_EVS0399897.tsv",
+        sep="\t",
+    )
+    log_df = log_df[log_df["stored"]]
+    log_df["sample_id"] = log_df["view"].apply(lambda x: f"00003665_EVS0399897_{x}")
+    working_ids = sorted(log_df["sample_id"].values.tolist())
     body_inference_ids = tf.data.Dataset.from_tensor_slices(working_ids).batch(
         batch_size, drop_remainder=False
     )
@@ -54,36 +58,42 @@ def main(
     ).prefetch(2)
 
     encoder = create_video_encoder(
-        path=movinet_ckpt_dir,
-        input_shape=(n_input_frames, 224, 224, 3))
+        path=movinet_ckpt_dir, input_shape=(n_input_frames, 224, 224, 3)
+    )
     model = create_classifier(
-        encoder, 
-        trainable=False, 
-        input_shape=(n_input_frames, 224, 224, 3), 
+        encoder,
+        trainable=False,
+        input_shape=(n_input_frames, 224, 224, 3),
         categories={
-            f'annotation_{task}': len(category_dictionaries[task]) for task in view_annotation_tasks
-            }
+            f"annotation_{task}": len(category_dictionaries[task])
+            for task in view_annotation_tasks
+        },
     )
 
     model.load_weights(pretrained_ckpt_dir)
 
     model_predictions = {}
-    inference_steps = len(working_ids) // batch_size + int((len(working_ids) % batch_size) > 0.5)
+    inference_steps = len(working_ids) // batch_size + int(
+        (len(working_ids) % batch_size) > 0.5
+    )
 
     predictions = model.predict(io_inference_ds, steps=inference_steps, verbose=1)
     for i, task in enumerate(view_annotation_tasks):
-        model_predictions[f'{task}_prediction'] = predictions[i]
+        model_predictions[f"{task}_prediction"] = predictions[i]
 
     df = pd.DataFrame()
-    df['sample_id'] = working_ids[:len(model_predictions[f'{task}_prediction'])]    
+    df["sample_id"] = working_ids[: len(model_predictions[f"{task}_prediction"])]
     for task in view_annotation_tasks:
-        df[f'{task}_prediction'] = np.argmax(model_predictions[f'{task}_prediction'], axis=1)
-        df[f'{task}_prediction_probability'] = np.max(model_predictions[f'{task}_prediction'], axis=1)
-    df.to_csv(output_file, index=False, sep='\t')
+        df[f"{task}_prediction"] = np.argmax(
+            model_predictions[f"{task}_prediction"], axis=1
+        )
+        df[f"{task}_prediction_probability"] = np.max(
+            model_predictions[f"{task}_prediction"], axis=1
+        )
+    df.to_csv(output_file, index=False, sep="\t")
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--lmdb_folder", type=str)
@@ -92,6 +102,13 @@ if __name__ == "__main__":
     parser.add_argument("--output_file", type=str)
     parser.add_argument("--pretrained_ckpt_dir", type=str)
     parser.add_argument("--skip_modulo", type=int, default=1)
+    parser.add_argument(
+        "-v",
+        "--view_annotation_tasks",
+        choices=["doppler", "view", "quality", "canonical"],
+        type=str,
+        action="append",
+    )
 
     args = parser.parse_args()
 
@@ -103,4 +120,5 @@ if __name__ == "__main__":
         args.output_file,
         args.pretrained_ckpt_dir,
         args.skip_modulo,
+        args.view_annotation_tasks,
     )
