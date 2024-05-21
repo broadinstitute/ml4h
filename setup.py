308faa315406a84c3d4006a46d7ea5fb30478e32
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-05-21 11:28:40 -0700

    fix tests with reqs (#567)
    * fix tests, fix mint demo notebook

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
 # Get the requirements from the requirements file
+requirements = (here / 'docker/vm_boot_images/config/tensorflow-requirements.txt').read_text(encoding='utf-8')
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ml4h',
@@ -12,6 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/broadinstitute/ml4h',
     python_requires='>=3.6',
-    install_requires=["ml4ht", "tensorflow", "pytest", "numcodecs"],  # requirements
+    #install_requires=["ml4ht", "tensorflow", "pytest", "numcodecs"],  # requirements
+    install_requires=requirements,
     packages=find_packages(),
 )
