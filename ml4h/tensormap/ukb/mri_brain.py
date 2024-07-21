6bdcb819fe410a7e33f4bcb17a5c0b55731de59c
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-07-21 11:19:55 -0400

    kron

--- ml4h/tensormap/ukb/mri_brain.py
+++ ml4h/tensormap/ukb/mri_brain.py
@@ -61,6 +61,13 @@ t1_slice_100_224 = TensorMap(
     tensor_from_file=normalized_first_date,
     normalization=ZeroMeanStd1(),
 )
+t1_slice_100_256 = TensorMap(
+    'axial_100',
+    shape=(256, 256, 1),
+    path_prefix='ukb_brain_mri/T1/',
+    tensor_from_file=normalized_first_date,
+    normalization=ZeroMeanStd1(),
+)
 t1_slice_120 = TensorMap(
     'axial_120',
     shape=(216, 256, 1),
