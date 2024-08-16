22f4c3a6647e0ceb98f599c8822f4ac4145982fe
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-08-16 12:06:40 -0400

    fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
 from setuptools import setup, find_packages
 
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
+requirements_path = os.path.join(this_directory, 'docker/vm_boot_images/config/tensorflow-requirements.txt')
 # Function to read requirements from a file
+
+
 def load_requirements(filename):
     with open(filename, 'r') as file:
         return file.read().splitlines()
@@ -9,14 +14,14 @@ def load_requirements(filename):
 
 setup(
     name='ml4h',
-    version='0.0.16',
+    version='0.0.17',
     description='Machine Learning for Health python package',
-    #long_description= 'README.md'.read_text(encoding='utf-8'),  # Optional
+    long_description=os.path.join(this_directory, 'README.md').read_text(encoding='utf-8'),  # Optional
     long_description_content_type='text/markdown',
     url='https://github.com/broadinstitute/ml4h',
     python_requires='>=3.6',
     #install_requires=["ml4ht", "tensorflow", "pytest", "numcodecs"], # requirements
-    install_requires=load_requirements('docker/vm_boot_images/config/tensorflow-requirements.txt'),  # requirements
+    install_requires=load_requirements(requirements_path),  # requirements
     #install_requires=requirements,
     packages=find_packages(),
 )
