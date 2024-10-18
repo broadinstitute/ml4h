ae083af3a75048f25c353d837552f695559ff741
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-10-18 14:28:29 -0400

    test steps in diffusion plot

--- ml4h/models/train.py
+++ ml4h/models/train.py
@@ -201,8 +201,8 @@ def regress_on_controlled_generations(diffuser, regressor, tm_out, batches, batc
             controls = np.random.normal(0, std, size=batch_size)
         elif tm_out.is_categorical():
             controls = np.eye(tm_out.shape[-1])[np.random.choice(tm_out.shape[-1], batch_size)]
-        preds.append(regress_on_batch(diffuser, regressor, controls, tm_out, batch_size))
-        all_controls.append(controls)
+        preds.extend(regress_on_batch(diffuser, regressor, controls, tm_out, batch_size))
+        all_controls.extend(controls)
         if i % 4 == 0:
             logging.info(f'Inferred on {i+1} synthetic diffusion batches of {batches}')
 
