aa5c162444000c9b938399ffd7f8c44ce9ff97f5
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-11-27 15:16:57 -0500

    cumc docker

--- docker/ml4h_deploy/process_files.py
+++ docker/ml4h_deploy/process_files.py
@@ -2,11 +2,14 @@ import os
 import sys
 import numpy as np
 from tensorflow.keras.models import load_model
+from ml4h.TensorMap import TensorMap, Interpretation
 from ml4h.models.model_factory import get_custom_objects
 from ml4h.tensormap.ukb.survival import mgb_afib_wrt_instance2
-from ml4h.tensormap.ukb.demographics import age_2_wide, af_dummy, sex_dummy3
+from ml4h.tensormap.ukb.demographics import age_2_wide, af_dummy
 
-output_tensormaps = {tm.output_name(): tm for tm in [mgb_afib_wrt_instance2, age_2_wide, af_dummy, sex_dummy3]}
+sex_tmap = TensorMap(name='sex', interpretation=Interpretation.CATEGORICAL, channel_map={'Female': 0, 'Male':1})
+
+output_tensormaps = {tm.output_name(): tm for tm in [mgb_afib_wrt_instance2, age_2_wide, af_dummy, sex_tmap]}
 custom_dict = get_custom_objects(list(output_tensormaps.values()))
 model = load_model('ecg2af_quintuplet_v2024_01_13.h5', custom_objects=custom_dict)
 
