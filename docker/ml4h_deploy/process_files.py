13ee7457636096b977b312bd8560b9154cbcc8f2
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-11-27 15:13:20 -0500

    cumc docker

--- docker/ml4h_deploy/process_files.py
+++ docker/ml4h_deploy/process_files.py
@@ -8,10 +8,7 @@ from ml4h.tensormap.ukb.demographics import age_2_wide, af_dummy, sex_dummy3
 
 output_tensormaps = {tm.output_name(): tm for tm in [mgb_afib_wrt_instance2, age_2_wide, af_dummy, sex_dummy3]}
 custom_dict = get_custom_objects(list(output_tensormaps.values()))
-model = load_model('./ecg_5000_survival_curve_af_quadruple_task_mgh_v2021_05_21.h5', custom_objects=custom_dict)
-
-# Load the model
-model = load_model('ecg2af_quintuplet_v2024_01_13.h5')
+model = load_model('ecg2af_quintuplet_v2024_01_13.h5', custom_objects=custom_dict)
 
 def process_file(filepath):
     # Placeholder for file processing logic
