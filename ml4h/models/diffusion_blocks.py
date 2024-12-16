ab3c844522f53ea01e9bd506b94f97dc1ab93494
Sam Freesun Friedman <lucidtronix@gmail.com>
2024-12-16 11:35:18 -0500

    condition strategy

--- ml4h/models/diffusion_blocks.py
+++ ml4h/models/diffusion_blocks.py
@@ -106,12 +106,8 @@ def condition_layer_film(input_tensor, control_vector, filters):
     beta = layers.Dense(filters, activation="linear")(control_vector)
 
     # Reshape gamma and beta to match the spatial dimensions
-    # if 4 == len(input_tensor.shape):
-    #     gamma = tf.reshape(gamma, (-1, 1, 1, filters))
-    #     beta = tf.reshape(beta, (-1, 1, 1, filters))
-    # elif 3 == len(input_tensor.shape):
-    #     gamma = tf.reshape(gamma, (-1, 1, filters))
-    #     beta = tf.reshape(beta, (-1, 1, filters))
+    gamma = tf.reshape(gamma, input_tensor.input_shape[:-1] + (filters,))
+    beta = tf.reshape(beta, input_tensor.input_shape[:-1] + (filters,))
     # Apply FiLM (Feature-wise Linear Modulation)
     return input_tensor * gamma + beta
 
