26ecaf0d537765800a0a3edb67b3f34a24359164
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-04-07 11:44:18 -0400

    bmri 144

--- ml4h/tensormap/ukb/mri_brain.py
+++ ml4h/tensormap/ukb/mri_brain.py
@@ -477,6 +477,16 @@ t1_mni_random_slice_64_128 = TensorMap(
     dependent_map=axial_index_map,
 )
 
+t1_mni_random_slice_48_144 = TensorMap(
+    't1_mni_random_slice',
+    Interpretation.CONTINUOUS,
+    shape=(192, 192, 1),
+    path_prefix='ukb_brain_mri/T1_brain_to_MNI/',
+    tensor_from_file=_random_slice_bounded(48, 144),
+    normalization=ZeroMeanStd1(),
+    dependent_map=axial_index_map,
+)
+
 t1_mni_random_slice_32_160 = TensorMap(
     't1_mni_random_slice',
     Interpretation.CONTINUOUS,
