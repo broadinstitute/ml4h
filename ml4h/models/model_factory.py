de37b6b1ba5d74d60b3ff6c16f5fe428b4cd18e8
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-04-18 13:48:23 -0400

    mae loss

--- ml4h/models/model_factory.py
+++ ml4h/models/model_factory.py
@@ -134,7 +134,7 @@ def make_multimodal_multitask_model(
     losses = [tm.loss for tm in tensor_maps_out]
     if len(losses) == 0:
         logging.info(f"No losses found adding MAE")
-        #losses = [keras.losses.MeanAbsoluteError]
+        losses = None
         #full_model.add_loss(keras.losses.MeanAbsoluteError)
     full_model.compile(
         optimizer=opt, loss=losses, metrics={tm.output_name(): tm.metrics for tm in tensor_maps_out},
