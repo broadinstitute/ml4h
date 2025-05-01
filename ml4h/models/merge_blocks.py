6d4c6cb55f7ce24d33b2144e3153a089de923221
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-05-01 13:38:27 -0400

    remove mlflow

--- ml4h/models/merge_blocks.py
+++ ml4h/models/merge_blocks.py
@@ -427,8 +427,8 @@ class ContrastiveLossLayer(Layer):
     def call(self, inputs, training = None):
         # We use `add_loss` to create a regularization loss
         # that depends on the inputs.
-        if training is False:
-            return inputs
+        # if training is False:
+        #     return inputs
         contrastive_loss = self.weight * contrastive_difference(inputs[0], inputs[1], self.batch_size, self.temperature)
         self.add_loss(contrastive_loss)
         #self.add_metric(contrastive_loss)
