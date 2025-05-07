376dac5e5568249ab9eb34f4df83df6ac3ef85f5
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-05-07 15:54:09 -0400

    add license

--- ml4h/models/diffusion_blocks.py
+++ ml4h/models/diffusion_blocks.py
@@ -753,7 +753,7 @@ class DiffusionController(keras.Model):
 
     def get_config(self):
         config = super().get_config().copy()
-        config.update({'sigmoid_beta': self.sigmoid_beta, 'batch_size': self.batch_size})
+        config.update({'sigmoid_beta': self.beta, 'batch_size': self.batch_size})
         return config
 
     def compile(self, **kwargs):
