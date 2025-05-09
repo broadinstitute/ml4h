d8f1ac0386cc6f4d82732bb904cfb2c718686640
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-05-09 13:08:53 -0400

    add license

--- ml4h/models/train_diffusion.py
+++ ml4h/models/train_diffusion.py
@@ -36,10 +36,10 @@ def train_diffusion_model(args):
         feature_batch = batch[0][k]
     for k in batch[1]:
         logging.info(f"label {k} {batch[1][k].shape}")
-    checkpoint_path = f"{args.output_folder}{args.id}/{args.id}.keras"
+    checkpoint_path = f"{args.output_folder}{args.id}/{args.id}.h5"
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_path,
-        save_weights_only=False,
+        save_weights_only=True,
         monitor="val_i_loss",
         mode="min",
         save_best_only=True,
@@ -249,10 +249,10 @@ def train_diffusion_control_model(args, supervised=False):
         ),
         loss=loss,
     )
-    checkpoint_path = f"{args.output_folder}{args.id}/{args.id}.keras"
+    checkpoint_path = f"{args.output_folder}{args.id}/{args.id}.h5"
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_path,
-        save_weights_only=False,
+        save_weights_only=True,
         monitor="val_i_loss",
         mode="min",
         save_best_only=True,
