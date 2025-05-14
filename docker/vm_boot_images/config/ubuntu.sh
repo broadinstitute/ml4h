4f97a4e1737bbd63d81d0683c53c62cc996f1879
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-05-14 09:32:24 -0400

    turn cudnn off

--- docker/vm_boot_images/config/ubuntu.sh
+++ docker/vm_boot_images/config/ubuntu.sh
@@ -9,5 +9,5 @@ echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select t
 #dpkg -i cudnn-local-repo-ubuntu2204-9.8.0_1.0-1_amd64.deb
 #cp /var/cudnn-local-repo-ubuntu2204-9.8.0/cudnn-local-8138232B-keyring.gpg /usr/share/keyrings/
 apt-get update
-apt-get -y install cudnn
+#apt-get -y install cudnn
 apt-get install -y wget unzip curl python3-pydot python3-pydot-ng graphviz ttf-mscorefonts-installer git pip ffmpeg
