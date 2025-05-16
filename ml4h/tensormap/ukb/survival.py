cb0162593dc2484f5b486badc1c3f6de4d703fd6
Sam Freesun Friedman <lucidtronix@gmail.com>
2025-05-16 12:10:06 -0400

    GroupNorm SiLU Blocks in Diffusion Models (#601)

--- ml4h/tensormap/ukb/survival.py
+++ ml4h/tensormap/ukb/survival.py
@@ -410,6 +410,18 @@ mgb_afib_as_stroke_wrt_instance2 = TensorMap(
     ),
 )
 
+mgb_cad_wrt_instance2 = TensorMap(
+    'cad_event',
+    Interpretation.SURVIVAL_CURVE,
+    shape=(50,),
+    days_window=DAYS_IN_10_YEARS,
+    tensor_from_file=_survival_tensor(
+        'ukb_ecg_rest/ecg_rest_text/instance_0', DAYS_IN_10_YEARS,
+        disease_name_override='coronary_artery_disease',
+        start_date_is_attribute=True, incidence_only=True,
+    ),
+)
+
 
 prevalent_hf_wrt_instance2 = TensorMap(
     'heart_failure', Interpretation.CATEGORICAL, storage_type=StorageType.CATEGORICAL_FLAG,
