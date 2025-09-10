f80259f3ecf9825418a6272f3e07e76267a6070d
Danielle Pace <pace@broadinstitute.org>
2025-09-10 16:53:07 -0400

    ENH: Finish mechanism to merge derived ecg data

--- ml4h/tensorize/merge_hd5s.py
+++ ml4h/tensorize/merge_hd5s.py
@@ -6,7 +6,7 @@ import traceback
 from collections import Counter
 
 from ml4h.defines import TENSOR_EXT, HD5_GROUP_CHAR
-from ml4h.tensormap.ukb.ecg import _create_ecg_rest_random_beats
+from ml4h.tensormap.ukb.ecg import _check_valid_ecg_rest_random_beats, _create_ecg_rest_random_beats
 
 """
 This script copies all the hd5 datasets from all hd5 files within the 'sources'
@@ -25,7 +25,7 @@ python .merge_hd5s.py \
 """
 
 
-def merge_hd5s_into_destination(destination, sources, min_sample_id, max_sample_id, intersect, inplace, derived_data_fn):
+def merge_hd5s_into_destination(destination, sources, min_sample_id, max_sample_id, intersect, inplace, valid_fn, derived_data_fn):
     stats = Counter()
     if not os.path.exists(os.path.dirname(destination)):
         os.makedirs(os.path.dirname(destination))
@@ -47,35 +47,50 @@ def merge_hd5s_into_destination(destination, sources, min_sample_id, max_sample_
 
             with h5py.File(os.path.join(destination, source_file), 'a') as destination_hd5:
                 with h5py.File(os.path.join(source_folder, source_file), 'r') as source_hd5:
-                    _copy_hd5_datasets(source_hd5, destination_hd5, derived_data_fn=derived_data_fn, stats=stats)
+                    _copy_hd5_datasets(source_hd5, destination_hd5, valid_fn=valid_fn, derived_data_fn=derived_data_fn, stats=stats)
         logging.info(f"Done copying source folder {source_folder}")
 
     for k in stats:
         logging.info(f"{k} has {stats[k]} tensors")
 
 
-def _copy_hd5_datasets(source_hd5, destination_hd5, group_path=HD5_GROUP_CHAR, derived_data_fn=None, stats=None):
+def _copy_hd5_datasets(source_hd5, destination_hd5, group_path=HD5_GROUP_CHAR, valid_fn=None, derived_data_fn=None, stats=None):
     for k in source_hd5[group_path]:
         data = source_hd5[group_path][k]
-        if isinstance(data, h5py.Dataset):
+        name = group_path + k
+
+        is_dataset = isinstance(data, h5py.Dataset)
+        if valid_fn:
+            is_valid = valid_fn(name)
+        else:
+            is_valid = is_dataset
+
+        if is_valid:
             try:
                 if derived_data_fn:
-                    out_dict = derived_data_fn(group_path+k, data)
+                    out_dict = derived_data_fn(name, source_hd5)
+                    for out_name, out_data in out_dict.items():
+                        logging.debug(f"creating derived {out_name}")
+                        if isinstance(out_data, np.ndarray) or isinstance(out_data, list):
+                            destination_hd5.create_dataset(out_name, data=out_data, compression='gzip')
+                        else:
+                            destination_hd5.create_dataset(out_name, data=out_data)
+                        stats[out_name] += 1
                 else:
-                    out_dict = {group_path + k:data}
-
-                for out_name, out_data in out_dict.items():
-                    if out_data.chunks is None:
-                        destination_hd5.create_dataset(out_name, data=out_data)
+                    logging.debug(f"copying {name}")
+                    if data.chunks is None:
+                        destination_hd5.create_dataset(name, data=data)
                     else:
-                        destination_hd5.create_dataset(out_name, data=out_data, compression='gzip')
-
-                    stats[out_name] += 1
+                        destination_hd5.create_dataset(name, data=data, compression='gzip')
+                    stats[name] += 1
             except (OSError, KeyError, RuntimeError, ValueError) as e:
                 logging.debug(f"Error trying to write:{k} at group path:{group_path} error:{e}\n{traceback.format_exc()}\n")
-        else:
-            logging.debug(f"copying group {group_path + k}")
-            _copy_hd5_datasets(source_hd5, destination_hd5, group_path=group_path + k + HD5_GROUP_CHAR, derived_data_fn=derived_data_fn, stats=stats)
+        if not is_dataset:
+            _copy_hd5_datasets(source_hd5, destination_hd5, group_path=name + HD5_GROUP_CHAR, valid_fn=valid_fn, derived_data_fn=derived_data_fn, stats=stats)
+
+VALID_MAP = {
+    'ecg_rest_random_beats': _check_valid_ecg_rest_random_beats,
+}
 
 DEF_MAP = {
     'ecg_rest_random_beats': _create_ecg_rest_random_beats,
@@ -101,7 +116,12 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(args.logging_level)
     if args.derived_data_fn_name:
         derived_data_fn = DEF_MAP[args.derived_data_fn_name]
+        valid_fn = VALID_MAP[args.derived_data_fn_name]
     else:
         derived_data_fn = None
+        valid_fn = None
 
-    merge_hd5s_into_destination(args.destination, args.sources, args.min_sample_id, args.max_sample_id, args.intersect, args.inplace, derived_data_fn)
+    merge_hd5s_into_destination(
+        args.destination, args.sources, args.min_sample_id, args.max_sample_id, args.intersect,
+        args.inplace, valid_fn, derived_data_fn,
+    )
